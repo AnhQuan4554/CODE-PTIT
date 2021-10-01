10660fa527d3e9dfcd92a5a6c63b35e657d5c4b9
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-01 15:23:24 +0700

    ƯỚC SỐ CHUNG - BỘI SỐ CHUNG

---
+++ CODE PTIT/ƯỚC SỐ CHUNG - BỘI SỐ CHUNG.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+long long UCLN(long long a, long long b)
+{
+	while (a != b)
+	{
+		if (a > b)a = a - b;
+		if (a < b)b = b - a;
+	} return a;
+}
+long long BCNN(long long a, long long b)
+{
+	return (a * b) / UCLN(a, b);
+}
+int main()
+{
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		long long a;
+		long long b;
+		cin >> a >> b;
+		cout << BCNN(a, b) << ' ' << UCLN(a, b) << endl;
+	}
+	system("pause");
+	return 0;
+}
