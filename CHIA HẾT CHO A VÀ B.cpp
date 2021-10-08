d795b5fa3aaabb2fc9cb294f88dbf1294a7f9492
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-08 10:58:26 +0700

    Create CHIA HẾT CHO A VÀ B.cpp

---
+++ CHIA HẾT CHO A VÀ B.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long M, N, A, B;
+		long long count = 0;
+		cin >> M >> N >> A >> B;
+		for (long long i = M; i <= N; i++) {
+			if (i % A == 0) {
+				count++;
+				continue;
+			}
+				
+			if (i % B == 0) {
+				count++;
+				continue;
+			}
+				
+
+		}
+		cout << count << endl;
+	}
+
+
+}
