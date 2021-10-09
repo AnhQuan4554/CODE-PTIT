093b446ee230d65e83c4723e5a26faf71db2203e
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-09 14:41:54 +0700

    Update CHIA HẾT CHO 8.cpp
    Cho xâu ký tự S bao gồm các số từ 0 đến 9. Nhiệm vụ của bạn là đếm số các xâu con của S là số chia hết cho 8 và không chia hết cho 3.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một xâu ký tự số S.
    T, S thỏa mãn ràng buộc: 1≤ T ≤100; 0≤ Length(S) ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    888
    6564525600
    
    5
    15

--- CHIA HẾT CHO 8.cpp
+++ CHIA HẾT CHO 8.cpp
@@ -1,27 +1,4 @@
-Cho xâu ký tự S bao gồm các số từ 0 đến 9. Nhiệm vụ của bạn là đếm số các xâu con của S là số chia hết cho 8 và không chia hết cho 3.
-
-Input:
-
-Dòng đầu tiên đưa vào số lượng bộ test T.
-Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một xâu ký tự số S.
-T, S thỏa mãn ràng buộc: 1≤ T ≤100; 0≤ Length(S) ≤103.
-Output:
-
-Đưa ra kết quả mỗi test theo từng dòng.
-Input:
-
-Output:
-
-2
-888
-6564525600
-
-5
-15
-  
-  
-                                                        CODE  
-﻿#include<iostream>
+#include<iostream>
 #include<string>
 using namespace std;
 typedef long long ll;
