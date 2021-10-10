c9c3531bb8dc24d5d6ef6277d3adac4052e9392a
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-10 21:02:21 +0700

    Create  KHAI BÁO LỚP SINH VIÊN - 1.cpp
    Viết chương trình khai báo lớp Sinh Viên gồm các thông tin: Mã SV, Họ tên, Lớp, Ngày sinh và Điểm GPA (dạng số thực float). Hàm khởi tạo không có tham số, gán các giá trị thuộc tính ở trạng thái mặc định (xâu ký tự rỗng, giá trị số bằng 0).
    
    Đọc thông tin 1 sinh viên từ bàn phím (không có mã sinh viên) và in ra màn hình. Trong đó Mã SV được gán là B20DCCN001. Ngày sinh được chuẩn hóa về dạng dd/mm/yyyy.
    
    Input
    
    Gồm 4 dòng lần lượt là Họ tên, Lớp, Ngày sinh và Điểm GPA.
    
    Trong đó:
    
    Họ tên không quá 30 chữ cái.
    Lớp theo đúng định dạng thường dùng ở PTIT
    Ngày sinh có đủ 3 phần ngày tháng năm nhưng có thể chưa đúng chuẩn dd/mm/yyyy.
    Điểm GPA đảm bảo trong thang điểm 4 với 2 nhiều nhất 2 số sau dấu phẩy. 
    Output
    
    Ghi thông tin sinh viên trên 1 dòng, mỗi thông tin cách nhau 1 khoảng trống.
    
    Ví dụ
    
    Input
    
    Output
    
    Nguyen Hoa Binh
    
    D20CQCN04-B
    
    2/2/2002
    
    2
    
    B20DCCN001 Nguyen Van A D20CQCN04-B 02/02/2002 2.00
    
     
    
    Giới hạn thời gian: 2s
    
    Giới hạn bộ nhớ: 65536 Kb
    
    Bài tập này yêu cầu sử dụng hàm main cho sẵn như sau:
    
    C/C++
    int main(){
        SinhVien a();
        nhap(a);
        in(a);
        return 0;

---
+++ Hướng đối tượng C++/KHAI BÁO LỚP SINH VIÊN - 1.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+#include<string>
+#include<iomanip>
+#define a()a;//do de bai yeu cau la a()
+using namespace std;
+class SinhVien
+{
+public:
+	friend void nhap(SinhVien&);
+	friend void in(SinhVien);
+	string getNgay_sinh();
+	SinhVien();
+	~SinhVien();
+
+private:
+	string Ten;
+	string Lop;
+	string Ngay_sinh;
+	float Diem{};
+};
+void nhap(SinhVien& x) {
+	getline(cin, x.Ten);
+	getline(cin, x.Lop);
+	getline(cin, x.Ngay_sinh);
+	cin >> x.Diem;
+	cin.ignore();//xoa truoc khi nhapp thang tiep theo
+}
+//ham lay duoc ngay sinh ra de sua
+string SinhVien::getNgay_sinh() {
+	return this->Ngay_sinh;
+}
+//Ham chuan hoa ngay sinh
+void Chuan_hoa(SinhVien& x) {
+	x.getNgay_sinh().insert(x.getNgay_sinh().begin() + 0, 'feve');
+	/*if (x.getNgay_sinh()[1] == '/') {
+		x.getNgay_sinh().insert(x.getNgay_sinh().begin() + 0, '0');
+	}
+	if (x.getNgay_sinh()[4] == '/') {
+		x.getNgay_sinh().insert(x.getNgay_sinh().begin() + 3, '0');
+	}*/
+	//cout << x.getNgay_sinh()[1] << endl;;
+}
+
+void in(SinhVien x) {
+	//Chuan_hoa(x);
+	if (x.Ngay_sinh[1] == '/') {
+		x.Ngay_sinh.insert(x.Ngay_sinh.begin() + 0, '0');
+	}
+	if (x.Ngay_sinh[4] == '/') {
+		x.Ngay_sinh.insert(x.Ngay_sinh.begin() + 3, '0');
+	}
+	cout << "B20DCCN001" << " Nguyen Van A" << " " << x.Lop << " " << x.Ngay_sinh<<" "<<fixed << setprecision(2) << x.Diem;
+}
+SinhVien::SinhVien()
+{
+}
+
+SinhVien::~SinhVien()
+{
+}
+int main(){
+    SinhVien a();
+    nhap(a);
+    in(a);
+    return 0;
+}
