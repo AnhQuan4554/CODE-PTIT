cf0ba276cffef1e2b5310ec16d46bd9601cd4aa1
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-10 21:01:19 +0700

    dùng friend để chuẩn hóa ngày sinh và họ tên
    Viết chương trình khai báo lớp Sinh Viên gồm các thông tin: Mã SV, Họ tên, Lớp, Ngày sinh và Điểm GPA (dạng số thực float). Hàm khởi tạo không có tham số, gán các giá trị thuộc tính ở trạng thái mặc định (xâu ký tự rỗng, giá trị số bằng 0).
    
    Yêu cầu sử dụng chồng toán tử nhập và xuất để nhập đối tượng sinh viên với cin và in ra đối tượng sinh viên với cout.
    
    Đọc thông tin 1 sinh viên từ bàn phím (không có mã sinh viên) và in ra màn hình. Trong đó Mã SV được gán là B20DCCN001. Ngày sinh được chuẩn hóa về dạng dd/mm/yyyy. Tên được đưa về dạng chuẩn. 
    
    Input
    
    Gồm 4 dòng lần lượt là Họ tên, Lớp, Ngày sinh và Điểm GPA.
    
    Trong đó:
    
    Họ tên không quá 30 chữ cái.
    Lớp theo đúng định dạng thường dùng ở PTIT
    Ngày sinh có đủ 3 phần ngày tháng năm nhưng có thể chưa đúng chuẩn dd/mm/yyyy.
    Điểm GPA đảm bảo trong thang điểm 4 với 2 nhiều nhất 2 số sau dấu phẩy. 
    Output
    
    Ghi thông tin sinh viên trên 1 dòng, mỗi thông tin cách nhau 1 khoảng trống.
    
    Ví dụ
    
    Input
    
    Output
    
    NguyEn hoa BiNH
    
    D20CQCN04-B
    
    2/2/2002
    
    2
    
    B20DCCN001 Nguyen Hoa Binh D20CQCN04-B 02/02/2002 2.00
    
     
    
    Giới hạn thời gian: 2s
    
    Giới hạn bộ nhớ: 65536 Kb
    
    Bài tập này yêu cầu sử dụng hàm main cho sẵn như sau:
    
    C/C++
    int main(){
        SinhVien a();
        cin >> a;
        cout << a;
        return 0;

---
+++ Hướng đối tượng C++/KHAI BÁO LỚP SINH VIÊN - 3.cpp
@@ -0,0 +1,86 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<sstream>
+#define a() a;
+using namespace std;
+class SinhVien
+{
+public:
+	friend ostream& operator <<(ostream &, SinhVien);
+	friend	istream& operator >>(istream &, SinhVien &);
+	friend void Chuan_hoa(SinhVien&);
+	
+	SinhVien();
+	~SinhVien();
+
+private:
+	string Ten;
+	string Lop;
+	string Ngay_sinh;
+	float Diem{};
+};
+//ham sua tung tu trong ten
+void Sua(string &a) {
+	if (a[0] <= 122 && a[0] >= 97) {
+		a[0] -= 32;
+	}
+	for (int i = 1; i < a.size(); i++) {
+		if (a[i] >= 65 && a[i] <= 90) {
+			a[i] += 32;
+		}
+	}
+}
+void Chuan_hoa(SinhVien &a) {
+	//chuan hoa ho ten
+	string es = a.Ten;//chuoi lay ra
+	//xoa di toan bo string a de ti lay vao 
+	a.Ten.erase(a.Ten.begin() + 0, a.Ten.begin() + a.Ten.size());
+	stringstream s(es);
+	string temp;
+	while (s>>temp)
+	{
+		Sua(temp);
+		a.Ten.insert(a.Ten.begin() + a.Ten.size(), temp.begin() + 0, temp.begin() + temp.size());
+		a.Ten.insert(a.Ten.begin() + a.Ten.size(), ' ');//sẽ thùa 1 dấu cách
+	}
+	
+	//chuan hoa ngay sinh
+	if (a.Ngay_sinh[1] == '/') {
+		a.Ngay_sinh.insert(a.Ngay_sinh.begin() + 0, '0');
+	}
+	if (a.Ngay_sinh[4] == '/') {
+		a.Ngay_sinh.insert(a.Ngay_sinh.begin() + 3, '0');
+	}
+}
+istream& operator >>(istream& in, SinhVien& x) {
+	getline(in, x.Ten);
+	getline(in, x.Lop);
+	getline(in, x.Ngay_sinh);
+	in >> x.Diem;
+	
+		return in;
+}
+
+ostream& operator <<(ostream& os, SinhVien x) {
+	//Ham chuan hoa ngay thang nam sinh
+	Chuan_hoa(x);
+	//khong can them dau " " truoc lop vi thua 1 dau cach từ lúc đồng bộ hóa rồi 
+	os << "B20DCCN001" << " " << x.Ten  << x.Lop << " " << x.Ngay_sinh <<" "<< fixed << setprecision(2) << x.Diem;
+	return os;
+}
+
+SinhVien::SinhVien()
+{
+}
+
+SinhVien::~SinhVien()
+{
+}
+int main(){
+    SinhVien a();
+    cin >> a;
+    cout << a;
+    return 0;
+}
