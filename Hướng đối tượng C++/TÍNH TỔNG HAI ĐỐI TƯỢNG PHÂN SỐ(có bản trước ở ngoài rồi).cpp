617fa3977d1d9783e03dd974eedbef7507bc495f
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-10 21:05:07 +0700

    Create TÍNH TỔNG HAI ĐỐI TƯỢNG PHÂN SỐ(có bản trước ở ngoài rồi).cpp
    Viết chương trình xây dựng class Phân số.
    
    Sau đó thực hiện nhập vào hai phân số p và q. Tính tổng p + q, rút gọn và in ra kết quả.
    
    Input
    
    Có bốn số nguyên dương lần lượt là tử số và mẫu số của p rồi đến q. Các giá trị không quá 9 chữ số.
    
    Output
    
    Ghi ra phân số tổng p + q ở dạng tối giản như trong ví dụ
    
    Ví dụ
    
    Input
    
    Output
    
    123 456 12 34
    
    1609/2584
    
     
    
    Giới hạn thời gian: 2s
    
    Giới hạn bộ nhớ: 65536 Kb
    
    Bài tập này yêu cầu sử dụng hàm main cho sẵn như sau:
    
    C/C++
    int main() {
    	PhanSo p(1,1), q(1,1);
    	cin >> p >> q;
    	cout << p + q;
    	return 0;

---
+++ Hướng đối tượng C++/TÍNH TỔNG HAI ĐỐI TƯỢNG PHÂN SỐ(có bản trước ở ngoài rồi).cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+typedef long long ll;
+//ham tim ucln
+ll Uoc(ll x, ll y) {
+	if (y == 0) {
+		return x;
+	}
+	return Uoc(y, x % y);
+}
+class PhanSo
+{
+public:
+	friend ostream& operator<<(ostream& , PhanSo );
+	friend istream& operator>>(istream& , PhanSo& );
+	friend PhanSo operator + (PhanSo, PhanSo);
+	PhanSo(ll, ll);
+	~PhanSo();
+
+private:
+	ll tu;
+	ll mau;
+};
+
+istream& operator>>(istream& is, PhanSo& x) {
+	is >> x.tu >> x.mau;
+	return is;
+}
+ostream& operator<<(ostream& os, PhanSo x) {
+	os << x.tu << "/" << x.mau;
+	return os;
+}
+//Ham cong phan so 
+PhanSo operator + (PhanSo x, PhanSo y) {
+	PhanSo Tong(1, 1);
+	Tong.tu = x.tu * y.mau + y.tu * x.mau;
+	Tong.mau = x.mau * y.mau;
+	//Rut gon phan so
+	ll xx = Uoc(Tong.tu, Tong.mau);
+	Tong.tu = Tong.tu / xx;
+	Tong.mau = Tong.mau / xx;
+	return Tong;
+}
+PhanSo::PhanSo(ll tu, ll mau)
+{
+	this->tu = tu;
+	this->mau = mau;
+}
+
+PhanSo::~PhanSo()
+{
+}
+int main() {
+	PhanSo p(1,1), q(1,1);
+	cin >> p >> q;
+	cout << p + q;
+	return 0;
+}
