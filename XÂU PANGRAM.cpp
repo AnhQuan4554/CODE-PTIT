43ef221ae40adf345c7c1504eba904905582bd8c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-14 16:11:12 +0700

    Create XÂU PANGRAM.cpp
    Một xâu ký tự được gọi là Pangram nếu nó chứa đầy đủ các ký tự từ ‘a’, ..’z’. Cho xâu ký tự S và số K. Hãy xác định xem có thể thực hiện nhiều nhất K phép biến đổi các ký tự để S trở thành Pangram hay không? Mỗi phép biến đổi là một phép thay thế ký tự này bằng một ký tự khác.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào xâu ký tự S; dòng tiếp theo đưa vào số K.
    T, S, K thỏa mãn ràng buộc: 1≤ T ≤100; 0≤ K ≤62; 1≤ Length(S) ≤106.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    qwqqwqeqqwdsdadsdasadsfsdsdsdasasas
    4
    qwqqwqeqqwdsdadsdasadsfsdsdsdasasas
    24
    
    0
    
    1

---
+++ XÂU PANGRAM.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int KT(string a) {
+	int b[1000] = { 0 };
+	int dem = 0;
+	for (int i = 0; i < a.size(); i++) {
+		b[a[i]]++;
+	}
+	for (char i = 'a'; i <= 'z'; i++) {
+		if (b[i] == 0) {
+			dem++;
+		}
+	}return dem;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string a;
+		cin.ignore();
+		int so;
+		getline(cin, a);
+		cin >> so;
+		if (so < KT(a)) {
+			cout << "0";
+		}
+		else {
+			cout << "1";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
