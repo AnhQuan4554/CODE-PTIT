6a909a40a002960af2cdc458a5e6adb36cd66ab2
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-14 09:25:00 +0700

    Update Số lớn nhất của dãy con liên tục.cpp
     SỐ LỚN NHẤT CỦA DÃY CON LIÊN TỤC
    
    Bài làm tốt nhất
    Cho mảng A[] gồm n số nguyên không âm và số k. Hãy tìm số lớn nhất của mỗi dãy con liên tục gồm k phần tử của mảng. Ví dụ với mảng A[] = {1, 2, 3, 1, 4, 5, 2, 3, 6}, K = 3, ta có kết quả 3 3 4 5 5 5 6.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[] và số k; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ k < n ≤107; 0≤ A[i] ≤107;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    9 3
    1 2 3 1 4 5 2 3 6
    10 4
    8 5 10 7 9 4 15 12 90 13
    
    3 3 4 5 5 5 6
    10 10 10 15 15 90 90
    
    =====================================================================CODE=====================================================================

--- Số lớn nhất của dãy con liên tục.cpp
+++ Số lớn nhất của dãy con liên tục.cpp
@@ -1,30 +1,4 @@
- SỐ LỚN NHẤT CỦA DÃY CON LIÊN TỤC
 
-Bài làm tốt nhất
-Cho mảng A[] gồm n số nguyên không âm và số k. Hãy tìm số lớn nhất của mỗi dãy con liên tục gồm k phần tử của mảng. Ví dụ với mảng A[] = {1, 2, 3, 1, 4, 5, 2, 3, 6}, K = 3, ta có kết quả 3 3 4 5 5 5 6.
-
-Input:
-
-Dòng đầu tiên đưa vào số lượng bộ test T.
-Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[] và số k; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
-T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ k < n ≤107; 0≤ A[i] ≤107;
-Output:
-
-Đưa ra kết quả mỗi test theo từng dòng.
-Input:
-
-Output:
-
-2
-9 3
-1 2 3 1 4 5 2 3 6
-10 4
-8 5 10 7 9 4 15 12 90 13
-
-3 3 4 5 5 5 6
-10 10 10 15 15 90 90
-
-=====================================================================CODE=====================================================================
   
   
   #include<iostream>
