21cb54d121f7c6c36cbb1af9cea3085b21073801
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-10-15 11:02:59 +0700

    Test setter
     KHAI BÁO LỚP SINH VIÊN - 2
    
    Bài làm tốt nhất
    Viết chương trình khai báo lớp Sinh Viên gồm các thông tin: Mã SV, Họ tên, Lớp, Ngày sinh và Điểm GPA (dạng số thực float). Hàm khởi tạo không có tham số, gán các giá trị thuộc tính ở trạng thái mặc định (xâu ký tự rỗng, giá trị số bằng 0).
    
    Yêu cầu sử dụng chồng toán tử nhập và xuất để nhập đối tượng sinh viên với cin và in ra đối tượng sinh viên với cout.
    
    Đọc thông tin 1 sinh viên từ bàn phím (không có mã sinh viên) và in ra màn hình. Trong đó Mã SV được gán là B20DCCN001. Ngày sinh được chuẩn hóa về dạng dd/mm/yyyy.
    
    Input
    
    Gồm 4 dòng lần lượt là Họ tên, Lớp, Ngày sinh và Điểm GPA.
    
    Trong đó:
    
    Họ tên không quá 30 chữ cái.
    Lớp theo đúng định dạng thường dùng ở PTIT
    Ngày sinh có đủ 3 phần ngày tháng năm nhưng có thể chưa đúng chuẩn dd/mm/yyyy.
    Điểm GPA đảm bảo trong thang điểm 4 với 2 nhiều nhất 2 số sau dấu phẩy. 
    Output
    
    Ghi thông tin sinh viên trên 1 dòng, mỗi thông tin cách nhau 1 khoảng trống.
    
    Ví dụ
    
    Input
    
    Output
    
    Nguyen Hoa Binh
    
    D20CQCN04-B
    
    2/2/2002
    
    2
    
    B20DCCN001 Nguyen Hoa Binh D20CQCN04-B 02/02/2002 2.00

---
+++ Hướng đối tượng C++/KHAI BÁO LỚP SINH VIÊN - 2.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<fstream>
+using namespace std;
+#include<string>
+#include<iomanip>
+#define a() a;
+class SinhVien
+{
+public:
+	friend istream& operator >> (istream &, SinhVien&);
+	friend ostream& operator <<(ostream &, SinhVien);
+	void setngaysinh( );
+	
+	SinhVien();
+	~SinhVien();
+
+private:
+	string Ten;
+	string Lop;
+	string Ngay_sinh;
+	float Diem{};
+};
+//ham sua ngay sinh
+void SinhVien::setngaysinh() {
+	
+	if (Ngay_sinh[1] == '/') {
+		Ngay_sinh.insert(Ngay_sinh.begin() + 0, '0');
+	}
+	if (Ngay_sinh[4] == '/') {
+		Ngay_sinh.insert(Ngay_sinh.begin() + 3, '0');
+	}
+}
+istream& operator >> (istream & in, SinhVien & x) {
+	getline(in, x.Ten);
+	getline(in, x.Lop);
+	getline(in, x.Ngay_sinh);
+	in >> x.Diem;
+	return in;
+}
+ostream& operator <<(ostream& ot, SinhVien x) {
+	//ham chinh sua ngay sinh
+	x.setngaysinh();
+	ot << "B20DCCN001" << " " << x.Ten << " " << x.Lop << " " << x.Ngay_sinh << " " << fixed << setprecision(2) << x.Diem;
+	return ot;
+}
+SinhVien::SinhVien()
+{
+}
+
+SinhVien::~SinhVien()
+{
+}
+int main(){
+    SinhVien a();
+    cin >> a;
+    cout << a;
+    return 0;
+}
