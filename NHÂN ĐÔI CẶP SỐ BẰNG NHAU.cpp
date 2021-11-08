847d5f4ff6d1704241b577812685f9598c3bd2a2
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-08 21:42:46 +0700

    Create NHÂN ĐÔI CẶP SỐ BẰNG NHAU.cpp
    Cho mảng A[] gồm n số nguyên không âm. Ta gọi phần tử A[i] là hợp lệ nếu A[i]≠0 ngược lại là không hợp lệ. Nhiệm vụ của bạn là hãy sắp đặt lại các phần tử của mảng theo nguyên tắc nếu số A[i+1] (i=0,..,n-2) là số hợp lệ và A[i] = A[i+1] thì nhân đôi A[i] và thiết lập A[i+1] = 0. Sau khi thay đổi, hãy di chuyển các số không hợp lệ vào cuối mảng. Các số hợp lệ phải bảo toàn quan hệ trước sau. Ví dụ với mảng A[] = {2, 2, 0, 4, 0, 8 }thực hiện theo nguyên tắc trên ta được kết quả A[] = { 4, 4, 8, 0, 0, 0}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤105; 1≤ A[i] ≤105;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    6
    
    2  2  0  4  0  8
    
    10
    0  2  2  2  0  6  6  0  0  8
    
    4  4  8  0  0  0
    
    4  2  12  8  0  0  0  0  0  0

---
+++ NHÂN ĐÔI CẶP SỐ BẰNG NHAU.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<vector>
+typedef long long ll;
+void input(vector<ll>& a, ll n) {
+	for (ll i = 0; i < n; i++) {
+		ll x; cin >> x;
+		a.push_back(x);
+	}
+}
+void output(vector<ll>a) {
+	vector <ll>b;
+	for (int i = 0; i < a.size()-1; i++) {
+	
+		if (a[i + 1] != 0 && a[i] == a[i + 1]) {
+			a[i] *= 2;
+			a[i + 1] = 0;
+		}
+	}
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != 0) {
+			cout << a[i] << " ";
+		}
+		else {
+			b.push_back(a[i]);
+		}
+	}
+	for (int i = 0; i < b.size(); i++) {
+		cout << b[i] << " ";
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<ll>a;
+		int n; cin >> n;
+		input(a, n);
+		output(a);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
