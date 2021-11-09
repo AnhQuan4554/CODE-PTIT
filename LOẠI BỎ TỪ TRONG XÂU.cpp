093031957ef0f0355c42f7398c0b449b106193b2
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:38:59 +0700

    Create LOẠI BỎ TỪ TRONG XÂU.cpp
    Viết chương trình cho phép nhập vào một chuỗi và từ cần loại bỏ khỏi chuỗi. Thực hiện loại bỏ từ và in ra kết quả
    
    Trong đó:
    
    INPUT
    
    - Hàng thứ nhất là chuỗi ban đầu
    
    - Hàng tiếp theo là từ cần loại bỏ
    
    OUTPUT
    
    - Chuỗi kết quả
    
    Input
    
    Output
    
    Toi Yeu PTIT
    Toi
    
    Yeu PTIT

---
+++ LOẠI BỎ TỪ TRONG XÂU.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+#include<sstream>
+#include<vector>
+void input(string a, string b) {
+	stringstream s(a);
+	vector<string>in;
+	string temp;
+	while (s>>temp)
+	{
+		if (temp != b) {
+			in.push_back(temp);
+		}
+	}
+	for (int i = 0; i < in.size(); i++) {
+		cout << in[i] << " ";
+	}
+}
+int main() {
+	string a;
+	getline(cin,a);
+	string b; getline(cin,b);
+	input(a, b);
+	system("pause");
+	return 0;
+}
