074177f071105355b4e162bc6c3d371abaf3c61f
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:29:24 +0700

    Create  ƯỚC SỐ NGUYÊN TỐ THỨ K.cpp
    Cho số tự nhiên N. Nhiệm vụ của bạn là hãy đưa ra ước số nguyên tố thứ k của N. Đưa ra -1 nếu không tồn tại ước số thứ k của N. Ví dụ N = 225, k =2 ta có kết quả là 3 vì 225 = 3×3×5×5. Với N = 81, k = 5 ta có kết quả -1 vì 81 = 3×3×3×3.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một bộ đôi N và k.
    T, N thỏa mãn ràng buộc 1≤T≤100; 1≤N, k≤104.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    225 2
    81 5
    
    3
    -1

---
+++ ƯỚC SỐ NGUYÊN TỐ THỨ K.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+#include<cmath>
+int NT(int a)
+{
+	if (a < 2)return 0;
+	for (int i = 2; i <=sqrt(a); i++)
+	{
+		if (a % i == 0)return 0;
+	}return 1;
+}
+void Xuat(long long a,int chiso)
+{	
+	int dem = 0;//vi tri o nho cua b
+	vector<int>b;
+	for (int i = 2; i <= sqrt(a); i++)
+	{;
+		while (a % i == 0&&NT(i)==1)
+		{
+			b.push_back(i);
+			a = a / i;
+			dem++;
+		}
+	}
+	if (a != 1)b.push_back(a);
+	if (chiso >= b.size())cout << "-1";
+	else cout << b[chiso];
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+	int a;
+	cin >> a;
+	int b;
+	cin >> b;
+	int chiso = b - 1;//vi neu dung vecto thi chi so tu 0;
+	Xuat(a,chiso);
+	cout << endl;
+	}
+	
+	system("pause");
+	return 0;
+}
