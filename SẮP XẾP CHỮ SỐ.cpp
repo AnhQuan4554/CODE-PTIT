f971f7a52194d20904397b099fa62440e8be02e3
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 18:03:57 +0700

    Create SẮP XẾP CHỮ SỐ.cpp
    Cho mảng A[] gồm n phần tử. Nhiệm vụ của bạn là đưa ra mảng đã được sắp xếp bao gồm các chữ số của mỗi phần tử trong A[]. Ví dụ A[] = {110, 111, 112, 113, 114 }ta có kết quả là {0, 1, 2, 3, 4}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng tiếp theo là n số A[i] ; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤107; 0≤ A[i] ≤1016.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    3
    131 11 48
    4
    111 222 333 446
    
    1 3 4 8
    1 2 3 4 6

---
+++ SẮP XẾP CHỮ SỐ.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<set>
+#include<vector>
+using namespace std;
+void Nhap(vector<long long>& a, long long n) {
+	for (long long i = 0; i < n; i++) {
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xep_mang(vector<long long>a,set<long long>&b) {
+	for (long long i = 0; i < a.size(); i++) {
+		while (a[i] != 0) {
+			int x = a[i] % 10;
+			b.insert(x);
+			a[i] /= 10;
+		}
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n;
+		cin >> n;
+		vector<long long>a;
+		Nhap(a, n);
+		set<long long>b;
+		Xep_mang(a, b);
+		//sử dụng con trỏ 
+		for (set<long long>::iterator i = b.begin(); i != b.end();i++){
+			cout << *i << " ";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
