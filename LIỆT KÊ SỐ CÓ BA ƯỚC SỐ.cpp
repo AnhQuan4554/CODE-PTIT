c336ad5a59d12bf43cfbb5a66b101576839b8c52
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:29:48 +0700

    Create LIỆT KÊ SỐ CÓ BA ƯỚC SỐ.cpp
    Cho số tự nhiên N. Nhiệm vụ của bạn là hãy liệt kê tất cả các số có đúng ba ước số. Ví dụ n=100, ta có các số 4, 9, 25, 49.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số N.
    T, N thỏa mãn rang buộc 1≤T≤100; 1≤N ≤106.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    50
    
    200
    
    4 9 25 49
    
    4 9 25 49 121 169

---
+++ LIỆT KÊ SỐ CÓ BA ƯỚC SỐ.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+#include<cmath>
+int kt(long long a) {
+	for (long long i = 2; i < sqrt(a); i++) {//khac kt nguyen to 
+		if (a % i == 0)return 0;
+		/*vi neu chia het cho so nho hon can tuc laf nhieu hon
+		3 uoc (vi mac dinh 3 uoc la 1 can so do vaf chinh no)*/
+	}return 1;
+}
+int main() {
+	int t;
+	
+	cin >> t;
+	while (t--) {
+		long long n;
+		cin >> n;
+		for (long long i = 2;; i++) {
+			if (i * i > n) {
+				break;
+			}
+			if (kt(i * i) == 1) {
+			cout << i * i << ' ';
+
+			}
+		}
+		
+		cout << endl;
+	}
+}
