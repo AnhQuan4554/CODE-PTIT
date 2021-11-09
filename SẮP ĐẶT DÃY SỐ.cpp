6b14b3c697e777b7b1beaeb5360b9c0fe61c11ec
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:46:18 +0700

    Create SẮP ĐẶT DÃY SỐ.cpp
    Cho mảng A[] gồm n phần tử. Nhiệm vụ của bạn là hãy sắp đặt lại các phần tử của mảng sao cho A[i] = i. Nếu phần tử A[j] của có giá trị khác j, hãy đưa ghi vào -1. Ví dụ với mảng A[] = {-1,-1,6,1,9, 3, 2, -1, 4, -1} ta có kết quả A[] = {-1, 1, 2, 3, 4, -1, 1, -1, -1, 9}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤107; 1≤ A[i] ≤1018;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    10
    
    -1 -1 6 1 9 3 2 -1 4 -1
    
    6
    0  -3  1  -2  3 - 4
    
    -1 1 2 3 4 -1 6 -1 -1 9
    
    0 1 -1 3 -1 -1

---
+++ SẮP ĐẶT DÃY SỐ.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+#include<vector>
+void Nhap(vector<long long >&a,long long n)
+{
+	for (long long i = 0; i < n; i++)
+	{
+		long long x;
+		cin >> x;
+	a.push_back(x);
+
+	}
+}
+void Xep(vector<long long >&a)
+{
+	vector<long long>b;
+	long long chiso = 0;
+	long long dai = a.size();
+	for (long long i = 0; i < a.size(); i++)
+	{
+		int kt = 0;
+		for (long long j = 0; j < a.size(); j++)
+		{
+			if (a[j] == i)
+			{
+				kt = 1;
+			}
+		}
+		if (kt == 1)b.push_back(i);
+		else b.push_back(-1);
+	}
+	for (long long i = 0; i < b.size(); i++)
+	{
+		cout << b[i] << ' ';
+	}
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		vector<long long>a;
+		long long n;
+		cin >> n;
+		Nhap(a, n);
+		Xep(a);
+		
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
