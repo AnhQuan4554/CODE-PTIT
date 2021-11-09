92cbd8c7e17bed67681e31ed4d9c94974f08ea29
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:41:07 +0700

    Create  TÍNH LŨY THỪA.cpp
    Cho ba số nguyên dương x, y, p. Nhiệm vụ của bạn là tính (xy) %p. Ví dụ với x = 2, y = 3, p = 5 thì (23)%5=3.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp mỗi dòng đưa vào một test. Mỗi test là bộ ba x, y, p được viết cách nhau một vài khoảng trống.
    T, x, y, p thỏa mãn ràng buộc : 1≤T≤100; 1≤x, y≤106; 1≤P≤109+7.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    2 3 5
    
    3 2 4
    
    3
    
    1

---
+++ TÍNH LŨY THỪA.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+#include<vector>
+long long DU(long long x, long long y,long long z)
+{
+	long long tich = 1;
+	for (long long i = 1; i <=y; i++)
+	{
+		tich = (tich * x % z);
+	}return tich;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		long long x, y, z;
+		cin >> x >> y>>z;
+		cout << DU(x, y,z);
+		cout << endl;
+
+	}
+	system("pause");
+	return 0;
+}
