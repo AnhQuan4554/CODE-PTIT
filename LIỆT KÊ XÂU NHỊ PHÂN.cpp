793172143733be2927202412de35c78b18aca69c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:44:57 +0700

    Create LIỆT KÊ XÂU NHỊ PHÂN.cpp
    Cho số tự nhiên N. Hãy đưa ta các xâu nhị phân có độ dài N.
    
     Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số N được viết trên 1 dòng.
    T, N thỏa mãn ràng buộc :1 ≤ T, N ≤ 20.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input
    
    Output
    
    2
    2
    3
    
    00  01  10 11
    000 001 010 011 100 101 110 111

---
+++ LIỆT KÊ XÂU NHỊ PHÂN.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+void Bien_doi(int* a, int n,int &temp) {
+	int i = n-1;
+	while (i >= 1-1 && a[i] == 1) {
+		a[i] = 0;
+		i--;
+	}
+	if (i <1-1) {
+		temp = 0;
+	}
+	else  {
+		a[i] = 1;
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 1-1; i <= n-1; i++) {
+		a[i] = 0;
+	}
+	int temp = 1;
+	while (temp!=0) {
+		for (int i = 1-1; i <= n-1; i++) {
+			cout << a[i];
+		}
+		Bien_doi(a, n, temp);//sau khi che truong hop tat ca cac so deu bang 0 thi dung lai tuc truoc do la n bit 1
+		cout << " ";
+
+	}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
