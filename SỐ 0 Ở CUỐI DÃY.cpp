20b8eddbcd0027f6ac800e262fac9b4c73ba28af
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:46:59 +0700

    Create SỐ 0 Ở CUỐI DÃY.cpp
    Cho mảng A[] gồm n phần tử. Nhiệm vụ của bạn là hãy sắp đặt lại các phần tử của mảng sao cho các số 0 để ở cuối cùng, các phần tử khác không được bảo toàn thứ tự trước sau. Ví dụ với mảng A[] = {1, 2, 0, 0, 0, 3, 6} ta có kết quả A[] = {1, 2, 3, 6, 0, 0, 0}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤107; 0≤ A[i] ≤1018;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    7
    
    1  2 0  0  0  3  6
    
    6
    0  1  0  2  0  3
    
    1  2  3  6  0  0  0
    
    1  2  3  0  0  0

---
+++ SỐ 0 Ở CUỐI DÃY.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+#include<cmath>
+#include<string>
+void Nhap(vector<long long >&a, long long n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xep(vector<long long>& a,vector<long long>&b)
+{
+	long long dem = 0;
+	for (long long i = 0; i < a.size(); i++)
+	{
+		if (a[i] != 0) {
+			cout << a[i] << ' ';
+		}
+		if (a[i] == 0)dem++;
+
+	}
+	while (dem--)
+	{
+		cout << "0" << ' ';
+	}
+
+}
+void Xuat(vector<long long>a)
+{
+	for (long long i = 0; i < a.size(); i++)
+	{
+		cout << a[i] << ' ';
+	}
+}
+int main()
+{
+	
+	long long t;
+	cin >> t;
+	while (t--)
+	{
+		vector<long long>a;
+		vector<long long>b;
+		long long n;
+		cin >> n;
+		Nhap(a, n);
+		Xep(a,b);
+	//	Xuat(b);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
