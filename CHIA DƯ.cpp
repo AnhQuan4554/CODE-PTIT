2d305cb86eeb27f7898f20c4fb47790018b8e440
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:43:25 +0700

    Create CHIA DƯ.cpp
    Cho hai số nguyên dương a và m. Nhiệm vụ của bạn là tìm x nhỏ nhất trong khoảng [0,m-1] sao cho a * x  ≡ 1( mod m). Ví dụ a = 3, m=11 ta tìm được x = 4 vì 4*3%11=1.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp mỗi dòng đưa vào một test. Mỗi test là bộ đôi a, m được viết cách nhau một vài khoảng trống.
    T, a, m thỏa mãn ràng buộc : 1≤T≤100; 1≤a ≤m≤100.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng. Nếu phương trình đồng dư không có nghiệm, hày đưa ra -1
    Input:
    
    Output:
    
    2
    3 11
    10 17
    
    4
    12

---
+++ CHIA DƯ.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+void KQ(long long a,long long b)
+{
+    int kt = 0;
+    for (long long i = 0; i < b; i++)
+    {
+        if ((a * i) % b == 1) {
+            cout << i;
+            kt = 1;
+            break;
+        }
+    }
+    if (kt == 0)cout << "-1";
+}
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a,b;
+        cin >> a>>b;
+        KQ(a,b);
+        cout << endl;
+    }
+    system("pause");
+    return 0;
+}
