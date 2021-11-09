5257017215d0d00c234b1242ad81591b76c21d2e
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 18:05:10 +0700

    Create TRỘN HAI DÃY VÀ SẮP XẾP.cpp
    Cho mảng A[] gồm n phần tử và mảng B[] gồm m phần tử. Nhiệm vụ của bạn là hợp nhất hai mảng A[] và B[] để được một mảng mới đã được sắp xếp. Ví dụ A[] = {10, 5, 15}, B[] = {20, 3, 2} ta có kết quả là C[] = {2, 3, 5, 10, 15, 20}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm ba dòng: dòng đầu tiên đưa vào n, m tương ứng với số phần tử của mảng A[] và B[]; dòng tiếp theo là n số A[i] ; dòng cuối cùng là m số B[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, m, A[i], B[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, m ≤106; -108≤ A[i] ≤108.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    1
    
    3 3
    
    10 5 15
    
    20 3 2
    
    2 3 5 10 15 20

---
+++ TRỘN HAI DÃY VÀ SẮP XẾP.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void Nhap(vector<long long>&a,int n)
+{
+	for (long long i = 0; i < n; i++)
+	{
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xap_xep(vector<long long>& a) {
+	for (long long i = 0; i < a.size()-1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+			if (a[i] > a[j]) {
+				long long x = a[i];
+				a[i] = a[j];
+				a[j] = x;
+			}
+		}
+	}
+}
+void Tron(vector<long long>a,vector<long long>&b)
+{
+	for (long long i = 0; i < a.size(); i++) {
+		b.push_back(a[i]);
+	}
+}
+void Xuat(vector<long long>b) {
+	for (long long i = 0; i < b.size(); i++) {
+		cout << b[i] << ' ';
+	}
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n,m;
+		vector<long long >a;
+		vector<long long >b;
+		cin >> n>>m;
+		Nhap(a, n);
+		Nhap(b, m);
+		Tron(a, b);
+		Xap_xep(b);
+		Xuat(b);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
