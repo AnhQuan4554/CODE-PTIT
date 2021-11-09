0f13d58e356f93fdf303a333ceaee08f017bcf42
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 18:28:36 +0700

    Create BỔ SUNG PHẦN TỬ.cpp
    Cho mảng A[] gồm n số nguyên dương. Gọi L, R là max và min các phần tử của A[]. Nhiệm vụ của bạn là tìm số phần tử cần thiết cần thêm vào mảng để mảng có đầy đủ các số trong khoảng [L, R]. Ví dụ A[] = {5, 7, 9, 3, 6, 2 } ta nhận được kết quả là 2 tương ứng với các số còn thiếu là 4, 8.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n, tương ứng với số phần tử của mảng A[]; dòng tiếp theo là n số A[i] ; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, A[i] ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5
    4 5 3 8 6
    3
    2 1 3
    
    1
    0

---
+++ BỔ SUNG PHẦN TỬ.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+#include<sstream>
+#include<vector>
+#include<algorithm>
+void input(vector<int>&a,int n) {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		a.push_back(x);
+	}
+}
+void out(vector<int>a) {
+	int dem = 0;
+	for (int i = 0; i < a.size() - 1; i++) {
+		if (a[i] + 1 < a[i + 1]) {
+			for (int j = a[i] + 1; j < a[i + 1]; j++) {
+				dem++;
+			}
+		}
+	}cout << dem;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<int>a;
+		int n; cin >> n;
+		input(a, n);
+		sort(a.begin(), a.end());
+		
+		out(a);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
