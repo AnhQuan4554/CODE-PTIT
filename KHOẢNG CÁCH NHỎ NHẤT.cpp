3bc97ba2a2fcb56eb6e788a5e9803511f1b87138
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:58:47 +0700

    khoảng cách nên luôn +
    Cho mảng A[] gồm n số chưa được sắp xếp. Hãy tìm Min(A[i]-A[j]) : i ≠j và i, j =0, 1, 2, .., n-1. Ví dụ với A[] = {1, 5, 3, 19, 18, 25} ta có kết quả là 1 = 19-18. với A[] = {1, 19, -4, 31, 28, 35, 100} ta có kết quả là 3 = 31-28.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n; dòng tiếp theo là n số A[i] của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤103; -103≤ A[i]  ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5
    2 4 5 7 9
    10
    87 32 99 75 56 43 21 10 68 49
     
    
    1
    6

---
+++ KHOẢNG CÁCH NHỎ NHẤT.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void Nhap(vector<int>&a,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+int KQ(vector<int>a)
+{
+	int khoang_cach = abs(a[0] - a[1]);
+	for (int i = 0; i < a.size() - 1; i++) {
+		for (int j = i + 1; j < a.size(); j++) {
+			if (abs(a[i] - a[j]) < khoang_cach) {
+				khoang_cach = abs(a[i] - a[j]);
+			}
+		}
+	}return khoang_cach;
+
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int>a;
+		Nhap(a, n);
+		cout << KQ(a) << endl;
+	}
+	system("pause");
+	return 0;
+}
