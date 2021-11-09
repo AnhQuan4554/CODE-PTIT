79242c69cf4e99fe61a409c4ae2a95f13ac933b9
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:40:21 +0700

    Create  LIỆT KÊ SỐ NGUYÊN TỐ - 1.cpp
    Viết chương trình cho phép nhập vào hai số nguyên dương và tìm tất cả các số nguyên tố nằm trong khoảng đó. 
    
    Input
    
    Chỉ có 2 số nguyên dương a và b (không quá 106)
    
    Output
    
    Ghi ra lần lượt các số nguyên tố trong khoảng. Cách nhau một khoảng trống. 
    
    Ví dụ
     
    
    Input
    
    Output
    
    10 50
    
    11 13 17 19 23 29 31 37 41 43 47

---
+++ LIỆT KÊ SỐ NGUYÊN TỐ - 1.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+#include<cmath>
+#include<string>
+int NT(long long a)
+{
+	if (a < 2)return 0;
+	else
+	{
+		for (long long i = 2; i <= sqrt(a); i++)
+		{
+			if (a % i == 0)return 0;
+		}return 1;
+	}
+}
+void Xu_li(long long a, long long b)
+{
+	long long start = a < b ? a : b;
+	long long end = a > b ? a : b;
+	for (long long i = start; i <= end; i++)
+	{
+		if (NT(i) == 1)cout << i << ' ';
+	}
+}
+int main()
+{
+	
+	long long a, b;
+	cin >> a >> b;
+	Xu_li(a, b);
+	system("pause");
+	return 0;
+}
