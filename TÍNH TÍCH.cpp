bc991224f54c7e20cd1f9661b0a9079f1db52085
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 18:04:40 +0700

    Create TÍNH TÍCH.cpp
    Cho mảng A[] gồm n phần tử và mảng B[] gồm m phần tử. Nhiệm vụ của bạn là tìm tích giữa phần tử lớn nhất của mảng A[] và phần tử nhỏ nhất của mảng B[]. Ví dụ A[] = {5, 7, 112, 9, 3, 6, 2 }, B[] = {1, 2, 6, -1, 0, 9} ta có kết quả là -9 = 9*(-1).
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm ba dòng: dòng đầu tiên đưa vào n, m tương ứng với số phần tử của mảng A[] và B[]; dòng tiếp theo là n số A[i] ; dòng cuối cùng là m số B[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, m, A[i], B[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, m ≤106; -108≤ A[i] ≤108.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    6 6
    5 7 9 3 6 2
    1 2 6 -1 0 9
    6 6
    
    1 4 2 3 10 2
    4 2 6 5 2 9
    
    -9
    20

---
+++ TÍNH TÍCH.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void Nhap(vector<long long>&a,int n)
+{
+	for (long long i = 0; i < n; i++)
+	{
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xap_xep(vector<long long>& a) {
+	for (long long i = 0; i < a.size()-1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+			if (a[i] > a[j]) {
+				long long x = a[i];
+				a[i] = a[j];
+				a[j] = x;
+			}
+		}
+	}
+}
+long long KQ(vector<long long>a,vector<long long>b)
+{
+	return a[a.size() - 1] * b[0];
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n,m;
+		vector<long long >a;
+		vector<long long >b;
+		cin >> n>>m;
+		Nhap(a, n);
+		Nhap(b, m);
+		Xap_xep(a);
+		Xap_xep(b);
+		cout << KQ(a,b);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
