b98591ba9f7a3864133ca5f192258bc6912264c6
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 18:03:07 +0700

    Create ĐẾM CẶP PHẦN TỬ CÓ TỔNG BẰNG K.cpp
    Cho mảng A[] gồm n phần tử và số k. Đếm tất cả các cặp phần tử của mảng có tổng bằng k. Ví dụ A[] = {1, 5, 3, 4, 2 }, k = 7 ta có kết quả là 2 cặp (3, 4), (5, 2).
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[] và k; dòng tiếp theo là n số A[i] của mảng A[]các số được viết cách nhau một vài khoảng trống.
    T, n, k, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤100; 0≤ k ≤100, 0≤ A[i] ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5 0 
    1 5 4 1 2
    3 2
    
    1 1 1
    
    0
    3

---
+++ ĐẾM CẶP PHẦN TỬ CÓ TỔNG BẰNG K.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void Nhap(vector<int>&a,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+int xap_xep(vector<int>&a,int k)
+{
+	int dem = 0;
+	for (int i = 0; i < a.size()-1; i++) {
+		for (int j = i + 1; j<a.size(); j++) {
+			if (a[i] + a[j] == k) {
+				dem++;
+			}
+		}
+	}
+	return dem;
+}
+void Xuat(vector<int>a,int n) {
+	
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i] << ' ';
+	}
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int k;
+		cin >> k;
+		vector<int>a;
+		Nhap(a, n);
+		cout << xap_xep(a, k);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
