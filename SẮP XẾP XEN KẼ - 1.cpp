97bc89dbcc9de5866cb47c4591bc03c813457aaa
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:59:25 +0700

    Create SẮP XẾP XEN KẼ - 1.cpp
    Cho mảng A[] gồm n số nguyên khác nhau. Hãy đưa ra các phần tử của mảng theo khuôn dạng lớn nhất, nhỏ nhất, lớn thứ hai, nhỏ thứ 2, … Ví dụ với A[] = {9, 7, 12, 8, 6, 5} ta đưa ra : 12, 5, 9, 6, 8, 7.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n; dòng tiếp theo là n số A [i] của mảng A [];các số được viết cách nhau một vài khoảng trống.
    T, n thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    7
    7 1 2 3 4 5 6
    8
    1 6 9 4 3 7 8 2
    
    7 1 6 2 5 3 4
    9 1 8 2 7 3 6 4

---
+++ SẮP XẾP XEN KẼ - 1.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void Nhap(vector<int>&a,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void xap_xep(vector<int>&a)
+{
+	for (int i = 0; i < a.size()-1; i++) {
+		for (int j = i + 1; j<a.size(); j++) {
+			if (a[i] > a[j]) {
+				int x = a[i];
+				a[i] = a[j];
+				a[j] = x;
+			}
+		}
+	}
+
+}
+void Xuat(vector<int>a,int n) {
+	
+	int end = (a.size() % 2 == 0) ? a.size() / 2 : (a.size() / 2 + 1);
+	for (int i = 0; i < end; i++) {
+		if (a[a.size() - 1 - i] == a[i]) {
+			cout << a[i];
+		}
+		else {
+		cout << a[a.size() - 1 - i] << ' ' <<a[i] <<' ';
+		}
+
+	}
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int>a;
+		Nhap(a, n);
+		xap_xep(a);
+		Xuat(a,n);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
