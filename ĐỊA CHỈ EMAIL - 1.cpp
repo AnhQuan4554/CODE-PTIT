06bfc714e5d2183796f022af337f90d325b774e4
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:39:29 +0700

    Create ĐỊA CHỈ EMAIL - 1.cpp
    Địa chỉ email của cán bộ PTIT được cấp theo nguyên tắc ghép tên với chữ cái đầu tiên của họ và tên đệm. Viết chương trình cho phép tạo các địa chỉ email theo tên cán bộ
    
    Input
    
    Chỉ có một xâu ký tự họ tên độ dài không quá 100. 
    
    Output
    
    Ghi ra địa chỉ email theo mẫu trong ví dụ. 
    
    Ví dụ
    
    Input
    
    Output
    
    Nguyen vAn nAM
    
    namnv@ptit.edu.vn

---
+++ ĐỊA CHỈ EMAIL - 1.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+#include<cmath>
+#include<string>
+void Xu_li(string a)
+{
+	string b;
+	int dem = 0;
+	for (int i = a.length() - 1; i >= 0; i--)
+	{
+		if (a[i] == ' ')break;
+		if (a[i] >= 65 && a[i] <= 90)a[i] = a[i] + 32;
+		b.insert(b.begin() + 0, a[i]);//bo tu cuoi vao trong string b
+		a.erase(a.begin() + i);
+	}
+	for (int i = 0; i < a.length(); i++)
+	{
+		int trung[100];
+		int dem = 0;
+		while (a[i] != ' ')
+		{
+			trung[dem] = a[i];
+			i++;
+			dem++;
+		}trung[dem] = '\0';
+		if (trung[0] >= 65 && trung[0] <= 90)trung[0] = trung[0] + 32;
+		b.insert(b.begin() + (b.length()), trung[0]);
+	}
+	for (int i = 0; i < b.length(); i++)
+	{
+		cout << b[i];
+	}cout<<"@ptit.edu.vn";
+}
+int main()
+{
+	
+	string a;
+	getline(cin, a);
+	Xu_li(a);
+	system("pause");
+	return 0;
+}
