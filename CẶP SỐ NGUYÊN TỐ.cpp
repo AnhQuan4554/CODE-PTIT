c97ee99d58077ab799331655c229e0e56936fb5e
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-09 17:27:09 +0700

    Create CẶP SỐ NGUYÊN TỐ.cpp
    Cho số nguyên dương N>2. Hãy đưa ra cặp số nguyên tố p, q đầu tiên tìm được có tổng đúng bằng N. Ví dụ N = 6 ta có cặp số nguyên tố đầu tiên là 3 + 3 =6.
    
    Trong trường hợp không tìm thấy đáp án thì không in ra gì cả.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số chẵn N.
    T, N thỏa mãn ràng buộc : 1≤T≤100; 4≤N≤10000.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng. Nếu không có cặp số nào thỏa mãn thì không in ra gì cả. 
    Input:
    
    Output:
    
    2
    74
    1024
    
    3  71
    3  1021

---
+++ CẶP SỐ NGUYÊN TỐ.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<cmath>
+int NT(long long a)
+{
+	if (a < 2)return 0;
+	else
+	{
+		for (long long i = 2; i <= sqrt(a); i++)
+		{
+			if (a % i == 0)return 0;
+		}return 1;
+	}
+}
+void Xuat(long long a)
+{
+	int kt = 0;
+	for (long long i = 2; i <= a/2; i++)
+	{
+		if (NT(i) == 1 && NT(a - i) == 1) {
+			cout << i << " " << a - i << endl;
+			kt = 1;
+			break;
+		}
+	}
+	
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		long long a;
+		cin >> a; Xuat(a);
+	}
+	system("pause");
+	return 0;
+}
