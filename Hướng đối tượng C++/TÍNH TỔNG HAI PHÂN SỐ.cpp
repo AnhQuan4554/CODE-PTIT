4e2360688fe03dde45e8e4b8af2a5d58f21b6162
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:59:36 +0700

    Create TÍNH TỔNG HAI PHÂN SỐ.cpp
    Viết chương trình xây dựng cấu trúc Phân số gồm hai phần tử là tử số và mẫu số. Các giá trị đều nguyên dương và không quá 9 chữ số.
    
    Sau đó thực hiện nhập vào hai phân số p và q. Tính tổng p + q, rút gọn và in ra kết quả.
    
    Input
    
    Có bốn số nguyên dương lần lượt là tử số và mẫu số của p rồi đến q.
    
    Output
    
    Ghi ra phân số tổng p + q ở dạng tối giản như trong ví dụ
    
    Ví dụ
    
    Input
    
    Output
    
    123 456 12 34
    
    1609/2584
    int main() {
    	struct PhanSo p,q;
    	nhap(p); nhap(q);
    	PhanSo t = tong(p,q);
    	in(t);
    	return 0;
    }

---
+++ Hướng đối tượng C++/TÍNH TỔNG HAI PHÂN SỐ.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+long long Uoc(long long a, long long b) {
+	if (b == 0) {
+		return a;
+	}
+	return Uoc(b, a % b);
+}
+struct PhanSo {
+	long long tu;
+	long long mau;
+};
+typedef struct PhanSo PS;
+void nhap(PS& p) {
+	cin >> p.tu;
+	cin >> p.mau;
+}
+void Rut_gon(PS& p) {
+	long long x = Uoc(p.tu, p.mau);
+	p.tu = p.tu / x;
+	p.mau = p.mau / x;
+}
+PS tong(PS& p, PS& q) {
+	PS t;
+	/*Rut_gon(p);
+	Rut_gon(q);*/
+	if (p.mau == q.mau) {
+		t.tu = p.tu + q.tu;
+		t.mau = p.mau;
+	}
+	else {
+		t.tu = p.tu * q.mau + q.tu * p.mau;
+		t.mau = p.mau * q.mau;
+	}
+	return t;
+}
+void in(PS t) {
+	Rut_gon(t);
+	cout << t.tu << "/" << t.mau;
+}
+int main() {
+	struct PhanSo p,q;
+	nhap(p); nhap(q);
+	PhanSo t = tong(p,q);
+	in(t);
+	return 0;
+}
