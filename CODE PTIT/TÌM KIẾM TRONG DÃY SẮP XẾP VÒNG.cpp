8261ea9003066a6b8c2bfe13ddcbc0fdc20ab641
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:18:38 +0700

    Create TÌM KIẾM TRONG DÃY SẮP XẾP VÒNG.cpp
    Một mảng được sắp được chia thành hai đoạn tăng dần được gọi là mảng sắp xếp vòng. Ví dụ mảng A[] = { 5, 6, 7, 8, 9, 10, 1, 2, 3, 4} là mảng sắp xếp vòng. Cho mảng A[] gồm n phần tử, hãy tìm vị trí của phần tử x trong mảng A[].
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào n và x; dòng tiếp theo đưa vào n số A[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i], x  thỏa mãn ràng buộc: 1≤T≤100; 1≤N, x, A[i] ≤107.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    10 3
    5 6 7 8 9 10 1 2 3 4
    10 3
    1 2 3 4 5 6 7 8 9 10
    
    9
    3

---
+++ CODE PTIT/TÌM KIẾM TRONG DÃY SẮP XẾP VÒNG.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+void Nhap(vector<long long>&a, int n) {
+	for (long long i = 0; i < n; i++) {
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xep(vector<long long>&a) {
+	for (long long i = 0; i < a.size()-1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+					if (a[i] < a[j]) {
+			long long z = a[i];
+			a[i] = a[j];
+			a[j] = z;
+		}
+		}
+	}
+}
+void Xuat(vector<long long>a,long long so_luong) {
+	for (int i = 0; i<a.size(); i++) {
+		if (a[i] == so_luong) {
+			cout <<i+1;
+			break;
+		}
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<long long>a;
+		long long n, so_luong;
+		cin >> n >> so_luong;
+		Nhap(a, n);
+		//Xep(a);
+		Xuat(a, so_luong);
+		cout << endl;
+		}
+	system("pause");
+	return 0;
+}
+
+
