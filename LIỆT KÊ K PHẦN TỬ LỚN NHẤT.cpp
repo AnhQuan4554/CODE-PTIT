af9e6fb308b72389e67e637f8e9fc4198f8510f9
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:24:00 +0700

    Create LIỆT KÊ K PHẦN TỬ LỚN NHẤT.cpp
    Cho mảng A[] gồm n phần tử, hãy tìm k phần tử lớn nhất của mảng. Các phần tử được đưa ra theo thứ tự giảm dần.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào N và K; dòng tiếp theo đưa vào n số A[i]; các số được viết cách nhau một vài khoảng trống.
    T, N, K, A[i] thỏa mãn ràng buộc: 1≤T≤100; 1≤K < N ≤103, 1≤A[i] ≤106.
    Output:
    
    Đưa ra kết quả mỗi bộ test trên một dòng.
    Input:
    
    Output:
    
    2
    5 3
    
    10 7 9 12 6
    
    6 2
    
    9 7 12 8 6 5
     
    
    12 10 9
    12 9

---
+++ LIỆT KÊ K PHẦN TỬ LỚN NHẤT.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+void Nhap(vector<long long>&a, int n) {
+	for (long long i = 0; i < n; i++) {
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xep(vector<long long>&a) {
+	for (long long i = 0; i < a.size()-1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+					if (a[i] < a[j]) {
+			long long z = a[i];
+			a[i] = a[j];
+			a[j] = z;
+		}
+		}
+	}
+}
+void Xuat(vector<long long>a,long long so_luong) {
+	for (int i = 0; i < so_luong; i++) {
+		cout << a[i] << ' ';
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<long long>a;
+		long long n, so_luong;
+		cin >> n >> so_luong;
+		Nhap(a, n);
+		Xep(a);
+		Xuat(a, so_luong);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
