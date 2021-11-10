2913256606fdf2501bf6b9a58a008807b800369d
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 22:14:54 +0700

    Create SẮP XẾP THEO CHỮ SỐ.cpp
    Cho mảng A[] gồm n số nguyên không âm. Hãy sắp đặt lại các phần tử trong mảng sao cho khi nối các số lại với nhau ta được một số lớn nhất. Ví dụ với mảng A[] = {54, 546, 548, 60 } thực hiện theo nguyên tắc trên ta được số lớn nhất là  6054854654.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤105; 1≤ A[i] ≤105;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    4
    
    54  546  548  60
    
    8
    1  34  3  98  9  76  45  4
    
    6054854654
    
    998764543431

---
+++ SẮP XẾP THEO CHỮ SỐ.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+#include<vector>
+#include<algorithm>
+bool cmp(string a, string b) {
+	return a+b > b+a;//vd 9 98 thif tong theo kieu string 9+98>98+9
+	//tong string la noi chuoi nen 998>989
+}
+void input(vector<string>& a, int n) {
+	for (int i = 0; i < n; i++) {
+		string x;
+		cin >> x;//nhap so nen ko can getline 
+		a.push_back(x);
+	}
+}
+void output(vector<string>a) {
+	for (auto i : a) {
+		cout << i;
+	}
+}
+int main() {
+	int t;
+	cin >> t;//ko co getline nen ko can cin.ignore
+	
+	while (t--) {
+		vector<string>a;
+		int n; cin >> n;
+		input(a, n);
+		sort(a.begin(), a.end(), cmp);
+		output(a);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
