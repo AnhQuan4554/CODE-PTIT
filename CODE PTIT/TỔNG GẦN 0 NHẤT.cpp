34323387fa2805a330e21d9121a0c2a5859bd730
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:19:51 +0700

    Create TỔNG GẦN 0 NHẤT.cpp
    Cho mảng A[] gồm n phần tử, hãy tìm cặp phần tử có tổng gần nhất so với 0.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào n là số phần tử của mảng A[]; dòng tiếp theo đưa vào n số A[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤T≤100; 2≤N ≤103, -106≤A[i] ≤106.
    Output:
    
    Đưa ra tổng gần nhất với 0 của cặp phần tử.
    Input:
    
    Output:
    
    2
    3
    -8 -66 -60 
    6
    -21 -67 -37 -18 4 -65
    
    -68
    -14

---
+++ CODE PTIT/TỔNG GẦN 0 NHẤT.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+void Nhap(vector<long long>&a,int n)
+{
+	for (long long i = 0; i < n; i++)
+	{
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+long long KQ(vector<long long>a)
+{
+	long long tong = abs(a[0] + a[1]);
+	for (long long i = 0; i < a.size() - 1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+			if (abs(a[i] + a[j]) < abs(tong)) {
+				tong = a[i] + a[j];
+			}
+		}
+	}return tong;
+
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n;
+		vector<long long >a;
+		cin >> n;
+		Nhap(a, n);
+		cout << KQ(a);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
