14709ffed25a1c0a19e515cc2932f887ae2c1293
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:27:22 +0700

    Create BIÊN CỦA MA TRẬN.cpp
    Cho ma trận vuông A[N][N]. Hãy in các phần tử thuộc vùng biên.
    
    
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào N là cấp của ma trận A[N][N]; dòng tiếp theo đưa vào N×N số A[i][j] ; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i][j] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ N ≤100; 1≤ A[i][j] ≤150.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    4
    1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
    3
    45 48 54 21 89 87 70 78 15
    
    1 2 3 4 
    5     8 
    1     4 
    5 6 7 8 
    45 48 54 
    21   87 
    70 78 15

---
+++ BIÊN CỦA MA TRẬN.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+void Nhap(int** a,int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+	}
+}
+void Xuat(int** a, int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i > 0 && i < n - 1) {
+				if (j == 0 || j == n - 1) {
+					 cout <<a[i][j]<<' ';
+				}
+				else if (j >0 && j <n-1) {
+					a[i][j] = ' ';
+					char x = a[i][j];
+					cout << x <<' ';
+				}
+				
+			}
+			else {
+				cout << a[i][j] << ' ';
+			}
+		}cout << endl;
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	int n;
+	cin >> n;
+	int** a = new int* [n];
+	for (int i = 0; i < n; i++) {
+		//khai bao so cot
+		a[i] = new int[n];
+	}
+	Nhap(a, n);
+	Xuat(a, n);
+	cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
