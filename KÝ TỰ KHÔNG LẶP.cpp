3e2c0ea3cabc16de96840cf9ba93484fe6290b91
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:36:05 +0700

    Create KÝ TỰ KHÔNG LẶP.cpp
    Cho xâu ký tự S. Nhiệm vụ của bạn là in ra tất cả các ký tự không lặp khác nhau trong S. Ví dụ S =”ABCDEABC” ta nhận được kết quả là “DE”.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một xâu ký tự S được viết trên một dòng.
    T, S thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ Length(S)≤105.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng. In ra theo thứ tự bảng chữ cái.
    Input:
    
    Output:
    
    2
    ABCDEAABC
    ABC
    
    DE
    ABC

---
+++ KÝ TỰ KHÔNG LẶP.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+void KQ(string a) {
+	int b[100] = { 0 };
+	for (long long i = 0; i < a.size()-1; i++) {
+			long long tong = 0;
+		for (long long j = i + 1; j < a.size(); j++) {
+			if (a[i] == a[j]) {
+				b[a[i]]++;
+			}
+		}
+	}
+	for (long long j = 0; j < a.size(); j++) {
+		if (b[a[j]]==0) {
+			cout << a[j];
+		}
+	}
+			
+	}
+
+int main() {
+	int t;
+	cin >> t;
+	cin.ignore();
+	while (t--) {
+		string a;
+		getline(cin, a);
+		//cout << KT(a);
+		KQ(a);
+		cout << endl;
+	}
+		system("pause");
+		return 0;
+}
