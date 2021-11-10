460c590f70a8686e0e7b21c8307c5b6a97662c25
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:19:13 +0700

    Create SỐ NHỎ NHẤT VÀ NHỎ THỨ HAI.cpp
    Cho mảng A[] gồm n phần tử, hãy đưa ra số nhỏ nhất và số nhỏ thứ hai của mảng. Nếu không có số nhỏ thứ hai, hãy đưa ra -1.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào n là số phần tử của mảng A[]; dòng tiếp theo đưa vào n số A[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤T≤100; 1≤N, A[i] ≤107.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    10
    5 6 7 8 9 10 1 2 3 4
    5
    1 1 1 1 1
    
    1  2
    -1

---
+++ CODE PTIT/SỐ NHỎ NHẤT VÀ NHỎ THỨ HAI.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+#include<vector>
+void Nhap(vector<long long>&a,int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xuat(vector<long long>a)
+{
+	
+	for (long long i = 0; i < a.size()-1; i++)
+	{
+		for (long long j = i + 1; j < a.size(); j++)
+		{
+			if (a[i] > a[j])
+			{
+				long long x = a[i];
+				a[i] = a[j];
+				a[j] = x;
+			}
+		}
+	}
+	int kt = 0;
+	if (a[0] == a[ 1]) {
+			cout << "-1";
+			kt = 1;
+		
+		}
+	if (kt != 1)
+	{
+		cout << a[0] << ' ' << a[1];
+	}
+	
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		vector<long long>a;
+		int n;
+		cin >> n;
+		Nhap(a, n);
+		Xuat(a);
+		cout << endl;
+
+	}
+	system("pause");
+	return 0;
+}
