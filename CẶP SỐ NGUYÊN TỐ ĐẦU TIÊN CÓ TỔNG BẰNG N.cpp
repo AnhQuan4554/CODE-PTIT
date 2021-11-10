f52c8f1f9e632a7b6e238aa545a382648a0b63b4
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:25:45 +0700

    Create CẶP SỐ NGUYÊN TỐ ĐẦU TIÊN CÓ TỔNG BẰNG N.cpp
    Cho số tự nhiên N. Hãy tìm cặp số nguyên tố đầu tiên có tổng là N. Nếu không tồn tại cặp số nguyên tố có tổng bằng N, hãy đưa ra -1.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm là một số N được ghi trên một dòng.
    T, N thỏa mãn ràng buộc: 1≤T≤100; 1≤ N ≤106.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    4
    
    8
    
    2 2
    
    3 5

---
+++ CẶP SỐ NGUYÊN TỐ ĐẦU TIÊN CÓ TỔNG BẰNG N.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+#include<cmath>
+int Nguyen_to(long long a) {
+	if (a < 2) {
+		return 0;
+	}
+	for (long long i = 2; i <= sqrt(a); i++) {
+		if (a % i == 0) {
+			return 0;
+		}
+	}return 1;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n;
+		cin >> n;
+		int check = 0;
+		for (long long i = 2; i <= sqrt(n)+1000; i++) {
+			if (Nguyen_to(i) && Nguyen_to(n - i)) {
+				cout << i << " " << n - i << endl;
+				check = 1;
+				break;
+			}
+		}
+		if (check == 0) {
+			cout << "-1\n";
+		}
+	}
+	system("pause");
+	return 0;
+}
