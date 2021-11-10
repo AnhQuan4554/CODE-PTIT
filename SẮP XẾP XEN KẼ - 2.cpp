13cf150c6c55c3810b2c5666ca6ed92669bcb266
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 22:12:01 +0700

    Create SẮP XẾP XEN KẼ - 2.cpp
    Cho mảng A[] gồm n số nguyên dương. Nhiệm vụ của bạn là hãy sắp đặt lại các phần tử của mảng sao theo nguyên tắc số lớn nhất ở đầu, số nhỏ nhất thứ 2, số lớn thứ nhì ở vị trí tiếp theo, số nhỏ thứ nhì ở vị trí kế tiếp …. Ví dụ với mảng A[] = {1, 7, 3, 5, 9 } ta được mảng được sắp A[] = { 9, 1, 7, 3, 5}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤103; 1≤ A[i] ≤103;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    4
    
    1  5  2  7
    
    6
    1  3  2  7  9  8
    
    7  1  5  2
    
    9  1  8  2  7  3

---
+++ SẮP XẾP XEN KẼ - 2.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+void Nhap(vector<long long>&a, int n) {
+	for (int i = 0; i < n; i++) {
+		long long x;
+		cin >> x;
+		a.push_back(x);
+
+	}
+}
+void Sap_xep(vector<long long>& a) {
+	for (long long i = 0; i < a.size()-1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+			if (a[i] < a[j]) {
+				long long temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+void Xuat(vector<long long>a) {
+	for (int i = 0; i <= (a.size() - 1) / 2; i++) {
+		if (a[i] == a[a.size() - 1 - i]&&i== a.size() - 1 - i) {
+			cout << a[i] << " ";
+		}
+		else {
+			cout << a[i] << " " << a[a.size() - 1 - i]<<" ";
+		}
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<long long>a;
+		long long n;
+		cin >> n;
+		Nhap(a, n);
+		Sap_xep(a);
+		Xuat(a);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
