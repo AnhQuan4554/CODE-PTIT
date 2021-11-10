e083daba8ac3f0f98079a26757c8e9f8c049671f
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:16:27 +0700

    Create TÌM KIẾM NHỊ PHÂN.cpp
    Cho mảng A[] gồm n phần tử đã được sắp xếp. Hãy đưa ra 1 nếu X có mặt trong mảng A[], ngược lại đưa ra -1.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào n, X là số các phần tử của mảng A[] và số X cần tìm; dòng tiếp theo đưa vào n số A[i] (1≤i≤n) các số được viết cách nhau một vài khoảng trống.
    T, n, A, X thỏa mãn ràng buộc: 1≤T≤100; 1≤N, X, A[i] ≤106.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2 
    5  16
    9  7  2  16  4
    7  98
    1  22  57  47  34  18  66
    
    1
    -1

---
+++ CODE PTIT/TÌM KIẾM NHỊ PHÂN.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+void Nhap(vector<long long>&a, int n) {
+	for (long long i = 0; i < n; i++) {
+		long long x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+int KT(vector<long long>a,long long so) {
+	
+	for (long long i = 0; i < a.size(); i++) {
+		if (a[i] == so) {
+			return 1;
+		}
+	}return -1;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long n, a;
+		cin >>n>> a;
+		vector<long long>b;
+		Nhap(b, n);
+		cout << KT(b, a);
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
