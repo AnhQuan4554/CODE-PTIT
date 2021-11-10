554610676801f747424faa919e5500aa042a450c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:33:18 +0700

    Create ĐẦU CUỐI GIỐNG NHAU.cpp
    Cho xâu ký tự S. Hãy đếm tất cả các xâu con của S có ký tự đầu và ký tự cuối giống nhau. Ví dụ với xâu “aba” ta có 4 xâu con bao gồm: “a”, “b”, “a”, “aba”.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một xâu ký tự S được viết trên một dòng.
    T, S thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ k ≤26;1≤ Length(S)≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    abcab
    
    aba
    
    7
    4

---
+++ ĐẦU CUỐI GIỐNG NHAU.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+long long KQ(string a) {
+	long long tong = 0;
+	for (long long i = 0; i < a.size()-1; i++) {
+		for (long long j = i + 1; j < a.size(); j++) {
+			if (a[i] == a[j]) {
+				tong++;
+			}
+		}
+	}return tong;
+}
+int main() {
+	int t;
+	cin >> t;
+	cin.ignore();
+	while (t--) {
+		string a;
+		getline(cin, a);
+		cout << KQ(a)+a.size();
+		cout << endl;
+	}
+		system("pause");
+		return 0;
+}
