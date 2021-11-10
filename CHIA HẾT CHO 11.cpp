38197506a70d07620e0e529c8910f1cacd9fe705
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:29:53 +0700

    Create CHIA HẾT CHO 11.cpp
    Cho số tự nhiên N, hãy kiểm tra xem N có phải là số chia hết cho 11 hay không? Đưa ra 1 nếu N chia hết cho 11, trái lại đưa ra 0.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một số tự nhiên N.
    T, N thỏa mãn ràng buộc: 1≤ T ≤100; 0≤ N ≤101000.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    76945
    363588395960667043875487
    
    1
    0

---
+++ CHIA HẾT CHO 11.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+int KT(string a) {
+	long long tong = 0;
+	for (long long i = 0; i < a.size(); i++) {
+		int x = a[i] - 48;
+		if (i % 2 == 0) {
+			tong = tong + x;
+		}
+		else {
+			tong = tong - x;
+		}
+	}return tong;
+}
+int main() {
+	int t;
+	cin >> t;
+		cin.ignore();
+	while (t--) {
+		string a;
+		getline(cin, a);
+		
+		//cout << KT(a);
+		if (KT(a) % 11 == 0){
+			cout << "1";
+		}
+		else {
+			cout << "0";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
