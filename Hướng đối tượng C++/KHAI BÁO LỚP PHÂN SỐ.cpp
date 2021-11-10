1c64594436ab900b7388bb4517df3b452719bf91
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 21:58:00 +0700

    Create KHAI BÁO LỚP PHÂN SỐ.cpp
    Viết chương trình xây dựng class Phân số.
    
    Sau đó thực hiện nhập vào một phân số và in ra phân số đó ở dạng tối giản.
    
    Input
    
    Có hai số nguyên dương lần lượt là tử số và mẫu số. Các giá trị không quá 18 chữ số.
    
    Output
    
    Ghi ra phân số tối giản như trong ví dụ
    
    Ví dụ
    
    Input
    
    Output
    
    123 456
    
    41/152

---
+++ Hướng đối tượng C++/KHAI BÁO LỚP PHÂN SỐ.cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<fstream>
+using namespace std;
+typedef long long ll;
+ll Uoc(ll a, ll b) {
+	if (b == 0)return a;
+	return Uoc(b, a % b);
+}
+class PhanSo
+{
+public:
+	 void rutgon();
+	//ham nhap tao
+	friend istream& operator>>(istream&, PhanSo&);
+	friend ostream& operator<<(ostream&, PhanSo);
+	PhanSo(ll,ll);//theo de bai nen cai nay them 2 bien
+	~PhanSo();
+
+private:
+	ll tu;
+	ll mau;
+};
+istream& operator>>(istream&in, PhanSo&x) {
+
+	in >> x.tu;
+	in >> x.mau;
+	return in;
+}
+ostream& operator<<(ostream& ot, PhanSo a) {
+	ll x = Uoc(a.mau, a.tu);
+	a.tu = a.tu / x;
+	a.mau = a.mau / x;
+	ot << a.tu<<"/";
+	ot << a.mau;
+	return ot;
+}
+ void PhanSo::rutgon() {
+	 PhanSo a(1,1);
+	 ll x = Uoc(a.mau, a.tu);
+	 a.tu = a.tu / x;
+	 a.mau = a.mau / x;
+}
+PhanSo::PhanSo(ll tu, ll mau)// dang le trong hung theo main de bai 
+{
+	this->tu = tu;
+	this->mau = mau;
+}
+
+PhanSo::~PhanSo()
+{
+}
+int main() {
+	PhanSo p(1,1);
+	cin >> p;
+	p.rutgon();
+	cout << p;
+	return 0;
+}
