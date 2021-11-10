7b89154db1f3f3282cd349da0314fce976d3ce6c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 22:02:10 +0700

    Create  CẤU TRÚC SINH VIÊN.cpp
    Viết chương trình khai báo cấu trúc Sinh Viên gồm các thông tin: Mã SV, Họ tên, Lớp, Ngày sinh và Điểm GPA (dạng số thực float).
    
    Đọc thông tin 1 sinh viên từ bàn phím (không có mã sinh viên) và in ra màn hình. Trong đó Mã SV được gán là B20DCCN001. Ngày sinh được chuẩn hóa về dạng dd/mm/yyyy.
    
    Input
    
    Gồm 4 dòng lần lượt là Họ tên, Lớp, Ngày sinh và Điểm GPA.
    
    Trong đó:
    
    Họ tên không quá 30 chữ cái.
    Lớp theo đúng định dạng thường dùng ở PTIT
    Ngày sinh có đủ 3 phần ngày tháng năm nhưng có thể chưa đúng chuẩn dd/mm/yyyy.
    Điểm GPA đảm bảo trong thang điểm 4 với 2 nhiều nhất 2 số sau dấu phẩy.  
    Output
    
    Ghi thông tin sinh viên trên 1 dòng, mỗi thông tin cách nhau 1 khoảng trống.
    
    Ví dụ
    
    Input
    
    Output
    
    Nguyen Hoa Binh
    
    D20CQCN04-B
    
    2/2/2002
    
    2
    
    B20DCCN001 Nguyen Hoa Binh D20CQCN04-B 02/02/2002 2.00
    
     
    
    Giới hạn thời gian: 2s
    
    Giới hạn bộ nhớ: 65536 Kb
    
    Bài tập này yêu cầu sử dụng hàm main cho sẵn như sau:
    
    C/C++
    int main(){
        struct SinhVien a;
        nhap(a);
        in(a);
        return 0;
    }

---
+++ Hướng đối tượng C++/CẤU TRÚC SINH VIÊN.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+#include<string>
+struct SinhVien {
+    string Ten;
+    string Lop;
+    string ngay_sinh;
+    float Diem{};
+};
+typedef struct SinhVien SV;
+void nhap(SV& a) {
+    getline(cin, a.Ten);
+    getline(cin, a.Lop);
+    getline(cin, a.ngay_sinh);
+    cin >> a.Diem;
+}
+void in(SV a) {
+    //them so 0 vao truoc ngay tháng sinh 
+    if (a.ngay_sinh[1]=='/') {
+        a.ngay_sinh.insert(a.ngay_sinh.begin() + 0, '0');
+    }
+    if (a.ngay_sinh[4]=='/') {
+        a.ngay_sinh.insert(a.ngay_sinh.begin() + 3, '0');
+    }
+    
+    cout << "B20DCCN001" << " " << a.Ten << " " << a.Lop << " " << a.ngay_sinh << " " << fixed << setprecision(2) << a.Diem;
+}
+int main(){
+    struct SinhVien a;
+    nhap(a);
+    in(a);
+    return 0;
+}
