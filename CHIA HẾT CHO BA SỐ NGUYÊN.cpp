d88219d35cc0235d872e1c2c4c149119758ccce4
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 22:04:44 +0700

    Create  CHIA HẾT CHO BA SỐ NGUYÊN.cpp
    Cho bốn số nguyên dương X, Y, Z và N. Hãy tìm số nguyên dương nhỏ nhất có N chữ số chia hết đồng thời cho X, Y, Z.  Ví dụ với X = 2, Y = 3, Z = 5, N = 4 ta tìm được số nguyên dương nhỏ nhất có 4 chữ số là 1020 chia hết cho cả 2, 3, 5.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T (T≤100).
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là bộ bốn số X, Y, Z, N. Các số X, Y, Z, N thỏa mãn ràng buộc dưới đây:
    1≤ X, Y, Z ≤105; N≤18.
    
    Output:
    
    Đưa ra theo từng dòng kết quả mỗi test là số nguyên nhỏ nhất có N chữ số chia hết đồng thời cho X, Y, Z. Trong trường hợp không có số nguyên N chữ số thỏa mãn yêu cầu bài toán đưa ra giá trị -1.
    Input
    
    Output
    
    3
    
    2  3  5  4
    
    4  5  6  3
    
    3  5  7  2
    
    1020
    
    120
    
    -1

---
+++ CHIA HẾT CHO BA SỐ NGUYÊN.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+long long UCLN(long long a, long long b) {
+	if (b == 0) {
+		return a;
+	}
+	return UCLN(b, a % b);
+}
+long long BCNN(long long a, long long b) {
+	return (a * b) / UCLN(a, b);
+}
+//tim uoc cuar ca day
+long long KQ(long long a,long long b,long long c, long long N) {
+	//so luog chu so ma de yeu cau
+	long long start = 1;
+	for (long long i = 1; i < N; i++) {
+		start = start * 10;
+	}
+	//return start;
+	long long end = start * 10 - 1;
+	long long temp = BCNN(a, b);
+	long long kq = BCNN(temp, c);
+	//return kq;
+
+	for (long long i = start/kq;; i++) {
+		if (kq * i >= start && kq * i <= end) {
+			return kq*i;
+
+		}
+		if (kq * i > end) {
+			return -1;
+		}
+
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		long long X, Y, Z, N;
+		cin >> X >> Y >> Z >> N;
+		//cout << BCNN(X, Y );
+		cout << KQ(X, Y, Z, N) << endl;;
+	}
+	system("pause");
+	return 0;
+}
