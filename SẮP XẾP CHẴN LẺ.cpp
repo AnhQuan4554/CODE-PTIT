d652e430ec8ed8363a60a4926fbce1c22706febe
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 22:11:32 +0700

    Create  SẮP XẾP CHẴN LẺ.cpp
    Cho mảng A[] gồm n số nguyên dương. Nhiệm vụ của bạn là hãy sắp xếp lại các phần tử của mảng sao cho A[i] ≥A[i-1] nếu i chẵn,  A[i] ≤A[i-1] nếu i lẻ. Ví dụ với mảng A[] = {1, 2, 2, 1} ta được mảng được sắp A[] = { 1, 2, 1, 2}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng kế tiếp đưa vào n số A[i] của mảng; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤103; 1≤ A[i] ≤103;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    
    4
    
    1  2  2 1
    
    3
    1  3  2
    
    1  2  1  2
    
    1  3  2

---
+++ SẮP XẾP CHẴN LẺ.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+void Nhap(vector<int>& a, int n) {
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		a.push_back(x);
+	}
+}
+void Xuat(vector<int>a) {
+	for (int i = 0; i <= (a.size()-1) / 2; i++) {
+		
+		cout << a[i] << " ";
+		if ((a.size() - 1) / 2 + 1 + i < a.size()) {
+			cout << a[(a.size() - 1) / 2 + 1 + i]<<" ";
+		}
+	}
+
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+vector<int>a;
+	int n;
+	cin >> n;
+	Nhap(a, n);
+	sort(a.begin(), a.end());
+	Xuat(a);
+		cout << endl;
+	}
+	
+	system("pause");
+	return 0;
+}
