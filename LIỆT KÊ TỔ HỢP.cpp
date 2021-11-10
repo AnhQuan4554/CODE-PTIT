a28d1eb56321ba56cb3fbdb17e3048ee566f46b3
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-10 22:10:01 +0700

    Create LIỆT KÊ TỔ HỢP.cpp
    Cho số tự nhiên N và số K. Hãy đưa ta các tổ hợp chập K của 1, 2, .., N.
    
     Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là bộ đôi N, K được viết trên 1 dòng.
    T, N, K thỏa mãn ràng buộc :1 ≤ T, K, N ≤ 20.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input
    
    Output
    
    1
    5 3
    
    123  124 125 134  135 145 234 235 245 345

---
+++ LIỆT KÊ TỔ HỢP.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+void Khoi_tao(int* a, int k) {
+	for (int i = 0; i < k; i++) {
+		a[i] = i + 1;//theo de nguoi ta yeu cau phan tu dau cos gia tri bat dau tu 0
+	}
+}
+void Bien_doi(int* a,int k, int n,int &kt) {
+	int i = k-1;//vi do minh bat dau ti 0
+	while (i >= 0 && a[i] == n - k + 1 + i) {
+		i--;
+	}
+	if (i < 0) {
+		kt = 1;
+	}
+	else {
+		a[i]++;
+		/*VD tổ hợp chập 3 của 5 có phần tử 1 2 5 , vị trí i=2(tức a[i]=5 đã max) thì ta lùi xuống xét vị trí tiếp theo
+		sau đó vị trí i=2 sẽ bằng vị trí tiếp theo cộng 1 sẽ từ 1 2 5  thành 1 2 3 rồi lúc đó sẽ lại quay lại xét vòng while từ
+		vị trí i=2 là vị trí cuối cùng có giá trị bằng 3 đó */
+		for (int j = i; j < k-1; j++) {
+			a[j + 1] = a[j] + 1;
+		}
+	}
+
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	int n, k;
+	cin >> n >> k;
+	int* a = new int[k];
+	Khoi_tao(a, k);
+	int kt = 0;
+	while (kt != 1) {
+		for (int i = 0; i < k; i++) {
+			cout << a[i];
+		}
+		Bien_doi(a, k, n, kt);
+		cout << " ";
+	}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
