121a68f3d23daa120fa57f080bc44c445f321222
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-19 16:32:28 +0700

    thuật toán tính tổng dãy con
    Cho mảng A[] gồm n phần tử. Nhiệm vụ của bạn là tìm tổng lớn nhất các dãy con liên tục của mảng A[]. Ví dụ với A[] = { -2, -3, 4, -1, -2, 1, 5, -3} ta có câu trả lời là 7 tương ứng với tổng lớn nhất của dãy con liên tục {4, -1, -2, 1, 5}. 
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n; dòng tiếp theo là n số A[i] của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n  ≤106; 10-6≤ A[i]  ≤106.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5
    1 2 3 -2 5
    4
    -1 -2 -3 -4
    
    9
    -1

---
+++ TỔNG LỚN NHẤT CỦA DÃY CON LIÊN TỤC.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#include<algorithm>
+void input(vector<ll>& a, ll n) {
+	for (ll i = 0; i < n; i++) {
+		ll x; cin >> x;
+		a.push_back(x);
+	}
+}
+ll output(vector<ll>a) {
+	ll result = a[0];
+	ll tong = 0;
+	for (int i = 0; i < a.size(); i++) {
+		tong = max(a[i],tong + a[i]);
+		result = max(tong, result);
+	}return result;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<ll>a;
+		ll n; cin >> n;
+		input(a, n);
+		cout << output(a) << endl;
+	}
+	system("pause");
+	return 0;
+}
