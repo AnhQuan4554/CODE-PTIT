14563e6a84cec7bf6ac220d7d6e9c6e254270a44
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-11-26 10:19:49 +0700

    Create BIẾN ĐỔI DÃY SỐ.cpp
    Cho mảng A[] gồm n số nguyên dương. Hãy biến mảng A[] thành một mảng đối xứng sao cho phép thay thế A[i] = Merge(A[i], A[i+1]) được thực hiện ít nhất. Trong đó, Merge(A[i], A[i+1]) = A[i] + A[i+1]. Ví dụ với A[] = {3, 2, 3, 3, 5} ta chỉ cần thực hiện 1 phép Merge(A[0], A[1]) để trở thành mảng A[] = {5, 3, 3, 5}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n; dòng tiếp theo là n số A[i] của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤103; 1≤ A[i]  ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5
    3 2 3 3 5
    4
    5 3 3 4
     
    
    1
    3

---
+++ BIẾN ĐỔI DÃY SỐ.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+void input(vector<int>& a, int n) {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		a.push_back(x);
+	}
+}
+int output(vector<int>a) {
+	int count = 0;
+	int left = 0;
+	int right = a.size() - 1;
+	while (left < right) {
+		if (a[left] == a[right]) {
+			left++;
+			right--;
+		}
+		else if (a[left] < a[right]) {
+			a[left + 1] += a[left];
+			count++;
+			left++;
+			//phai tang len cu ap dung vd de hieu 
+			//neu ko tang se bi treo vi left right van giu nguyen
+		}
+		else {
+			a[right - 1] += a[right];
+			count++;
+			right--;
+		}
+	}return count;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<int>a;
+		int n; cin >> n;
+		input(a, n);
+		cout << output(a) << endl;
+	}
+	system("pause");
+	return 0;
+}
