c8f40bff93c4ce64295dbe003fb386ebd6f9b0be
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-02 16:19:24 +0700

    Create DANH SÁCH ĐỐI TƯỢNG NHÂN VIÊN.cpp
    Một nhân viên làm việc trong công ty được lưu lại các thông tin sau:
    
    Mã nhân viên: được gán tự động tăng, bắt đầu từ 00001
    Họ tên: Xâu ký tự không quá 40 chữ cái.
    Giới tính: Nam hoặc Nu
    Ngày sinh: đúng theo chuẩn dd/mm/yyyy
    Địa chỉ: Xâu ký tự không quá 100 chữ cái
    Mã số thuế: Dãy số có đúng 10 chữ số
    Ngày ký hợp đồng: đúng theo chuẩn dd/mm/yyyy
    Viết chương trình nhập danh sách nhân viên (không nhập mã) trong đó có sử dụng chồng toán tử nhập/xuất và in ra màn hình danh sách vừa nhập.
    
    Input
    
    Dòng đầu ghi số N là số nhân viên (không quá 40). Mối nhân viên ghi trên 6 dòng lần lượt ghi các thông tin theo thứ tự đã ghi trong đề bài. Không có mã nhân viên.
    
    Output
    
    Ghi ra danh sách đầy đủ nhân viên, mỗi nhân viên trên một dòng, các thông tin cách nhau đúng một khoảng trống.
    
    Ví dụ
    
    Input
    
    3
    
    Nguyen Van A
    
    Nam
    
    10/22/1982
    
    Mo Lao-Ha Dong-Ha Noi
    
    8333012345
    
    31/12/2013
    
    Ly Thi B
    
    Nu
    
    10/15/1988
    
    Mo Lao-Ha Dong-Ha Noi
    
    8333012346
    
    22/08/2011
    
    Hoang Thi C
    
    Nu
    
    04/02/1981
    
    Mo Lao-Ha Dong-Ha Noi
    
    8333012347
    
    22/08/2011
    
    Output
    
    00001 Nguyen Van A Nam 10/22/1982 Mo Lao-Ha Dong-Ha Noi 8333012345 31/12/2013
    
    00002 Ly Thi B Nu 10/15/1988 Mo Lao-Ha Dong-Ha Noi 8333012346 22/08/2011
    
    00003 Hoang Thi C Nu 04/02/1981 Mo Lao-Ha Dong-Ha Noi 8333012347 22/08/2011
    
     
    
    Giới hạn thời gian: 2s
    
    Giới hạn bộ nhớ: 65536 Kb
    
    Bài tập này yêu cầu sử dụng hàm main cho sẵn như sau:
    
    C/C++
    int main(){
        NhanVien ds[50];
        int N,i;
        cin >> N;
        for(i=0;i<N;i++) cin >> ds[i];
        for(i=0;i<N;i++) cout << ds[i];
        return 0;
    }
    Trình biên

---
+++ Hướng đối tượng C++/DANH SÁCH ĐỐI TƯỢNG NHÂN VIÊN.cpp
@@ -0,0 +1,80 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+int dem = 1;
+int kt = 0;
+class NhanVien
+{
+public:
+	//ham nhap 
+	friend istream& operator >> (istream &in,NhanVien &x) {
+		if (kt==0)
+		{
+			cin.ignore(); kt++;
+		}
+		x.ma = dem; dem++;
+		getline(in, x.Ten);
+		getline(in, x.Gioi_tinh);
+		getline(in, x.Ngay_sinh);
+		getline(in, x.Dia_chi);
+		getline(in, x.ma_so_thue);
+		getline(in, x.ngay_ki_hop_dong);
+		return in;
+	}
+	//ham xuat
+	friend ostream& operator<<(ostream& ot, NhanVien x ) {
+		x.setngaysinh(); x.setngay_ki_hop_dong();//sua ngay sinh va ngay ki hop dong
+		if (x.ma < 10)cout << "0000" << x.ma<<" ";
+		else cout << "000" << x.ma<<" ";
+		cout << x.Ten << " " << x.Gioi_tinh << " " << x.Ngay_sinh << " " << x.Dia_chi << " " << x.ma_so_thue << " " << x.ngay_ki_hop_dong;
+		cout << endl;
+		return ot;
+	}
+	//ham chinh ngay sinh
+	void setngaysinh() {
+
+		if (Ngay_sinh[1] == '/') {
+			Ngay_sinh.insert(Ngay_sinh.begin() + 0, '0');
+		}
+		if (Ngay_sinh[4] == '/') {
+			Ngay_sinh.insert(Ngay_sinh.begin() + 3, '0');
+		}
+	}
+	//ham chinh ngay sinh
+	void setngay_ki_hop_dong() {
+		if (ngay_ki_hop_dong[1] == '/') {
+			ngay_ki_hop_dong.insert(ngay_ki_hop_dong.begin() + 0, '0');
+		}
+		if (ngay_ki_hop_dong[4] == '/') {
+			ngay_ki_hop_dong.insert(ngay_ki_hop_dong.begin() + 3, '0');
+		}
+	}
+	NhanVien();
+	~NhanVien();
+
+private:
+	int ma = 0;
+	string Ten;
+	string Gioi_tinh;
+	string Ngay_sinh;
+	string Dia_chi;
+	string ma_so_thue;
+	string ngay_ki_hop_dong;
+};
+
+NhanVien::NhanVien()
+{
+}
+
+NhanVien::~NhanVien()
+{
+}
+int main(){
+    NhanVien ds[50];
+    int N,i;
+    cin >> N;
+    for(i=0;i<N;i++) cin >> ds[i];
+    for(i=0;i<N;i++) cout << ds[i];
+    return 0;
+}
