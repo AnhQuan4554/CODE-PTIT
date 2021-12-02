99511277d0744996200acad948b700fa09e404c0
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-02 10:16:42 +0700

    chuyển string về số với thu viện cstdlib
    Cho dãy A[] chỉ bao gồm các số nguyên dương không quá 105 nhưng không biết trước số phần tử của dãy. Người ta gọi dãy A[] là dãy ưu thế nếu thỏa mãn 1 trong 2 điều kiện sau đây:
    
    Dãy gọi là ưu thế chẵn nếu số phần tử của dãy là chẵn và số lượng số chẵn trong dãy nhiều hơn số lượng số lẻ.
    Dãy gọi là ưu thế lẻ nếu số phần tử của dãy là lẻ và số lượng số lẻ trong dãy nhiều hơn số lượng số chẵn.
    Hãy kiểm tra xem dãy A[] có phải là dãy ưu thế hay không.
    
    Dữ liệu vào
    
    Dòng đầu ghi số bộ test, không quá 10
    Mỗi bộ test là một dãy các số nguyên dương (không quá 104) và có không quá 200 số, các số cách nhau 1 khoảng trống, không biết trước số lượng phần tử.
    Kết quả
    
    Nếu dãy A[] thỏa mãn là dãy ưu thế thì in ra YES, nếu không in ra NO
    Ví dụ
    
    Input
    
    Output
    
    2
    
    11 22 33 44 55 66 77
    
    23 34 45 56 67 78 89 90 121 131 141 151 161 171
    
    YES
    
    NO

---
+++ DÃY ƯU THẾ.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+#include<cstdlib>
+using namespace std;
+int check(string a) {
+	
+		string temp;
+		stringstream s(a);
+		int chan = 0;int le = 0;
+		int count = 0;//so luong so vi neu size se rinh ca dau cach
+		while (s >> temp) {
+			int x = stoi(temp);//chuyển string về số với thu viện cstdlib
+			if (x % 2 == 0) {
+				chan++;
+				count++;
+			}
+			else {
+				le++;
+				count++;
+			}
+			
+		}
+		if (count % 2 == 0 && chan > le || count % 2 == 1 && le > chan) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+}
+int main() {
+	int t; cin >> t; cin.ignore();
+	while (t--)
+	{
+	string a;
+	getline(cin, a);
+
+	if (check(a)==1) {
+		cout << "YES\n";
+	}
+	else {
+		cout << "NO\n";
+	}
+
+
+	}
+	
+	system("pause");
+	return 0;
+}
