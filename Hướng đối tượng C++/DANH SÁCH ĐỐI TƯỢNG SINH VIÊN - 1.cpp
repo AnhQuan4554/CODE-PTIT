72c20a5ca6bce35b776a79aede2bbd3f4ed68327
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-02 11:06:30 +0700

    tự động tăng mã số sv bằng biến đặt ngoài
    Viết chương trình khai báo lớp Sinh Viên gồm các thông tin: Mã SV, Họ tên, Lớp và Điểm GPA (dạng số thực float). Hàm khởi tạo không có tham số, gán các giá trị thuộc tính ở trạng thái mặc định (xâu ký tự rỗng, giá trị số bằng 0).
    
    Yêu cầu sử dụng chồng toán tử nhập và xuất để nhập đối tượng sinh viên với cin và in ra đối tượng sinh viên với cout.
    
    Đọc thông tin N thí sinh từ bàn phím (không có mã sinh viên) và in ra lần lượt màn hình mỗi dòng 1 sinh viên theo đúng thứ tự ban đầu. Trong đó Mã SV được tự tạo ra theo quy tắc thêm mã B20DCCN sau đó là giá trị nguyên tự động tăng tính từ 001 (tối đa là 099). Ngày sinh được chuẩn hóa về dạng dd/mm/yyyy
    
    Input
    
    Dòng đầu tiên ghi số sinh viên N (0 < N < 50).
    
    Mỗi sinh viên ghi trên 4 dòng lần lượt là Họ tên, Lớp, Ngày sinh và Điểm GPA.
    
    Trong đó:
    
    Họ tên không quá 30 chữ cái.
    Lớp theo đúng định dạng thường dùng ở PTIT
    Ngày sinh có đủ 3 phần ngày tháng năm nhưng có thể chưa đúng chuẩn dd/mm/yyyy.
    Điểm GPA đảm bảo trong thang điểm 4 với 2 nhiều nhất 2 số sau dấu phẩy.  
    Output
    
    Ghi ra danh sách lần lượt các sinh viên có đầy đủ Mã sinh viên, Họ tên, Lớp, Ngày sinh (đã chuẩn hóa về dạng dd/mm/yyyy), Điểm GPA (với đúng 2 số sau dấu phẩy).
    
    Mỗi sinh viên ghi trên 1 dòng, mỗi thông tin cách nhau 1 khoảng trống.
    
    Ví dụ
    
    Input
    
    Output
    
    1
    
    Nguyen Van An
    
    D20CQCN01-B
    
    2/12/2002
    
    3.19
    
    B20DCCN001 Nguyen Van An D20CQCN01-B 02/12/2002 3.19

---
+++ Hướng đối tượng C++/DANH SÁCH ĐỐI TƯỢNG SINH VIÊN - 1.cpp
@@ -0,0 +1,70 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<iomanip>
+#include<fstream>
+int kt = 0;
+int dem = 1;
+using namespace std;
+class SinhVien
+{
+public:
+    //ham sua ngay sinh
+    void setngaysinh() {
+        if (Ngay_sinh[1] == '/') {
+            Ngay_sinh.insert(Ngay_sinh.begin() + 0, '0');
+        }
+        if (Ngay_sinh[4] == '/') {
+            Ngay_sinh.insert(Ngay_sinh.begin() + 3, '0');
+        }
+    }
+    friend istream& operator >>(istream& in, SinhVien& x) {
+        cin.ignore();
+        getline(in, x.ten);
+        getline(in, x.lop);
+        getline(in, x.Ngay_sinh);
+        in >> x.diem;
+        return in;
+    }
+    friend ostream& operator<<(ostream& ot, SinhVien x) {
+        x.setngaysinh();
+        x.ma = dem;
+        dem++;/*phai tang trong nay neu khong theo nhuw ham main 
+              no moi lan nhap la bien dem tang do do sau khi nhap xong 10 nguoi
+              thi bien dem =10 roi con cong gi nua*/
+        if (x.ma < 10)cout << "B20DCCN00" << x.ma<<" ";
+        else cout << "B20DCCN0" << x.ma<<" ";
+
+        cout << x.ten << " " << x.lop << " " << x.Ngay_sinh << " " << fixed << setprecision(2) << x.diem;
+        cout << endl;
+        return ot;
+    }
+	SinhVien();
+	~SinhVien();
+
+private:
+    int ma;
+    string ten;
+    string lop;
+    string Ngay_sinh;
+    float diem;
+};
+SinhVien::SinhVien()
+{
+}
+
+SinhVien::~SinhVien()
+{
+}
+int main(){
+    SinhVien ds[50];
+    int N, i;
+    cin >> N;
+    for(i=0;i<N;i++){
+        cin >> ds[i];
+    }
+    for(i=0;i<N;i++){
+        cout << ds[i];
+    }
+    return 0;
+}
