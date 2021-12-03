7bf49893755e87c0ef9fe8092bfed6e0ac5e3ac8
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-03 10:28:38 +0700

    Create  PHÉP TOÁN VỚI PHÂN SỐ.cpp
    Phân số là sự biểu diễn số hữu tỷ dưới dạng tỷ lệ của hai số nguyên, trong đó số ở trên được gọi là tử số, còn số ở dưới được gọi là mẫu số. Cho hai phân số A và B có tử số và mẫu số được nhập từ bàn phím.
    
    Viết chương trình thực hiện hai nhiệm vụ sau:
    
    Tính C = (A + B)2 và rút gọn kết quả.
    Tính D = A x B x C và rút gọn kết quả.
    Input:
    
    Dòng đầu tiên là số bộ test T (T <= 100)
    
    T dòng tiếp theo, mỗi dòng gồm 4 số lần lượt là tử và mẫu số của phân số A và phân số B với -102<= tử số, mẫu số <= 102. Mẫu số là số khác 0.
    
    Output:
    
    Kết quả của hai phép tính theo định dạng phân số.
    
    Ví dụ
    
    Input:
    
    Output:
    
    2
    1 2 3 4
    2 3 4 5
    
    25/16 75/128
    484/225 3872/3375
    int main() {
    	int t;
    	cin >> t;
    	while (t--) {
    		PhanSo A;
    		PhanSo B;
    		cin >> A.tu >> A.mau >> B.tu >> B.mau;
    		process(A, B);
    	}
    }

---
+++ Hướng đối tượng C++/PHÉP TOÁN VỚI PHÂN SỐ.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+struct PhanSo
+{
+	long long tu, mau;
+};
+typedef struct PhanSo PS;
+int UCLN(long long a, long long  b) {
+	if (b == 0)return a;
+	return UCLN(b, a % b);
+}
+void Rut_gon(PS& a) {
+	int temp = UCLN(a.tu, a.mau);
+	a.tu /= temp;
+	a.mau /= temp;
+}
+void process(PS a, PS b) {
+	PS sum;
+	sum.tu = (a.tu * b.mau + b.tu*a.mau)* (a.tu * b.mau + b.tu * a.mau);
+	sum.mau = (a.mau * b.mau )*(a.mau * b.mau);
+	Rut_gon(sum);
+	cout << sum.tu << "/" << sum.mau<<" ";
+	PS t;
+	t.tu = a.tu * b.tu * sum.tu;
+	t.mau = a.mau * b.mau * sum.mau;
+	Rut_gon(t);
+	cout << t.tu << "/" << t.mau;
+	cout << endl;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		PhanSo A;
+		PhanSo B;
+		cin >> A.tu >> A.mau >> B.tu >> B.mau;
+		process(A, B);
+	}
+}
