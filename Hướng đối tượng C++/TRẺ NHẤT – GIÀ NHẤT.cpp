11faa550cd8ac94aa239e4cdf69c6a900cf56caf
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-03 11:51:49 +0700

    Create TRẺ NHẤT – GIÀ NHẤT.cpp
    Cho một danh sách tên người và ngày tháng năm sinh.
    
    Hãy tìm ra người trẻ nhất và người già nhất.
    
    Input
    
    Dòng 1 ghi số N là số người (không quá 100).
    N dòng tiếp theo, mỗi dòng ghi tên (xâu ký tự không có khoảng trống và không quá 15 ký tự, sau đó là dãy ký tự mô tả ngày tháng năm sinh theo chuẩn dd/mm/yyyy.
    
    Output
    
    Dòng đầu ghi ra tên người trẻ nhất.
    
    Dòng thứ 2 ghi ra tên người già nhất.
    
    Dữ liệu đảm bảo không có 2 người nào trùng ngày sinh.
    
    Ví dụ
    
    Input
    
    Output
    
    5
    
    Nam 01/10/1991
    
    An 30/12/1990
    
    Binh 15/08/1993
    
    Tam 18/09/1990
    
    Truong 20/09/1990
    
    Binh
    
    Tam

---
+++ Hướng đối tượng C++/TRẺ NHẤT – GIÀ NHẤT.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+using namespace std;
+#include<string>
+#include<cstdlib>
+#include<algorithm>
+class Thongtin
+{
+public:
+	void Nhap() {
+		cin >> ten;
+		cin >> ngay_sinh;
+
+	}
+	string get_ngay_sinh() {
+		return ngay_sinh;
+	}
+	string get_ten() {
+		return ten;
+	}
+	Thongtin();
+	~Thongtin();
+
+private:
+	string ten, ngay_sinh;
+};
+bool cmp(Thongtin a, Thongtin b) {
+	int ngaya = stoi(a.get_ngay_sinh().substr(0, 1));
+	int thanga = stoi(a.get_ngay_sinh().substr(3, 4));
+	int nama = stoi(a.get_ngay_sinh().substr(6));
+	int ngayb = stoi(b.get_ngay_sinh().substr(0, 1));
+	int thangb = stoi(b.get_ngay_sinh().substr(3, 4));
+	int namb = stoi(b.get_ngay_sinh().substr(6));
+	if (nama != namb)return nama < namb;
+	if (thanga != thangb)return thanga < thangb;
+	return ngaya < ngayb;
+
+
+}
+Thongtin::Thongtin()
+{
+}
+
+Thongtin::~Thongtin()
+{
+}
+int main() {
+	Thongtin a[200];
+	int n; cin >>  n;
+	for (int i = 0; i < n; i++) {
+		a[i].Nhap();
+	}
+	sort(a, a + n, cmp);
+	cout << a[n - 1].get_ten() << endl<< a[0].get_ten();//gi nhat thi gia tri nho nhat 
+	system("pause");
+	return 0;
+}
+
+//Nam 01/10/1991
+//An 30/12/1990
+//Binh 15/08/1993
+//Tam 18/09/1990
+//Truong 20/09/1990
