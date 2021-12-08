bc11c65755be9587efa9201736464a5e96c87702
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-08 10:36:57 +0700

    Số chia dư đọc lại trước thi
    Cho số tự nhiên N được biểu diễn dưới dạng nhị phân. Hãy kiểm tra xem N có phải là số chia hết cho 5 hay không? Đưa ra “Yes” nếu N chia hết cho 5, trái lại đưa ra “No”.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một biểu diễn nhị phân của N.
    T, N thỏa mãn ràng buộc: 1≤ T ≤100; 0≤ N ≤101000.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    1010
    10000101001
    
    Yes
    Yes

---
+++ CHIA HẾT CHO 5.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+//ham bien doi
+int Change(string a) {
+	int sum = 0; 
+	for (int i = a.size() - 1; i >= 0; i--) {
+		if (a[i] == '1') {
+			if ((a.size() - i - 1) % 4 == 0)sum += 1;
+			else if ((a.size() - i - 1) % 4 == 1)sum += 2;
+			else if ((a.size() - i - 1) % 4 == 2)sum += 4;
+			else if ((a.size() - i - 1) % 4 == 3)sum += 3;
+		}
+	}return sum;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		string a; cin >> a;
+		if (Change(a) % 5 == 0)cout << "Yes\n";
+		else cout << "No\n";
+		
+	}
+	system("pause");
+	return 0;
+}
