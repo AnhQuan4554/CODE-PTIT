e5a2f88d7912550a869fafaa20f1b9e23a5b6597
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-09 09:11:59 +0700

    Update LIỆT KÊ HOÁN VỊ.cpp

--- LIỆT KÊ HOÁN VỊ.cpp
+++ LIỆT KÊ HOÁN VỊ.cpp
@@ -1,69 +1,48 @@
-﻿#include<iostream>
-#include<cmath>
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<algorithm>
 using namespace std;
 
-void generate_next_permutation(int A[], int n) {
-
-    int j, k, r, s, temp;
-    j = n - 2;
-    // tim j la chi so lon nhat aj < a_j+1
-    while (A[j] > A[j + 1]) {
-        j = j - 1;
-    }
-    // tim ak la so nho nhat lon hon aj o ben phai aj
-    k = n - 1;
-    while (A[j] > A[k]) {
-        k = k - 1;
-    }
-    // swap aj va ak
-    temp = A[j];
-    A[j] = A[k];
-    A[k] = temp;
-
-    // sap xep lai chuoi // lat nguoc doan tu a_j+1 den an
-    r = j + 1; s = n - 1;
-    while (r < s) {
-        temp = A[r];
-        A[r] = A[s];
-        A[s] = temp;
-        r = r + 1;
-        s = s - 1;
-    }
+void input(int a[], int n) {
+	for (int i = 1; i <= n; i++) {
+		a[i] = i;
+	}
+}
+void handle(int a[], int n,int &kt) {
+	int temp = n - 1;
+	while (a[temp] > a[temp+1]&&temp>0) {
+		temp--;
+	}
+	if (temp <= 0)kt = 0;
+	int x = n;
+	while (a[temp] > a[x]) {
+		x--;
+	}
+	swap(a[temp], a[x]);
+	int l = temp + 1;
+	int r = n;
+	while (l <= r) {
+		swap(a[l], a[r]);
+		l++;
+		r--;
+	}
 
 }
-
-void xuat(int A[], int n) {
-    for (int i = 0; i < n; i++)
-        cout << A[i];
-    cout <<" ";
-}
-
-int giaiThua(int n)
-{
-    if (n == 1)
-        return 1;
-    return n * giaiThua(n - 1);
-}
-
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-int n;
-    // nhap gia tri n
-    cin >> n;
-    int *A=new int[n];
-    //Kh?i t?o m?ng cau hinh dau tien
-    for (int i = 0; i < n; i++) A[i] = i + 1;
-
-    // liet ke tat ca cac cau hinh 
-    for (int i = 0; i < giaiThua(n); i++) {
-        xuat(A, n);
-        generate_next_permutation(A, n);
-        // xuat(A, n); 
-    }
-        cout << endl;
-    }
-    
+	int t; cin >> t;
+	while (t--) {
+		int a[1000];
+		int n; cin >> n;
+		input(a, n);
+		int kt = 1;
+		while (kt) {
+			for (int i = 1; i <= n; i++) {
+				cout << a[i];
+			}cout << " ";
+			handle(a, n,kt);
+		}
+		cout << endl;
+	}
+	return 0;
 }
-
