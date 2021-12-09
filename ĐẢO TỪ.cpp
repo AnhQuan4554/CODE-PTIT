43a831f20ff8ebfe96b9e135fde58f776b6f0c11
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-09 09:41:49 +0700

    Update ĐẢO TỪ.cpp

--- ĐẢO TỪ.cpp
+++ ĐẢO TỪ.cpp
@@ -1,37 +1,26 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
-void Xuat(string a) {
-	int dem = 0;
-	
-	for (int i = a.length() - 1; i >= 0; i--) {
-		dem++;
-		if (a[i] == ' ') {
-			cout << a.substr(i + 1, dem - 1);
-			//cout << "ok";
-			cout << " ";
-			dem = 0;
-		}
-	
-	}
-	//in tu cuoi cung
-	int cuoi = 0;
-	while (a[cuoi] != ' ') {
-		cout << a[cuoi];
-		cuoi++;
+#include<sstream>
+#include<vector>
+void Cut_string(vector<string>& a, string b) {
+	stringstream s(b);
+	string temp;
+	while (s >> temp) {
+		a.push_back(temp);
 	}
 }
 int main() {
-	int t;
-	cin >> t;
-	cin.ignore();
+	int t; cin >> t; cin.ignore();
 	while (t--) {
-		string a;
-		getline(cin, a);
-		Xuat(a);
+		vector<string>a;
+		string b;
+		getline(cin, b);
+		Cut_string(a, b);
+		for (int i = a.size() - 1; i >= 0; i--)cout << a[i] << " ";
 		cout << endl;
 	}
-
 	system("pause");
 	return 0;
 }
