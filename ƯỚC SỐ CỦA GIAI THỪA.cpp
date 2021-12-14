bb06bfd962dc975a79105d75a97044f6f8b3be10
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-14 12:01:00 +0700

    phân tích giai thừa hay đọc lại
    Cho số tự nhiên N và số nguyên tố P. Nhiệm vụ của bạn là tìm số x lớn nhất để N! chia hết cho px. Ví dụ với N=7, p=3 thì x=2 là số lớn nhất để 7! Chia hết cho 32. 
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là cặp số N, p được viết cách nhau một vài khoảng trống.
    T, N, p thỏa mãn rang buộc : 1≤T≤100; 1≤N≤105; 2≤p≤5000;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    3
    62  7
    76  2
    3  5
    
    9
    73
    0

---
+++ ƯỚC SỐ CỦA GIAI THỪA.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+#define MOD 10000007;
+#include<cmath>
+#include<vector>
+typedef long long ll;
+
+int main() {
+	/*thay vì tính 7! ta phan tich nó thành các số chia hết cho 3 
+ phân ra thành có 6 và 3 nên tích sẽ chia hết cho 3 mũ 2 là lơn nhất 
+ vd 9! thì chia hết cho số lớn nhất là 3 mũ 4 vì 9 chia het scho 3 mũ 2 , 6 và 3 chia hét cho 3
+ */
+	int t; cin >> t;
+	while (t--) {
+		int N, p;
+		cin >> N >> p;
+		int count = 0;
+		for (int i = p; i <= N; i += p) {
+			int x = i;
+			while (x % p == 0)
+			{
+				count++;
+				x /= p;
+			}
+		}cout << count << endl;
+		
+	}
+	
+	system("pause");
+	return 0;
+}
