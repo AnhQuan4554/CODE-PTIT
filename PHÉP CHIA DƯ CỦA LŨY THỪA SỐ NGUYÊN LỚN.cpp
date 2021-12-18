359e3a2d38136e830c8cbfc352caa2445612b2eb
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-18 11:42:18 +0700

    Create PHÉP CHIA DƯ CỦA LŨY THỪA SỐ NGUYÊN LỚN.cpp
    Cho số nguyên dương a, b, M, trong đó a là số rất lớn được biểu diễn như một xâu ký tự số. Hãy tìm K = (ab) %M. Ví dụ a = 3, b=2, M = 4 thì K = (32)%4 = 1
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp mỗi dòng đưa vào một test. Mỗi test là bộ ba a, b, M được viết trên một dòng.
    T, a, b, M thỏa mãn ràng buộc : 1≤T≤100; 0≤length(a) ≤1000; 2≤ b, M ≤1012.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    1
    3 2 4
     
    
    1

---
+++ PHÉP CHIA DƯ CỦA LŨY THỪA SỐ NGUYÊN LỚN.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<string>
+#include<cstdlib>
+using namespace std;
+typedef long long ll;
+ll Du(string  a, ll m) {
+	ll result = 0;
+	for (int i = 0; i < a.size(); i++) {
+		result = (result * 10 + (a[i] - '0')) % m;
+	}return result;
+}
+ll Luy_thua(ll a, ll b,ll m) {
+	ll res = 1;
+	a %= m;
+	while (b) {
+		if (b % 2 == 1) {
+		res *= a;
+		res %= m;
+	}
+	a *= a; a %= m;
+	b /= 2; 
+	}
+	return res%m;
+}
+
+int main() {
+	int t;
+	cin >> t; while (t--) {
+		string a; cin >> a;
+		ll b, c; cin >> b >> c;
+		cout << Luy_thua(Du(a,c), b, c) << endl;
+	}
+
+	
+}
