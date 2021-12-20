e7a6f5b44610721051dedcea3ed371db93480801
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2021-12-20 13:42:36 +0700

    ko cần nhập trước số chuỗi
    Cho dữ liệu vào là luồng văn bản bất kỳ, gồm các ký tự viết hoa, viết thường, các ký tự số và các dấu câu, không có các ký tự đặc biệt khác. Người ta muốn tách văn bản thành các câu. Mỗi câu in trên một dòng.
    
    Một câu được định nghĩa là dãy ký tự có ít nhất 1 ký tự chữ cái hoặc chữ số, không chứa các dấu ngắt câu gồm: dấu chấm (.), dấu chấm hỏi (?) và dấu chấm cảm (!).
    
    Dấu phẩy (,) và dấu hai chấm (:) không được coi là dấu ngắt câu.
    
    Nhiệm vụ của bạn là in ra mỗi câu trên một dòng, ký tự đầu câu viết hoa, các ký tự khác viết thường, các từ cách nhau đúng một khoảng trống. Không có khoảng trống ở đầu và cuối câu, và không in ra các dấu ngắt câu.
    
     
    
    Input
    
    Gồm một luồng văn bản không quá 100 dòng, mỗi dòng không quá 200 ký tự.
    
     
    
    Output
    
    In ra các câu, mỗi câu trên một dòng theo quy tắc đã cho.
    
     
    
    Ví dụ
    
     
    
     
    
    Input
    
    ky thi LAP TRINH ICPC PTIT  bat dau to chuc     tu     nam 2010. nhu vay, nam nay la          tron 10 nam!
    
        vay CO PHAI NAM NAY LA KY THI LAN THU 10?        khong phai! nam nay la KY THI LAN THU 11.
    
    Output
    
    Ky thi lap trinh icpc ptit bat dau to chuc tu nam 2010
    
    Nhu vay, nam nay la tron 10 nam
    
    Vay co phai nam nay la ky thi lan thu 10
    
    Khong phai
    
    Nam nay la ky thi lan thu 11

---
+++ XỬ LÝ VĂN BẢN.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int one = 1;
+void fix_text(string& a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] >= 'A' && a[i] <= 'Z') {
+			a[i] += 32;
+		}
+	}
+}
+int main() {
+	string a; int check = 1;
+	while (cin >> a) {
+		fix_text(a);
+
+		if (check == 1) {
+			a[0] -= 32;
+			check = 0;
+		}
+		if (a[a.size()-1] == '.' || a[a.size() - 1] == '?' || a[a.size() - 1] == '!') {
+			check = 1;
+			a.erase(a.begin() + a.size() - 1); cout << a;
+			cout << endl;
+		}
+		else {
+		cout << a<<" ";
+		}
+	}
+}
+//ky thi LAP TRINH ICPC PTIT  bat dau to chuc     tu     nam 2010. nhu vay, nam nay la          tron 10 nam!
+//      vay CO PHAI NAM NAY LA KY THI LAN THU 10 ? khong phai!nam nay la
+//KY THI LAN THU 11.
