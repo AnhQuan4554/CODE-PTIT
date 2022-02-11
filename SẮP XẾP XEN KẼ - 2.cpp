d34a9651db0fdf8e7e11b554f1a56887f73c087c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-11 21:11:53 +0700

    Update SẮP XẾP XEN KẼ - 2.cpp

--- SẮP XẾP XEN KẼ - 2.cpp
+++ SẮP XẾP XEN KẼ - 2.cpp
@@ -1,47 +1,33 @@
-﻿#include<iostream>
-#include<vector>
-#include<set>
+#include<iostream>
 using namespace std;
-void Nhap(vector<long long>&a, int n) {
+#include<vector>
+#include<algorithm>
+void input(vector<int>& a , int n) {
 	for (int i = 0; i < n; i++) {
-		long long x;
-		cin >> x;
+		int x; cin >> x;
+		
 		a.push_back(x);
-
-	}
-}
-void Sap_xep(vector<long long>& a) {
-	for (long long i = 0; i < a.size()-1; i++) {
-		for (long long j = i + 1; j < a.size(); j++) {
-			if (a[i] < a[j]) {
-				long long temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
-}
-void Xuat(vector<long long>a) {
-	for (int i = 0; i <= (a.size() - 1) / 2; i++) {
-		if (a[i] == a[a.size() - 1 - i]&&i== a.size() - 1 - i) {
-			cout << a[i] << " ";
-		}
-		else {
-			cout << a[i] << " " << a[a.size() - 1 - i]<<" ";
-		}
 	}
 }
 int main() {
-	int t;
-	cin >> t;
+	int t; cin >> t;
 	while (t--) {
-		vector<long long>a;
-		long long n;
-		cin >> n;
-		Nhap(a, n);
-		Sap_xep(a);
-		Xuat(a);
-		cout << endl;
+		int n; cin >> n;
+		vector<int>a;
+		input(a, n);
+		sort(a.begin(),a.end());
+		int end = n % 2 == 0 ? n / 2 - 1 : n / 2;
+		for (int i = 0; i <= end ; i++) {
+			if ((n - i - 1) == i) {
+				cout << a[i];
+				
+			}
+			else {
+			cout << a[n - i - 1] << " " << a[i]<<" ";
+
+			}
+
+		}cout << endl;
 	}
 	system("pause");
 	return 0;
