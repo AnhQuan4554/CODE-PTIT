b50037cade930ae595b30844049beef40bb52852
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:40:38 +0700

    Update SẮP XẾP THEO GIÁ TRỊ TUYỆT ĐỐI.cpp
    Cho mảng A[] gồm n phần tử và số X. Hãy đưa sắp xếp các phần tử của mảng theo trị tuyệt đối của |X - A[i] |. Ví dụ với A[] = {10, 5, 3, 9, 2} và X = 7 ta đưa ra mảng được sắp xếp theo nguyên tắc kể trên: A[] = {5, 9, 10, 3, 2} vì |7-10|=3, |7-5|=2, |7-3|=4, |7-9|=2, |7-2|=5.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n và X; dòng tiếp theo là n số A [i] của mảng A [];các số được viết cách nhau một vài khoảng trống.
    T, n, X thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, X, A[i] ≤105.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5 7
    10 5 3 9 2
    5 6
    1 2 3 4 5
     
    
    5 9 10 3 2
    5 4 3 2 1

--- CTDL&DT/SẮP XẾP THEO GIÁ TRỊ TUYỆT ĐỐI.cpp
+++ CTDL&DT/SẮP XẾP THEO GIÁ TRỊ TUYỆT ĐỐI.cpp
@@ -1,1 +1,36 @@
 
+#include<iostream>
+using namespace std;
+#include<cmath>
+#include<vector>
+#include<algorithm>
+
+void input(vector<pair<int, int>>&a,vector<int>&b, int n,int num) {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		b.push_back(x);
+		a[i].first = abs(num - x);
+		a[i].second = i;
+	}
+}
+
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n,num; cin >> n;
+		cin >> num;
+		vector<pair<int, int>>a(n);
+		vector<int>b;
+		input(a,b, n,num);
+		sort(a.begin(), a.end());
+		for (int i = 0; i < a.size();i++) {
+			cout << b[a[i].second ]<<" ";
+		}
+	
+		
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
