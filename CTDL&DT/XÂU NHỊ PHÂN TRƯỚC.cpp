14b0a3b546b9f766a59cd3ebbbd530345541ec98
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:34:42 +0700

    Create XÂU NHỊ PHÂN TRƯỚC.cpp
    Cho xâu nhị phân X[], nhiệm vụ của bạn là hãy đưa ra xâu nhị phân trước của X[]. Ví dụ X[] =”111111” thì xâu nhị phân trước của X[] là “111110”. Với xâu X[] =“000001” thì xâu nhị trước của X[] là “000000”. Chú ý: nếu xâu dữ liệu trong input là xâu đầu tiên thì trước nó sẽ là xâu cuối cùng.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một xâu nhi phân X.
    T, X[] thỏa mãn ràng buộc: 1≤T≤100; 1≤length(X)≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Ví dụ:
    
     Input
    
    Output
    
    2
    
    010101
    
    111111
    
    010100
    
    111110

---
+++ CTDL&DT/XÂU NHỊ PHÂN TRƯỚC.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+int check = 1;
+
+void handle(string &a) {
+	int i = a.size() - 1;
+	while (i >= 0 && a[i] == '0') {
+		a[i] = '1';
+		i--;
+	}
+	if (i < 0) {
+		check = 0;
+	}
+	else {
+		a[i] = '0';
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		string a; cin >> a;
+
+	
+		handle(a);
+		for (int i = 0; i < a.size(); i++) {
+			cout << a[i];
+		}cout << endl;
+
+	}
+	system("pause");
+	return 0;
+}
