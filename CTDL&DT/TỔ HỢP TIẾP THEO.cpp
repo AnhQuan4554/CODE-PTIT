284081e489222be9e0d93b152703e203e38e1aa7
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:35:09 +0700

    Create TỔ HỢP TIẾP THEO.cpp
    Cho số nguyên dương (1<N<40) và số nguyên dương K<N. Với 1 tổ hợp chập K phần tử của N, hãy cho biết tổ hợp tiếp theo sẽ có bao nhiêu phần tử mới. Nếu tổ hợp đã cho là cuối cùng thì kết quả là K.  
    
    Dữ liệu vào: Dòng đầu ghi số bộ test, không quá 20. Mỗi bộ test viết trên hai dòng
    
    Dòng 1: hai số nguyên dương N và K (K<N)
    Dòng 2 ghi K số của tổ hợp ban đầu. Theo đúng thứ tự tăng dần, không có số nào trùng nhau.
    Kết quả: Với mỗi bộ dữ liệu in ra số lượng phần tử mới.
    
     Ví dụ:
    
    INPUT
    
    OUTPUT
    
    3
    
    5 3
    
    1 3 5
    
    5 3
    
    1 4 5
    
    6 4
    
    3 4 5 6
    
    1
    
    2
    
    4

---
+++ CTDL&DT/TỔ HỢP TIẾP THEO.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<set>
+int check = 1;
+void input(int* a,set<int>&b, int k) {
+	for (int i = 1; i <= k; i++) {
+		cin >> a[i];
+		b.insert(a[i]);
+	}
+}
+void handle(int *a,int n,int k) {
+	int i = k;
+	while (i >= 1 && a[i] == n-k+i) {
+		
+		i--;
+	}
+	if (i <= 0) {
+		check = 0;
+	}
+	else {
+		a[i]++;
+		for (int j = i + 1; j <=k; j++) {
+			a[j] = a[j - 1] + 1;
+		}
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[1000];
+		set<int>b;
+		int n, k; cin >> n >> k;
+		input(a,b, k);
+		handle(a, n, k);
+		int count = 0;
+		for (int i = 1; i <= k; i++) {
+			if (b.count(a[i]) == 0)count++;
+		}
+		if (count != 0)cout << count;
+		else cout << k;
+		cout << endl;
+
+
+	}
+	system("pause");
+	return 0;
+}
