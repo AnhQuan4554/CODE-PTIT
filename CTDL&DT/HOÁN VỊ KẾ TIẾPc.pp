651fd1669e4f1523bbef8a5362468f1de0b584d3
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:30:43 +0700

    Create HOÁN VỊ KẾ TIẾPc.pp
    Cho số tự nhiên N và một hoán vị X[] của 1, 2, .., N. Nhiệm vụ của bạn là đưa ra hoán vị tiếp theo của X[]. Ví dụ N=5, X[] = {1, 2, 3, 4, 5} thì hoán vị tiếp theo của X[] là {1, 2, 3, 5, 4}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất là số N; dòng tiếp theo đưa vào hoán vị X[] của 1, 2, .., N.
    T, N, X[] thỏa mãn ràng buộc: 1≤T≤100; 1≤ N≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input
    
    Output
    
    2
    
    5 
    
    1  2  3  4  5
    
    5
    
    5  4  3  2  1
    
     
    
    1 2  3  5  4
    
    1 2  3  4  5

---
+++ CTDL&DT/HOÁN VỊ KẾ TIẾPc.pp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+int check = 1;
+int check_primary = 1;
+void input(int* a, int n) {
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+}
+void handle(int* a, int n) {
+	int i = n-1;
+	while (a[i] > a[i + 1]&i>=1) {
+		i--;
+	}
+	if (i <= 0) {
+		check = 0;
+	}
+	else {
+		check_primary = 0;
+		int start = n;
+		while (a[i] > a[start]) {
+			start--;
+		}
+		swap(a[i], a[start]);
+		int left = i+1;
+		int right = n;
+		while (left<right)
+		{
+			swap(a[left], a[right]);
+			left++;
+			right--;
+		}
+	}
+}
+int main() {
+
+
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		int a[1002];
+		input(a, n);
+		handle(a, n);
+		if (check_primary) {
+			for (int i = n; i >= 1; i--) {
+				cout << a[i] << " ";
+			}
+		}
+		else {
+
+			for (int i = 1; i <= n; i++) {
+				cout << a[i] << " ";
+			}
+		}cout << endl;
+	}
+	system("pause");
+	return 0;
+}
