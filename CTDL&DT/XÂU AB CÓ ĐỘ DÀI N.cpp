fa2582fc8324e1f2ae4a9c45e2b436bc520b0a77
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:33:41 +0700

    Create XÂU AB CÓ ĐỘ DÀI N.cpp
    Xâu ký tự str được gọi là xâu AB nếu mỗi ký tự trong xâu hoặc là ký tự ‘A’ hoặc là ký tự ‘B’. Ví dụ xâu str=”ABBABB” là xâu AB độ dài 6. Nhiệm vụ của bạn là hãy liệt kê tất cả các xâu AB có độ dài n.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số tự nhiên n.
    T, n thỏa mãn ràng buộc: 1≤T≤10; 1≤n≤10.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng. Mỗi xâu cách nhau 1 khoảng trống.
     
    
    Input
    
    Output
    
    2
    
    2
    
    3
    
    AA AB BA BB
    
    AAA AAB ABA ABB BAA BAB BBA BBB

---
+++ CTDL&DT/XÂU AB CÓ ĐỘ DÀI N.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+using namespace std;
+int check = 1;
+#include<vector>
+void input(string &a, int n) {
+	for (int i = 0; i < n; i++) {
+		a += 'A';
+	}
+}
+void handle(string &a, int n) {
+	int i = n-1;
+	while (i >= 0 && a[i] == 'B') {
+		a[i] = 'A';
+		i--;
+	}
+	if (i < 0) {
+		check = 0;
+	}
+	else {
+		a[i] = 'B';
+	}
+
+	
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string a;		
+		int n; cin >> n;
+		input(a, n);
+		//cout << a;
+		while (check) {
+			for (int i = 0; i < n; i++) {
+				cout << a[i];
+			}cout << " ";
+			handle(a, n);
+			
+		}check = 1;cout << endl;
+	}
+	system("pause");
+	return 0;
+}
