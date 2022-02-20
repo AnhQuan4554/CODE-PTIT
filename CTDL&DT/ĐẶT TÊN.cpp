ae3a2de41db7d85ebf9f4c17a1304b7009b79e1a
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:36:21 +0700

    Create ĐẶT TÊN.cpp
    Vương quốc PTIT sử dụng bảng chữ cái gồm N chữ cái Latinh viết hoa. Quy tắc đặt tên của gia đình Hoàng gia  PTIT là chọn ra K chữ cái (không trùng nhau) và sắp xếp lại theo thứ tự từ điển.
    
    Hãy liệt kê tất cả các cái tên có thể có của gia đình Hoàng gia PTIT
    
    Input
    
    Dòng đầu ghi số bộ test T (không quá 10)
    Mỗi bộ test ghi 2 số N và K (3 < K < N < 16)
    Output
    
    Với mỗi bộ test, ghi ra tất cả các cái tên có thể được tạo ra, mỗi kết quả viết trên một dòng.
    Ví dụ
    
    Input
    
    Output
    
    1
    
    4 2
    
    AB
    
    AC
    
    AD
    
    BC
    
    BD
    
    CD

---
+++ CTDL&DT/ĐẶT TÊN.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int check = 1;
+void input(vector<int>& a, int k) {
+	for (int i = 0; i < k; i++) {
+		a.push_back(i);
+	}
+}
+void handle(vector<int>& a,int n,int k) {
+	int i = k - 1;
+	while (i >= 0 && a[i] == n - k + i ) {//do xet tu 0
+		i--;
+	}
+	if (i < 0) {
+		check = 0;
+	}
+	else {
+		a[i]++;
+		for (int j = i + 1; j < a.size(); j++) {
+			a[j] = a[j - 1] + 1;
+		}
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n,k; cin >> n>>k;
+		vector<int>a;
+		input(a, k);
+		while (check) {
+			for (int i = 0; i < k; i++) {
+				char x = a[i] + 65;
+				cout << x ;
+			}handle(a, n, k);
+			cout << endl;
+
+		}check = 1;
+
+	}
+	system("pause");
+	return 0;
+}
