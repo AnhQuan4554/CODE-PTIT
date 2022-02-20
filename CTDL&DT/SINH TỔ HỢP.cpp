77d688f40f61a8a0d5f28ec304d2b559e9117b66
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:31:09 +0700

    Create SINH TỔ HỢP.cpp
    Cho hai số nguyên dương N và K. Nhiệm vụ của bạn là hãy liệt kê tất cả các tập con K phần tử của 1, 2, .., N. Ví dụ với N=5, K=3 ta có 10 tập con của 1, 2, 3, 4, 5 như sau: {1, 2, 3}, {1, 2, 4},{1, 2, 5},{1, 3, 4},{1, 3, 5},{1, 4, 5},{2, 3, 4},{2, 3, 5},{2, 4, 5},{3, 4, 5}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một cặp số tự nhiên N, K được viết trên một dòng.
    T, n thỏa mãn ràng buộc: 1≤T≤100; 1≤k ≤ n≤15.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input
    
    Output
    
    2
    
    4  3
    
    5  3
    
    123 124 134 234
    
    123 124 125 134 135 145 234 235 245 345

---
+++ CTDL&DT/SINH TỔ HỢP.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+int check = 1;
+
+int check_primary = 1;
+void input(int* a, int k) {
+	for (int i = 1; i <= k; i++) {
+		a[i] = i;
+	}
+}
+void handle(int* a, int n,int k) {
+	int i = k;
+	while (i >= 1 && a[i] == n - k + i) {
+		i--;
+	}
+	if (i <= 0) {
+		check = 0;
+	}
+	else {
+		a[i]++;
+		for (int j = i; j < k; j++) {
+			a[j + 1] = a[j] + 1;
+		}
+	}
+}
+int main() {
+
+
+	int t; cin >> t;
+	while (t--) {
+		int n,k; cin >> n>>k;
+		int a[1002];
+		input(a, n);
+	
+		while (check) {
+			for (int i = 1; i <= k; i++) {
+				cout << a[i];
+			}cout << " ";
+
+			handle(a, n, k);
+
+		}check = 1;
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
