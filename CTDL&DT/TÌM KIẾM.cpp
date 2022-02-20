00591846d79c736e8ac4b1ef55fd6386d3772309
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:49:21 +0700

    Create TÌM KIẾM.cpp
    Cho mảng A[] gồm n phần tử đã được sắp xếp. Hãy đưa ra 1 nếu X có mặt trong mảng A[], ngược lại đưa ra -1.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào n, X là số các phần tử của mảng A[] và số X cần tìm; dòng tiếp theo đưa vào n số A[i] (1≤i≤n) các số được viết cách nhau một vài khoảng trống.
    T, n, A, X thỏa mãn ràng buộc: 1≤T≤100; 1≤N, X, A[i] ≤106.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2 
    5 16
    2 4 7 9 16
    7 98
    1 22 37 47 54 88 96
    
    1
    -1

---
+++ CTDL&DT/TÌM KIẾM.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<map>
+
+int main() {
+	
+	int t; cin >> t;
+	while (t--) {
+		int n, k; cin >> n >> k;
+		set<int>a;
+		for (int i = 0; i< n; i++) {
+			int x; cin >> x;
+			a.insert(x);
+		}
+		int check = 0;
+		for (auto i : a) {
+			if (a.count(k)!=0) {
+				check = 1;
+				break;
+			}
+		}
+		if (check) {
+			cout << "1" << endl;
+		}
+		else {
+			cout << "-1" << endl;
+		}
+	}
+
+	system("pause");
+	return 0;
+ }
