c389867e81a4d62480ccc5ae846643219caeea04
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:34:08 +0700

    Create XÂU NHỊ PHÂN CÓ K BIT 1.cpp
    Hãy in ra tất cả các xâu nhị phân độ dài N, có K bit 1 theo thứ tự từ điển tăng dần.
    
    Input: Dòng đầu tiên là số lượng bộ test T (T ≤ 20). Mỗi test gồm 2 số nguyên N, K (1 ≤ K ≤ N ≤ 16).
    
    Output:  Với mỗi test, in ra đáp án tìm được, mỗi xâu in ra trên một dòng.
    
    Ví dụ:
    
     
    
     
    
     
    
    Input
    
    Output
    
    2
    
    4 2
    
    3 2
    
     
    
    0011
    
    0101
    
    0110
    
    1001
    
    1010
    
    1100
    
    011
    
    101
    
    110

---
+++ CTDL&DT/XÂU NHỊ PHÂN CÓ K BIT 1.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+int check = 1;
+void input(vector<int>& a, int n) {
+	for (int i = 0; i < n; i++) {
+		int x = 0; a.push_back(x);
+	}
+}
+void handle(vector<int>& a) {
+	int i = a.size() - 1;
+	while (i >= 0 && a[i] == 1) {
+		a[i] = 0;
+		i--;
+	}
+	if (i < 0) {
+		check = 0;
+	}
+	else {
+		a[i] = 1;
+	}
+}
+int check2(vector<int >a,int k ) {
+	int count = 0;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] == 1) {
+			count++;
+		}
+	}
+	if (count == k) {
+		return 1;
+	}
+	return 0;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		vector<int>a;
+		int n, k; cin >> n >> k;
+		input(a, n);
+		while (check) {
+			if (check2(a, k)) {
+				for (int i = 0; i < a.size(); i++) {
+					cout << a[i];
+				}
+			cout << endl;
+			}
+		
+			handle(a);
+		}check = 1;
+	}
+	system("pause");
+	return 0;
+}
