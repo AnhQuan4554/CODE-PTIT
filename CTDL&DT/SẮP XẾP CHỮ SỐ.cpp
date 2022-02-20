57c87be33412e011d00a7185f98cfaa4828c0414
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:47:14 +0700

    Create SẮP XẾP CHỮ SỐ.cpp
    Cho mảng A[] gồm n phần tử. Nhiệm vụ của bạn là đưa ra mảng đã được sắp xếp bao gồm các chữ số của mỗi phần tử trong A[]. Ví dụ A[] = {110, 111, 112, 113, 114 }ta có kết quả là {0, 1, 2, 3, 4}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[]; dòng tiếp theo là n số A[i] ; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤107; 0≤ A[i] ≤1016.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    3
    131 11 48
    4
    111 222 333 446
    
    1 3 4 8
    1 2 3 4 6

---
+++ CTDL&DT/SẮP XẾP CHỮ SỐ.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+#include<set>
+void devide(string a,set<char>&b) {
+	for (int i = 0; i < a.size(); i++) {
+		b.insert(a[i]);
+	}
+}
+int main() {
+	
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		set<char>b;
+		for (int i = 0; i < n; i++) {
+			string a; cin >> a;
+			devide(a, b);
+		}
+		for (auto i : b) {
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+ }
