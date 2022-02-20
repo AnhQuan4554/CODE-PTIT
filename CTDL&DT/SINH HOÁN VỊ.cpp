59830c84442f309328532c7e52e2dc7d8dd3707a
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:32:14 +0700

    Create SINH HOÁN VỊ.cpp
    Cho số nguyên dương N. Nhiệm vụ của bạn là hãy liệt kê tất cả các hoán vị của 1, 2, .., N. Ví dụ với N = 3 ta có kết quả: 123, 132, 213, 231, 312, 321.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số tự nhiên N được viết trên một dòng.
    T, n thỏa mãn ràng buộc: 1≤T, N≤10.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input
    
    Output
    
    2
    
    2
    
    3
    
    12  21
    
    123 132  213  231  312  321

---
+++ CTDL&DT/SINH HOÁN VỊ.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+int check = 1;
+
+int check_primary = 1;
+void input(int* a, int n) {
+	for (int i = 1; i <= n; i++) {
+		a[i] = i;
+	}
+}
+void handle(int* a, int n) {
+	int i = n - 1;
+	while (i >= 1 && a[i] > a[i + 1]) {
+		i--;
+	}
+	if (i <= 0) {
+		check = 0;
+	}
+	else {
+		int start = n;
+		while (a[i] > a[start]) {
+			start--;
+		}
+		swap(a[i], a[start]);
+		int left = i + 1;
+		int right = n;
+		while (left < right) {
+			swap(a[left], a[right]);
+			left++;
+			right--;
+		}
+	}
+}
+int main() {
+
+
+	int t; cin >> t;
+	while (t--) {
+		int n,k; cin >> n;
+		int a[1002];
+		input(a, n);
+	
+		while (check) {
+			for (int i = 1; i <= n; i++) {
+				cout << a[i];
+			}cout << " ";
+
+			handle(a, n);
+
+		}check = 1;
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
