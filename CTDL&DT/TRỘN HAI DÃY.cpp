42adda5a18020570beb68ececad4fd273ae90720
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:48:59 +0700

    Create TRỘN HAI DÃY.cpp
    Cho mảng A[] gồm n phần tử và mảng B[] gồm m phần tử. Nhiệm vụ của bạn là hợp nhất hai mảng A[] và B[] để được một mảng mới đã được sắp xếp. Ví dụ A[] = {5, 7, 112, 9, 3, 6, 2 }, B[] = {1, 2, 6, -1, 0, 9} ta có kết quả là C[] = {-1, 1, 0, 2, 3, 5, 6, 6, 7, .
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm ba dòng: dòng đầu tiên đưa vào n, m tương ứng với số phần tử của mảng A[] và B[]; dòng tiếp theo là n số A[i] ; dòng cuối cùng là m số B[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, m, A[i], B[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, m ≤106; -108≤ A[i] ≤108.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    1
    
    3 3
    
    10 5 15
    
    20 3 2
    
    2 3 5 10 15 20

---
+++ CTDL&DT/TRỘN HAI DÃY.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+void input(vector<int>& a, vector<int>& c,int n) {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		a.push_back(x); c.push_back(x);
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n,m; cin >> n>>m;
+		vector<int>a;
+		vector<int>b;
+		vector<int>c;
+		input(a, c,n);
+		input(b, c, m);
+		
+		sort(c.begin(), c.end());
+		for (int i = 0; i < c.size(); i++) {
+			cout << c[i] << " ";
+		}cout << endl;
+	}
+	system("pause");
+	return 0;
+ }
