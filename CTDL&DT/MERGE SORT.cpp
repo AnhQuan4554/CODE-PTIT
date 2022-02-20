b79bd83ebd2f29cb6c114d0f8c073d0cc24b432b
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-20 15:48:35 +0700

    Create MERGE SORT.cpp
    Cho mảng A[] gồm N phần tử chưa được sắp xếp. Nhiệm vụ của bạn là sắp xếp các phần tử của mảng A[] theo thứ tự tăng dần bằng thuật toán Merge Sort.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai phần: phần thứ nhất đưa vào số N tương ứng với số phần tử của mảng A[]; phần thứ 2 là N số của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤N, A[i]≤106.
    Output:
    
    Đưa ra kết quả các test theo từng dòng.
    Input
    
    Output
    
    2
    5
    4 1 3 9 7
    10
    10 9 8 7 6 5 4 3 2 1
    
    1 3 4 7 9
    1 2 3 4 5 6 7 8 9 10

---
+++ CTDL&DT/MERGE SORT.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		vector<int>a;
+		for (int i = 0; i < n; i++) {
+			int x; cin >> x;
+			a.push_back(x);
+		}
+		sort(a.begin(), a.end());
+		for (int i = 0; i < a.size(); i++) {
+			cout << a[i] << " ";
+		}cout << endl;
+	}
+	system("pause");
+	return 0;
+ }
