385bce2270686b6789ce666049ebd4df3285d4fc
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-25 18:12:21 +0700

    Sinh tổ hợp kết hợp vector<pair>
    Kỳ thi ICPC có K đội của PTIT tham gia và đội tuyển đang rất đau đầu không biết chọn các cái tên như thế nào cho các đội. Yêu cầu phải đảm bảo tên không có khoảng trống và không được trùng nhau. Sau khi thảo luận, có N cái tên được đề xuất (có thể bị trùng nhau). Với K<15 và 4 < N < 30.
    
    Hãy liệt kê tất cả danh sách các tổ hợp K cái tên khác nhau có thể được tạo ra theo thứ tự từ điển.
    
    Input
    
    Dòng đầu ghi 2 số N và K.
    
    Tiếp theo là 1 dòng ghi N cái tên, mỗi cái tên có độ dài không quá 15 và cách nhau một khoảng trống. Tất cả đều là ký tự in hoa.
    
    Output
    
    Ghi ra tất cả các tổ hợp tên có thể được lựa chọn theo thứ tự từ điển.
    
    Tức là các tên trong mỗi tổ hợp liệt kê theo thứ tự từ điển và các tổ hợp cũng được liệt kê theo thứ tự từ điển.
    
    Ví dụ
    
    Input
    
    Output
    
    6 2
    
    DONG TAY NAM BAC TAY BAC
    
    BAC DONG
    
    BAC NAM
    
    BAC TAY
    
    DONG NAM
    
    DONG TAY
    
    NAM TAY

---
+++ CTDL&DT/ĐẶT TÊN - 1.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<set>
+int check = 1;
+void input(int* a, int k) {
+	for (int i = 1; i <= k; i++) {
+		a[i] = i;
+	}
+}
+void handle(int* a, int n,int k) {
+	int i = k ;
+	while (i > 0 && a[i] == n - k + i ) {
+		i--;
+	}
+	if (i <= 0) {
+		check = 0;
+	}
+	else {
+		a[i]++;
+		int start = i + 1;
+		for (; start <= k; start++) {
+			a[start]=a[start-1]+1;
+		}
+	}
+
+}
+int main() {
+	int n, k; cin >> n >> k;
+	set<string>a;
+	vector<pair<string, int>>b(1000);
+	while (n--) {
+		string x;
+		cin >> x;
+		a.insert(x);
+	}
+	int dem = 1;
+	for (auto i : a) {
+
+		b[dem].first = i;
+		b[dem].second = dem;
+		dem++;
+	}
+	/*for (int i = 1; i < dem; i++) {
+		cout << b[i].first << " "<<b[i].second;
+		cout << endl;
+	}*/
+	int d[1000];
+	input(d, k);
+	while (check) {
+		for (int i = 1; i <= k; i++) {
+		/*	cout << d[i];
+			cout << " " << "cua b";*/
+			cout << b[d[i]].first << " ";
+		}cout << endl;
+		handle(d, dem-1, k);
+	}
+	system("pause");
+	return 0;
+}
