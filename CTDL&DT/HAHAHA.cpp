3b4e78444950b4dce5b0bcd6e82d69374082f934
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-02-25 16:50:19 +0700

    Create HAHAHA.cpp
    Khi chat chit vui vẻ, anh em chiến hữu thường hay gõ HAHA để thể hiện sự sảng khoái. Đôi khi cũng có thể gõ HAHAAAAA chẳng hạn cho thêm phần nhấn mạnh.
    
    Giả sử một xâu ký tự được coi là HAHA nếu thỏa mãn các điều kiện:
    
    Ký tự đầu tiên phải là chữ H, ký tự cuối cùng phải là chữ A
    Không có hai chữ H nào liền nhau
    Cho trước độ dài N, hãy liệt kê tất cả các xâu ký tự HAHA theo thứ tự từ điển.
    
    Input
    
    Dòng đầu ghi số bộ test T (không quá 10)
    Mỗi bộ test ghi độ dài N (2 <= N < 16)
    Output
    
    Với mỗi bộ test, ghi ra tất cả các xâu HAHA tìm được theo thứ tự từ điển, mỗi xâu viết trên một dòng.
    Ví dụ
    
    Input
    
    Output
    
    2
    
    2
    
    4
    
    HA
    
    HAAA
    
    HAHA

---
+++ CTDL&DT/HAHAHA.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+int check = 1;
+
+void input(string &a, int n) {
+	for (int i = 0; i < n; i++) {
+		a+='A';
+	}
+}
+void handle(string &a, int n) {
+	int  i = n - 1;
+	while (i >= 0 && a[i] == 'H') {
+		a[i] = 'A';
+		i--;
+	}
+	if (i < 0) {
+		check = 0;
+	}
+	else {
+		a[i] = 'H';
+	}
+}
+int check2(string a, int n) {
+	if (a[0] != 'H' || a[n - 1] != 'A')return 0;
+	for (int i = 0; i < a.length()-1; i++) {
+		if (a[i] == a[i + 1]&&a[i]=='H')return 0;
+	}
+	return 1;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		string  a;
+		int n; cin >> n;
+		input(a, n);
+		
+		while (check)
+		{
+			if (check2(a,n)) {
+
+				cout << a << endl;
+			}
+			handle(a, n);
+		}check = 1;
+	}
+	system("pause");
+	return 0;
+}
