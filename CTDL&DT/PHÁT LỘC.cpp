ee27b0266439ddea1e860aacd154389b834cae78
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-01 14:26:21 +0700

    Create PHÁT LỘC.cpp
    Một xâu ký tự số chỉ bao gồm các chữ số 6 và 8 sẽ được gọi là “phát lộc” nếu thỏa mãn các điều kiện sau:
    
    Độ dài xâu ít nhất là 6
    Chữ số đầu tiên là chữ số 8, chữ số cuối cùng là chữ số 6
    Không có 2 chữ số 8 nào ở cạnh nhau
    Không có nhiều hơn 3 chữ số 6 ở cạnh nhau.
    Viết chương trình liệt kê các xâu ký tự phát lộc độ dài N theo thứ tự tăng dần.
    
    Input
    
    Chỉ có 1 dòng ghi số N (5 < N < 16).
    
    Output
    
    Ghi ra các xâu ký tự phát lộc độ dài N, mỗi xâu trên một dòng.
    
    Ví dụ
    
    Input
    
    Output
    
    6
    
    866686
    
    866866
    
    868666
    
    868686

---
+++ CTDL&DT/PHÁT LỘC.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+using namespace std;
+#include<string>
+int check = 1;
+#include<set>
+void input(string& a, int n) {
+	for (int i = 0; i < n; i++) {
+		a += '8';
+	}
+}
+void handle(string& a) {
+	int i = a.length() - 1;
+	while (i >= 0 && a[i] == '6') {
+		a[i] = '8';
+		i--;
+	}
+	if (i < 0)check = 0;
+	else {
+		a[i] = '6';
+	}
+}
+int check2(string a){
+	if (a[0] != '8')return 0;
+	if (a[a.length() - 1] != '6')return 0;
+	for (int i = 0; i < a.length()-1; i++) {
+		if (a[i] == a[i + 1] && a[i] == '8')return 0;
+	}
+	for (int i = 0; i < a.length() - 3; i++) {
+		if (a[i] == a[i + 1] && a[i + 1] == a[i + 2]&&a[i+2]==a[i+3] && a[i] == '6')return	0;
+	}
+	return 1;
+}
+int main() {
+	string a;
+	int n; cin >> n;
+	input(a, n);
+	set<string>b;
+	while (check)
+	{
+		if (check2(a)) {
+			b.insert(a);
+		/*for (int i = 0; i < a.length(); i++) {
+			cout << a[i];
+		}cout << endl;*/
+
+		}
+		handle(a);
+	}
+	for (auto i : b) {
+		cout << i << endl;
+	}
+	system("pause");
+	return 0; 
+}
