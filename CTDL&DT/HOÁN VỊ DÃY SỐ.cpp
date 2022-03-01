435e02c3c387caa46b4849caf74e979e73a44456
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-01 14:47:11 +0700

    Create HOÁN VỊ DÃY SỐ.cpp
    Cho dãy số A[] có N phần tử là các số nguyên dương khác nhau từng đôi một. Hãy liệt kê tất cả các hoán vị của dãy số A[] theo thứ tự tăng dần, tức là hoán vị đầu tiên có giá trị tăng dần từ trái qua phải, hoán vị cuối cùng giảm dần từ trái qua phải.
    
    Input
    
    Dòng đầu ghi số N (1 < N < 9)
    
    Dòng thứ 2 ghi N số của dãy A[] (0 < A[i] < 10000)
    
    Output
    
    Ghi mỗi hoán vị của dãy số trên một dòng
    
    Ví dụ
    
    Input
    
    Output
    
    3
    
    88 77 99
    
    77 88 99
    
    77 99 88
    
    88 77 99
    
    88 99 77
    
    99 77 88
    
    99 88 77

---
+++ CTDL&DT/HOÁN VỊ DÃY SỐ.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+int check = 1;
+void input(int* a, int n) {
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+}
+void handle(int* a, int n) {
+	int i = n - 2;
+	while (i >= 0 && a[i] > a[i + 1]) {
+		i--;
+	}
+	if (i < 0)check = 0;
+	else {
+		int k = n - 1;
+		while (a[k] < a[i]) {
+			k--;
+		}swap(a[k], a[i]);
+		int left = i + 1, right = n - 1;
+		while (left < right) {
+			swap(a[left], a[right]);
+			left++;
+			right--;
+		}
+	}
+}
+int main() {
+	int n; cin >> n;
+	int a[1000];
+	input(a, n);
+	sort(a, a + n);
+	while (check) {
+		for (int i = 0; i < n; i++) {
+			cout << a[i] << " ";
+		}handle(a, n); cout << endl;
+	}
+	system("pause");
+	return 0;
+}
