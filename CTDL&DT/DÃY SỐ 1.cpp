902375e02c40334ac9f3d4ce47ec888d49abecc9
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-07 12:02:54 +0700

    QUay lui
    Cho dãy số A[] gồm n số nguyên dương. Tam giác đặc biệt của dãy số A[] là tam giác được tạo ra bởi n hàng, trong đó hàng thứ 1 là dãy số A[], hàng i là tổng hai phần tử liên tiếp của hàng i-1 (2≤i≤n). Ví dụ A[] = {1, 2, 3, 4, 5}, khi đó tam giác được tạo nên như dưới đây:
    
    [1, 2, 3, 4, 5 ]
    
    [3, 5, 7, 9 ]
    
    [8, 12, 16]
    
    [20, 28]
    
    [48]
    
                Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng tiếp theo đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào N là số lượng phần tử của dãy số A[]; dòng tiếp theo đưa vào N số của mảng A[].
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100; 1≤N, A[i] ≤10;
    Output:
    
    Đưa ra tam giác tổng của mỗi test theo từng dòng. Mỗi dòng của tam giác tổng được bao bởi ký tự [, ].
    Input
    
    Output
    
    1
    
    5
    
    1 2 3 4 5
    
    [1 2 3 4 5]
    
    [3 5 7 9]
    
    [8 12 16]
    
    [20 28]
    
    [48]

---
+++ CTDL&DT/DÃY SỐ 1.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+using namespace std;
+void input(int** a, int n) {
+	for (int j = 1; j <= n; j++) {
+		cin >> a[1][j];
+		
+	}
+}
+void handle(int** a, int n) {
+	int cot = n;
+	for (int i = 2; i <= n; i++) {
+		cot--;
+		for (int j = 1; j <= cot; j++) {
+			a[i][j] = a[i - 1][j] + a[i - 1][j + 1];
+		}
+	}
+}
+void export1 (int** a, int n) {
+	int cot = n;
+	for (int i = 1; i <= n; i++) {
+		cout << "[";
+		for (int j = 1; j < cot; j++) {
+			cout << a[i][j]<<" ";
+		}
+		cout << a[i][cot] << "]";
+		cot--;
+		cout << endl;
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+int** a = new int* [100];
+	for (int i = 0; i < 100; i++)
+		a[i] = new int[100];
+	int n; cin >> n;
+	input(a, n);
+	
+	handle(a, n);
+	export1(a, n);
+	}
+	
+	system("pause");
+	return 0;
+}
