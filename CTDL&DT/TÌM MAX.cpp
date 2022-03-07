1cde8af147bb90452dafddf61e84c0c16a5b7c26
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-07 09:22:44 +0700

    Kĩ thuật Tham Lam
    Cho mảng A[] gồm N phần tử.Nhiệm vụ của bạn là tìm  bằng cách sắp đặt lại các phần tử trong mảng. Chú ý, kết quả của bài toán có thể rất lớn vì vậy bạn hãy đưa ra kết quả lấy modulo với 109+7.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm 2 dòng: dòng thứ nhất đưa vào số phần tử của mảng N; dòng tiếp theo đưa vào N số A[i] tương ứng với các phần tử của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤N, A[i] ≤107.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Ví dụ:
    
    Input
    
    Output
    
    2
    5
    5 3 2 4 1
    3
    1 2 3
    
    40
    8

---
+++ CTDL&DT/TÌM MAX.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<vector>
+long long   modu = 1000000007;
+void input(vector<int>& a, int n) {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		a.push_back(x);
+	}
+}
+int Sum(vector<int>a) {
+	long long  sum = 0;
+	for (int i = 0; i < a.size(); i++) {
+		sum += a[i]*i ;
+		sum %= modu;
+	}
+	return sum;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		vector<int>a;
+		int n; cin >> n;
+		input(a, n);
+		sort(a.begin(), a.end());
+		/*for (auto i : a)cout << i << " ";*/
+		cout << Sum(a) << endl;
+	}
+}
