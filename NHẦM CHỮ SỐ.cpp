f0b2795a0873d4b8dba30464be990ad5dd6a421e
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-07 09:07:25 +0700

    kĩ thuật Tham Lam
    Trong một buổi học toán, giáo viên viết 2 số nguyên, A và B, và yêu cầu Tèo thực hiện phép cộng. Tèo không bao giờ tính toán sai, nhưng thỉnh thoảng cậu ta chép các con số một cách không chính xác. Lỗi duy nhất của là ghi nhầm '5' thành '6' hoặc ngược lại.  Cho hai số, A và B, tính tổng nhỏ nhất và lớn nhất mà Tèo có thể nhận được.
    
    Input: Có một dòng chứa hai số nguyên dương A và B ( 1 ≤ A, B ≤ 1 000 000).
    
    Output: In ra 2 số nguyên cách nhau một dấu cách, tổng nhỏ nhất và lớn nhất có thể nhận được.
    
    Ví dụ:
    
    Test 1
    
    Test 2
    
    Test 3
    
    Input:
    11 25
    
    Ouput:
    36 37
    
    Input:
    1430 4862
    
    Ouput:
    6282 6292
    
    Input:
    16796 58786
    
    Ouput:
    74580 85582

---
+++ NHẦM CHỮ SỐ.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+int numMin(string a) {
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] == '6')a[i] = '5';
+	}
+	long long sum = 0;
+	for (int i = 0; i < a.length(); i++) {
+		sum = sum*10+ (a[i] - '0');
+	}
+	return sum;
+}
+int numMax(string a) {
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] == '5')a[i] = '6';
+	}
+	long long sum = 0;
+	for (int i = 0; i < a.length(); i++) {
+		sum = sum * 10 + (a[i] - '0');
+	}
+	return sum;
+}
+int main() {
+	
+		string a, b; cin >> a >> b;
+		
+		cout << numMin(a) + numMin(b) << " " << numMax(a) + numMax(b) << endl;;
+	
+	system("pause");
+	return  0;
+}
