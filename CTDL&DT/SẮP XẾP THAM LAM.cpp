da0b57bfc47cb260e34512cfae10744f21876696
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-07 10:53:29 +0700

    bài này yêu cầu trường hợp số lẻ thôi
    Cho mảng A[] gồm N số và thực hiện các thao tác theo nguyên tắc dưới đây:
    
    Ta chọn một mảng con sao cho phần tử ở giữa của mảng con cũng là phần tử ở giữa của mảng A[] (trong trường hợp N lẻ).
    Đảo ngược mảng con đã chọn trong mảng A[]. Ta được phép chọn mảng con và phép đảo ngược mảng con bao nhiêu lần tùy ý.
    Ví dụ với mảng A[] = {1, 6, 3, 4, 5, 2, 7} ta có câu trả lời là Yes vì: ta chọn mảng con {3, 4, 5} và đảo ngược để nhận được mảng A[]={1, 6, 5, 4, 3, 2, 7}, chọn tiếp mảng con {6, 5, 4, 3, 2} và đảo ngược ta nhận được mảng A[]={1, 2, 3, 4, 5, 6, 7}. Hãy cho biết ta có thể sắp xếp được mảng A[] bằng cách thực hiện các thao tác kể trên hay không?
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm 2 dòng: dòng thứ nhất đưa vào số phần tử của mảng N; dòng tiếp theo đưa vào N số A[i] tương ứng với các phần tử của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤ N ≤50; 0≤A[i]≤1000.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.

---
+++ CTDL&DT/SẮP XẾP THAM LAM.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+
+void input(int * a, int n,int *b) {
+	for (int i = 0; i < n; i++) {
+		cin >> a[i]; b[i] = a[i];
+	}
+}
+int check(int* a, int* b, int n) {
+	for (int i = 0; i <= n / 2;i++) {
+		if (a[i] != b[i] && b[i] != a[n - i - 1])return 0;
+	}
+	return 1;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[100];
+		int b[100];
+		
+		int n; cin >> n;
+		input(a, n,b);
+		sort(b, b+n); //do o day mk dung mang bat dau tu 1 
+		/*for (int i = 0; i < n; i++) {
+			cout << a[i] << " ";
+		}cout << endl;
+		for (int i = 0; i < n; i++) {
+			cout << b[i] << " ";
+		}*/
+		if (check(a, b, n))cout << "Yes" << endl;
+		else cout << "No" << endl;
+	}
+}
