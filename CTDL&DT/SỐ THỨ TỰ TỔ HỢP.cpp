e8b5cf7abdb6790ee650b95a78774d506b333af5
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-08 08:12:58 +0700

    đoạn break hơi ảo nên xem lại
    Cho một tổ hợp chập K của N số nguyên dương đầu tiên (2 < K < N < 15).
    
    Hãy xác định xem đó là tổ hợp thứ bao nhiêu nếu liệt kê tất cả các tổ hợp theo thứ tự tăng dần (tính từ 1).
    
    Input
    
    Dòng đầu ghi số T là số bộ test (T < 10)
    
    Mỗi bộ test gồm 2 dòng
    
    Dòng đầu ghi 2 số nguyên dương N và K (2 < K < N < 15)
    Dòng tiếp theo ghi một tổ hợp chập K của các số nguyên dương từ 1 đến N.  
    Output
    
    Với mỗi bộ test, ghi ra trên một dòng số thứ tự của tổ hợp (tính từ 1, theo thứ tự liệt kê tăng dần).
    
    Ví dụ
    
    Input
    
    Output
    
    2
    
    6 4
    
    1 3 5 6
    
    6 4
    
    2 3 4 6
    
    9
    
    12

---
+++ CTDL&DT/SỐ THỨ TỰ TỔ HỢP.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+int check = 1;
+
+void input(int*b, int k) {
+	for (int i = 0; i < k; i++) {
+		cin >> b[i]; 
+		
+	}
+}
+void handle(int* a, int n,int k  ) {
+	int i = k - 1;
+	while (i >= 0 && a[i] == n - k +i+ 1) {
+		i--;
+	}
+	if (i < 0)check = 0;
+	else {
+		a[i]++;
+		for (int x = i + 1; x < k; x++) {
+			a[x] = a[x - 1] + 1;
+		}
+	}
+}
+int check2(int* a, int* b, int k) {
+	for (int i = 0; i < k; i++) {
+		if (a[i] != b[i])return  0;
+	}return 1;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n, k; cin >> n >> k;
+		int a[1000];
+		int b[1000];
+		for (int i = 0; i< k; i++) {
+			a[i] = i + 1;
+		}
+		input( b,k);
+		int count = 0;
+		while (check) {
+			if (check2(a, b, k) == 0) {
+				count++;
+				handle(a, n, k);
+			}
+			else break;
+			
+		}cout << count+1 << endl;// lần cuối cùng bằng nhau
+		check = 1;
+	}
+	system("pause");
+	return 0;
+}
