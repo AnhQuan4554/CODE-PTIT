61b58ba26a6ebb00ad124ddcd2d0c9a951bb01b0
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-09 21:02:15 +0700

    Quay lui + vectow hay nên xem lại
    Cho dãy số A[] gồm n số nguyên dương. Tam giác đặc biệt của dãy số A[] là tam giác được tạo ra bởi n hàng, trong đó hàng thứ n là dãy số A[], hàng i là tổng hai phần tử liên tiếp của hàng i+1 (1≤i≤n-1). Ví dụ A[] = {1, 2, 3, 4, 5}, khi đó tam giác được tạo nên như dưới đây:
    
    [48]
    
    [20, 28]
    
    [8, 12, 16]     
    
    [3, 5, 7, 9 ]
    
    [1, 2, 3, 4, 5 ]
    
                Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng tiếp theo đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào N là số lượng phần tử của dãy số A[]; dòng tiếp theo đưa vào N số của mảng A[].
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100; 1≤N, A[i] ≤10;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng. Mỗi dòng của tam giác tổng được bao bởi ký tự [, ].
    Input
    
    Output
    
    1
    
    5
    
    1 2 3 4 5
    
    [48] [20 28] [8 12 16] [3 5 7 9 ] [1 2 3 4 5 ]

--- DÃY SỐ 2.cpp
+++ DÃY SỐ 2.cpp
@@ -1,1 +1,63 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std; 
+void input(int** a, int n) {
+	for (int j = 1; j <= n; j++) {
+		cin >> a[1][j];
+	}
+}
+void handle(int** a, int n) { 
+	int cot = n - 1;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= cot; j++) {
+			a[i][j] = a[i - 1][j] + a[i-1][j + 1];
+		}cot--;
+	}
+}
+void qexport(int** a, int n) {
+	int cot = n;
+	vector<vector<int>>b;
+	for (int i = 1; i <= n; i++) {
+		vector<int>c;
 
+		for (int j = 1; j <= cot; j++) {
+			//cout << a[i][j] << " ";
+			c.push_back(a[i][j]);
+			
+		}//cout << endl;
+		cot--;
+		
+		//for (int x = 0; x < c.size();x++)cout << c[x] << " ";
+		b.push_back(c);
+	}
+
+	//xuaats 
+	for (int i = b.size() - 1; i >= 0; i--) {
+		cout << "[";
+		for (int j = 0; j < b[i].size()-1; j++) {
+			cout << b[i][j]<<" ";
+		}
+		cout<<b[i][b[i].size()-1] << "] ";
+	}
+	
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+
+		int** a = new int* [n + 1];
+		for (int i = 0; i < n + 1; i++) {
+
+			a[i] = new int[n + 1];
+		}
+		input(a, n);
+		handle(a, n);
+		qexport(a, n);
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
