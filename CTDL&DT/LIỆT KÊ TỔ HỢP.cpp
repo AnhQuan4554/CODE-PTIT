ef2f9de787e0f7f347d17a14f0ff35e6e4964192
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-10 09:31:30 +0700

    Xuất theo thứ tự  các số nhập sẵn
    Cho dãy số A[] có N phần tử. Hãy liệt kê tất cả các tổ hợp chập K của tập các phần tử khác nhau trong A[]. Các tổ hợp cần liệt kê theo thứ tự từ điển (tức là trong mỗi tổ hợp thì giá trị từ nhỏ đến lớn, và tổ hợp sau lớn hơn tổ hợp trước).
    
    Input
    
    Dòng đầu ghi hai số N và K.
    
    Dòng thứ 2 ghi N số của mảng A[]. Các giá trị không quá 1000.
    
    Dữ liệu đảm bảo số phần tử khác nhau của A[] không quá 20 và K không quá 10.
    
    Output
    
    Ghi ra lần lượt các tổ hợp tìm được, mỗi tổ hợp trên một dòng.
    
    Ví dụ
    
    Input
    
    Output
    
    8 3
    
    2 4 4 3 5 1 3 4
    
    1 2 3
    
    1 2 4
    
    1 2 5
    
    1 3 4
    
    1 3 5
    
    1 4 5
    
    2 3 4
    
    2 3 5
    
    2 4 5
    
    3 4 5

---
+++ CTDL&DT/LIỆT KÊ TỔ HỢP.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+int c[100];
+set<int>b;
+vector<int>a;
+void input(set<int>& b, vector<int>& a, int n) {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		b.insert(x);
+	}
+	a.push_back(0);
+	for (auto i : b) {
+		a.push_back(i);
+	}
+}
+void Try(int i, int n, int k) {
+	for (int j = c[i - 1] + 1; j <= n - k + i; j++) {
+		c[i] = j;
+		if (i == k) {
+			for (int x = 1; x <= k; x++) {
+				cout << a[c[x]] << " ";
+			}cout << endl;
+		}
+		else {
+			Try(i + 1, n, k);
+		}
+	}
+}
+int main() {
+	int n, k; cin >> n >> k;
+	
+	input(b, a, n); 
+	c[0] = 0;
+	/*for (int i = 1; i < a.size(); i++) {
+		cout << a[i];
+	}*/
+	Try(1, a.size() - 1, k);
+	system("pause");
+	return 0;
+}
