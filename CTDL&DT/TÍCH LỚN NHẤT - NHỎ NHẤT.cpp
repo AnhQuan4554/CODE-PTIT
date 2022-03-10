af26e4cf4580c4fb5bb32100c9f863e2858e38a9
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-10 12:13:45 +0700

    chú ý kiểu dữ liệu
    Cho mảng A[] gồm n phần tử và mảng B[] gồm m phần tử. Nhiệm vụ của bạn là tìm tích giữa phần tử lớn nhất của mảng A[] và phần tử nhỏ nhất của mảng B[]. Ví dụ A[] = {5, 7, 112, 9, 3, 6, 2 }, B[] = {1, 2, 6, -1, 0, 9} ta có kết quả là -9 = 9*(-1).
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm ba dòng: dòng đầu tiên đưa vào n, m tương ứng với số phần tử của mảng A[] và B[]; dòng tiếp theo là n số A[i] ; dòng cuối cùng là m số B[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, m, A[i], B[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n, m ≤106; -108≤ A[i] ≤108.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    6 6
    5 7 9 3 6 2
    1 2 6 -1 0 9
    6 6
    
    1 4 2 3 10 2
    4 2 6 5 2 9
    
    -9
    20

---
+++ CTDL&DT/TÍCH LỚN NHẤT - NHỎ NHẤT.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+typedef long long ll;
+void input(vector<ll>& a, long long n) {
+	for (ll i = 0; i < n; i++) {
+		int x; cin >> x;
+		a.push_back(x);
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		vector<ll>a;
+		vector<ll>b;
+		int n, m;
+		cin >> n >> m;
+		input(a, n);
+		input(b, n);
+		sort(a.begin(), a.end());
+		sort(b.begin(), b.end());
+		cout << a[a.size() - 1] * b[0] << endl;
+	}
+	system("pause");
+	return 0;
+}
