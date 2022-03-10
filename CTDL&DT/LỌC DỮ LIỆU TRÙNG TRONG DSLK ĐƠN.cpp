976f8e4941ff64847ce71beda84b36c9058a450c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-10 10:19:16 +0700

    Create LỌC DỮ LIỆU TRÙNG TRONG DSLK ĐƠN.cpp
    Cho danh sách liên kết đơn lưu giữ các số nguyên được quản lý bởi con trỏ First. Viết chương trình con lọc tất cả các phần tử có giá trị trùng nhau trong danh sách liên kết đơn First, chỉ để lại 1 phần tử đại diện cho nhóm trùng. Sau khi lọc xong, liệt kê các phần tử  trong danh sách liên kết đơn First.
    
    Ví dụ: Ta có Input :
    
    12 : là số lượng phân tử trong danh sách
    
    1 1 1 4  5 1 4  7 7 8 1 9 : là số phần tử
    
    Output : 1 4 5 7 8 9

---
+++ CTDL&DT/LỌC DỮ LIỆU TRÙNG TRONG DSLK ĐƠN.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<set>
+#include<vector>
+using namespace std;
+int main() {
+	int n; cin >> n;
+
+	int a[1000];
+	int b[1000];
+	for (int i = 0; i < n; i++) {
+		int x; cin >> a[i];
+		b[a[i]]= 1 ;
+	}
+	for (int i = 0; i < n; i++) {
+		if (b[a[i]] != 0)cout << a[i]<<" ";
+		b[a[i]] = 0;
+	}
+	system("pause");
+	return 0;
+}
+//1 1 1 4 5 1 4 7 7 8 1 9
