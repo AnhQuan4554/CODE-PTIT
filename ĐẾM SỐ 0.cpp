ee9822bfb28845633c42387ead41a9750485bc34
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-11 11:26:26 +0700

    Create ĐẾM SỐ 0.cpp
    Cho mảng A[] gồm N phần tử chỉ bao gồm các số 0 và 1. Các số 0 được đặt trước các số 1. Hãy đếm các số 0 với thời gian log(N).
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm 2 dòng: dòng thứ nhất đưa vào số N; dòng tiếp theo đưa vào N số của mảng A[]; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤ N≤1000; 0≤ A[i]≤1.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
      Ví dụ:
    
    Input
    
    Output
    
    3
    12
    0 0 0 0 0 0 0 0 0 1 1 1 
    5
    0 0 0 0 0
    6
    1 1 1 1 1 1
    
    9
    5
    0

---
+++ ĐẾM SỐ 0.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		int a[1008];
+		int count = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+			if (a[i] == 0)count++;
+		}
+		cout << count << endl;
+	}
+		system("pause");
+		return 0;
+}
