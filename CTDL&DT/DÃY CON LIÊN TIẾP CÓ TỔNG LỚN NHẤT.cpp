40b48057a61146aacd157333e063affb3041a0b0
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-14 10:50:48 +0700

    Create DÃY CON LIÊN TIẾP CÓ TỔNG LỚN NHẤT.cpp
    Cho mảng A[] gồm N số có cả các số âm và số dương. Nhiệm vụ của bạn là tìm mảng con liên tục có tổng lớn nhất của mảng. Ví dụ với mảng A[]={-2, -5, 6, -2,-3, 1, 5, -6} ta có kết quả là 7 tương ứng với dãy con {6, -2, -3, 1, 5}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm 2 dòng: dòng thứ nhất đưa vào hai số N tương ứng với số phần tử của mảng; dòng tiếp theo đưa vào N số A[i]; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤ N≤100; -100≤A[i] ≤100.
    Output:
    
    Đưa ra tổng con liên tục lớn nhất của mỗi test theo từng dòng.
    Ví dụ:
    
    Input
    
    Output
    
    1
    8
    -2 -5 6 -2 -3 1 5 -6
    
    7

---
+++ CTDL&DT/DÃY CON LIÊN TIẾP CÓ TỔNG LỚN NHẤT.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int n;
+		int a[1000];
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		int max1 = -1000;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += a[i];
+			if (sum < 0)sum = 0;
+			max1 = max(max1, sum);
+		}
+		cout << max1 << endl;
+	}
+	system("pause");
+	return 0;
+}
