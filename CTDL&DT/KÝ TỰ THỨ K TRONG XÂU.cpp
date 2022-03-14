e9764ababc06b8c312a219a44b05600759ad9c59
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-14 11:05:37 +0700

    Create KÝ TỰ THỨ K TRONG XÂU.cpp
    Xâu ký tự S được tạo ra bằng cách bổ sung dần các ký tự chữ cái Tiếng Anh in hoa như sau.
    
    Bước 1: Chỉ có chữ cái A
    Bước 2: Thêm chữ cái B vào giữa 2 chữ A => S = "ABA"
    Bước 3: Thêm chữ cái C vào giữa 2 xâu đã có ở bước 2: S = "ABACABA"
    Cứ như vậy cho đến bước thứ N (0 < N < 26)
    
    Hãy xác định ký tự thứ K trong bước biến đổi thứ N là chữ cái gì?
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
    Mỗi test gồm số nguyên dương N và K (1 ≤ N ≤ 25, 1 ≤ K ≤ 2N - 1).
    Output: 
    
    Với mỗi test, in ra đáp án trên một dòng.
    Ví dụ:
    
    Input:
    
    Output
    
    2
    
    3 2
    
    4 8
    
    B
    
    D

---
+++ CTDL&DT/KÝ TỰ THỨ K TRONG XÂU.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+typedef long long ll;
+ll Mu(ll a, ll b) {
+	if (b == 0)return 1;
+	ll x = Mu(a, b / 2);
+	if (b % 2 == 0)return x * x;
+	else {
+		return x * x * a;
+	}
+}
+
+int KQ(ll n, ll k) {
+	ll lenght = Mu(2, n - 1);
+	while (1) {
+		if (n == 1)return 1;
+		if (lenght == k) {
+			return n;
+		}
+		if (lenght < k) {
+			k = lenght - (k - lenght);
+		}
+		lenght /= 2;
+		n--;
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		ll n, k; cin >> n >> k;
+		char x = KQ(n, k) + 64;
+		cout << x << endl;
+	}
+	system("pause");
+	return 0;
+}
