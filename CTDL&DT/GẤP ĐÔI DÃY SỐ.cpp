fd94915fe7a965c799d166e574bf6cd9b2f5410b
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-14 09:32:42 +0700

    nếu ko cần modu(1e9+7) đừng cho vào thành sai
    Một dãy số tự nhiên bắt đầu bởi con số 1 và được thực hiện N-1 phép biến đổi “gấp đôi” dãy số như sau:
    
    Với dãy số A hiện tại, dãy số mới có dạng A, x, A trong đó x là số tự nhiên bé nhất chưa xuất hiện trong A.
    
    Ví dụ với 2 bước biến đổi, ta có [1] - [1 2 1] - [1 2 1 3 1 2 1].
    
    Các bạn hãy xác định số thứ K trong dãy số cuối cùng là bao nhiêu?
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
    
    Mỗi test gồm số nguyên dương N và K (1 ≤ N ≤ 50, 1 ≤ K ≤ 2N - 1).
    
    Output: 
    
    Với mỗi test, in ra đáp án trên một dòng.
    
    Ví dụ:
    
    Input
    
    Output
    
    2
    
    3 2
    
    4 8
    
    2
    
    4
    
    Giải thích test 1: Dãy số thu được là [1, 2, 1, 3, 1, 2, 1].
    
    Giải thích test 2: Dãy số thu được là [1, 2, 1, 3, 1, 2, 1, 4, 1, 2, 1, 3, 1, 2, 1].

---
+++ CTDL&DT/GẤP ĐÔI DÃY SỐ.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+
+ typedef long long ll;
+long long Luy_thua(int a,long long b  ) {
+	if(b == 0)return 1;
+	
+	long long x = Luy_thua(a, b / 2);
+	if (b % 2 == 0) {
+		return (x * x) ;
+	}
+	else {
+		return a*((x * x)  ) ;
+	}
+}
+//n luôn là vị trí trung tâm
+int KQ(int n, long long k) {//k là vị trí cần tìm
+	long long lenght = Luy_thua(2, n - 1);// khoảng cách từ vị trí đầu đến n
+		while (1) {
+			if (n == 1)return 1;
+			if (k == lenght)return n;//bằng khoảng cách từ vị trí đầu đến n ( hiểu 1/2 dãy )
+			//tức là bằng luôn = n
+			if (k > lenght) {
+				k = lenght - (k - lenght);//nếu k lơn hơn lấy đối xứng vị trí của k qua n 
+			}
+			lenght /= 2;
+			n--;
+		}
+}
+int main() {
+	
+	int t; cin >> t;
+	while (t--) {
+		int n; long long k; cin >> n >> k;
+		cout << KQ(n, k) << endl;
+		
+	}
+		system("pause");
+		return 0;
+}
