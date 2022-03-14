2af3db7d21c90cab7c93c165434ca613af251012
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-14 08:50:37 +0700

    Dãy con có tổng = nó = 2^(n-1)(n là số đó)
    Cho số nguyên dương n. Hãy cho biết có bao nhiêu dãy số nguyên dương có tổng các phần tử trong dãy bằng n. 
    
    Dữ liệu vào: dòng đầu tiên chứa số nguyên T là số bộ dữ liệu, mỗi bộ dữ liệu ghi một số nguyên dương n duy nhất không qua 1018. 
    
    Kết quả: Mỗi bộ dữ liệu ghi ra một số nguyên duy nhất là số dư của kết quả tìm được khi chia cho 123456789.
    
    Ví dụ:
    
    Input
    
    Output
    
    1
    
    3
    
    4

---
+++ CTDL&DT/ĐẾM DÃY.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+const long long modu = 123456789;
+long long Luy_thua(int a,long long b  ) {
+	if(b == 0)return 1;
+	long long x = Luy_thua(a, b / 2);
+	if (b % 2 == 0) {
+		return (x * x) % modu;
+	}
+	else {
+		return ((x * x) % modu * a) % modu;
+	}
+}
+int main() {
+	
+	long long t; cin >> t;
+	while (t--) {
+		long long n; cin >> n;
+		cout << (Luy_thua(2, n - 1)) << endl;
+	}
+		system("pause");
+		return 0;
+}
