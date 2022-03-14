678c0f8bdf73632870d65166a32256ed93869740
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-14 10:22:02 +0700

    chú ý độ rộng của mảng
    Cho dãy số A[] gồm có N phần tử đã được sắp xếp tăng dần và số K.
    
    Nhiệm vụ của bạn là kiểm tra xem số K có xuất hiện trong dãy số hay không. Nếu có hãy in ra vị trí trong dãy A[], nếu không in ra “NO”.
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 10).
    
    Mỗi test bắt đầu bằng số nguyên N và K (N ≤ 100 000, 0 ≤ K ≤ 106).
    
    Dòng tiếp theo gồm N số nguyên A[i] (0 ≤ A[i] ≤ 106), các phần tử là riêng biệt.
    
    Output: 
    
    Với mỗi test in ra trên một dòng đáp án tìm được.
    
    Ví dụ:
    
    Input:
    
    Output
    
    2
    
    5 3
    
    1 2 3 4 5
    
    6 5
    
    0 1 2 3 9 10
    
    3
    
    NO

---
+++ CTDL&DT/TÌM KIẾM NHỊ PHÂN.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+
+int find(int *a,int n,int k) {
+	int left = 0, right = n - 1;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (a[mid] == k)return mid+1;//vid phan tu cua minh chay tu 0 
+		if (a[mid] < k) {
+			left = mid + 1;
+		}
+		else {
+			right = mid - 1;
+		}
+	}
+	return 0;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[100000];
+		int n, k; cin >> n >> k;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		if (find(a, n, k) == 0) {
+			cout << "NO";
+		}
+		else {
+			cout << find(a, n, k);
+		}
+		cout << endl;
+	}
+
+}
