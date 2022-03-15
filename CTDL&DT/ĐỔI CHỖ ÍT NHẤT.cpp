b81f0e2ef47796db5f14485c16a843bdde6089b7
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-15 15:32:12 +0700

    Create ĐỔI CHỖ ÍT NHẤT.cpp
    Cho mảng A[] gồm n phần tử. Hãy tìm số phép đổi chỗ ít nhất giữa các phần tử của mảng để mảng A[] được sắp xếp. Ví dụ với A[] = {4, 3, 2, 1} ta cần thực hiện ít nhất 2 phép đổi chỗ: Swap(A[0], A[3]),  Swap(A[1], A[2]).
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên là số phần tử của mảng n và X; dòng tiếp theo là n số A [i] của mảng A [];các số được viết cách nhau một vài khoảng trống.
    T, n thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    4
    4 3 2 1
    5
    1 5 4 3 2
    
    2
    2

---
+++ CTDL&DT/ĐỔI CHỖ ÍT NHẤT.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+void input(int* a , int n) {
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[1000];
+		int cnt = 0;
+		int n; cin >> n;
+		input(a, n);
+		for (int i = 0; i < n - 1; i++) {
+			int k = i;
+			for (int j = i + 1; j < n; j++) {
+				if (a[j] < a[k]) {
+					k = j;
+				}
+			}
+			if (k != i) {
+				cnt++;
+				swap(a[i], a[k]);
+			}
+		}cout << cnt << endl;
+	}
+	system("pause");
+	return 0;
+}
