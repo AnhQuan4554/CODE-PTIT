640773adb659ae20c6cc4cbe9e024f152af90740
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-15 16:03:28 +0700

    Create TỔNG GẦN 0 NHẤT.cpp
    Cho mảng A[] gồm n phần tử, hãy tìm cặp phần tử có tổng gần nhất so với 0.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai dòng: dòng thứ nhất đưa vào n là số phần tử của mảng A[]; dòng tiếp theo đưa vào n số A[i]; các số được viết cách nhau một vài khoảng trống.
    T, n, A[i] thỏa mãn ràng buộc: 1≤T≤100; 2≤N ≤103, -106≤A[i] ≤106.
    Output:
    
    Đưa ra tổng gần nhất với 0 của cặp phần tử.
    Input:
    
    Output:
    
    2
    3
    -8 -66 -60
    
    6
    -21 -67 -37 -18 4 -65  
    
    -68
    -14

---
+++ CTDL&DT/TỔNG GẦN 0 NHẤT.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+void input(int* a , int n) {
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[1000];
+		int n; cin >> n  ;
+		input(a, n);
+		int sum = abs(a[0] + a[1]);
+		for (int i = 0; i < n-1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (abs(a[i] + a[j])<abs(sum))sum = (a[i] + a[j]);
+			}
+		}
+		cout << sum << endl;
+	}
+	system("pause");
+	return 0;
+}
