0db7f76932ff8db3c6d4034d5fae2e179977bb9f
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-15 15:52:07 +0700

    Create CẶP SỐ TỔNG BẰNG K.cpp
    Cho mảng A[] gồm n phần tử và số k. Đếm tất cả các cặp phần tử của mảng có tổng bằng k. Ví dụ A[] = {1, 5, 3, 4, 2 }, k = 7 ta có kết quả là 2 cặp (3, 4), (5, 2).
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test gồm hai dòng: dòng đầu tiên đưa vào n là số phần tử của mảng A[] và k; dòng tiếp theo là n số A[i] của mảng A[]các số được viết cách nhau một vài khoảng trống.
    T, n, k, A[i] thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ n ≤100; 0≤ k ≤100, 0≤ A[i] ≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Input:
    
    Output:
    
    2
    5 9 
    1 5 4 1 2
    3 2
    
    1 1 1
    
    1
    3

---
+++ CTDL&DT/CẶP SỐ TỔNG BẰNG K.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+void input(int* a , int n) {
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[1000];
+		int cnt = 0;
+		int n,k; cin >> n>>k;
+		input(a, n);
+		for (int i = 0; i < n-1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (a[i] + a[j] == k)cnt++;
+			}
+		}
+		cout << cnt << endl;
+	}
+	system("pause");
+	return 0;
+}
