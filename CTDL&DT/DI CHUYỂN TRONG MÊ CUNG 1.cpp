134f32f882de0e5e58dde64f127d7a617fc60947
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-24 17:39:41 +0700

    Quay lui đặc trưng
    Cho một mê cung bao gồm các khối được biểu diễn như một ma trận nhị phân A[N][N]. Một con chuột đi từ ô đầu tiên góc trái (A[0][0]) đến ô cuối cùng góc phải (A[N-1][N-1]) theo nguyên tắc:
    
    Down (D): Chuột được phép xuống dưới nếu ô dưới nó có giá trị 1.
    Right (R): Chuột được phép sang phải dưới nếu ô bên phải nó có giá trị 1.
    Hãy đưa ra một hành trình của con chuột trên mê cung. Đưa ra -1 nếu chuột không thể đi đến đích.
    
                            
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai phần: phần thứ nhất đưa vào số N là kích cỡ của mê cung; dòng tiếp theo đưa vào ma trận nhị phân A[N][N].
    T, N, A[i][j] thỏa mãn ràng buộc: 1≤T ≤10; 2≤N≤10; 0≤A[i][j] ≤1.
    Output:
    
    Đưa ra tất cả đường đi của con chuột trong mê cung theo thứ tự từ điển. Đưa ra -1 nếu chuột không đi được đến đích.
    Input
    
    Output
    
    2
    
    4
    
    1  0  0  0
    
    1  1  0  1 
    
    0  1  0  0 
    
    1  1  1  1
    
    5
    
    1 0 0 0 0
    
    1 1 1 1 1
    
    1 1 0 0 1
    
    0 1 1 1 1
    
    0 0 0 1 1
    
    DRDDRR
    
    DDRDRRDR DDRDRRRD DRDDRRDR DRDDRRRD DRRRRDDD

---
+++ CTDL&DT/DI CHUYỂN TRONG MÊ CUNG 1.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+vector<string>result;
+void input(int a[][100], int n) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> a[i][j];
+		}
+	}
+}
+void Try(int i, int j, int n, int a[][100], string b) {
+	if (i == n && j == n) {
+		result.push_back(b);
+		return;
+	}
+	if (i < n && a[i + 1][j] == 1) {
+		
+		Try(i + 1, j, n, a, b+"D");
+	}
+	if (j < n && a[i][j + 1] == 1) {
+	
+		Try(i, j + 1, n, a, b+"R");
+	}
+}
+int main() {
+
+	int t; cin >> t;
+	while (t--) {
+		result.clear();
+		int a[100][100];
+		string b="";
+		int n;
+		cin >> n;
+		input(a, n);
+		if(a[1][1]==1)
+		Try(1, 1, n, a, b);
+		if (result.size()) {
+			for (int i = 0; i < result.size(); i++) {
+
+				cout << result[i] << " ";
+			}
+		}
+		else cout << -1;
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
