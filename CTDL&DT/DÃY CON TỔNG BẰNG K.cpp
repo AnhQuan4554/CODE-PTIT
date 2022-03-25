bf4a0425bcd6132ecf2e8970cac2ae463c43ab73
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-25 10:14:14 +0700

    Create DÃY CON TỔNG BẰNG K.cpp
    Cho dãy số A[] = (a1, a2, .., an) và số tự nhiên K. Hãy đưa ra tất cả các dãy con của dãy số A[] sao cho tổng các phần tử của dãy con đó đúng bằng K. Các phần tử của dãy số A[] được giả thuyết là nguyên dương và không có các phần tử giống nhau. Ví dụ với dãy con A[] = {5, 10, 15, 20, 25}, K = 50 ta có 3 dãy con {5, 10, 15, 20}, {5, 20, 25}, {10, 15, 25}.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm hai phần: phần thứ nhất đưa vào số N là số lượng phần tử của dãy số A[] và số K; dòng tiếp theo đưa vào N phần tử của dãy số A[].
    T, N, A[i] thỏa mãn ràng buộc: 1≤T ≤100; 1≤N≤10; 1≤ K, A[i] ≤100.
    Output:
    
    Đưa ra tất cả các dãy con của dãy số A[] thỏa mãn yêu cầu bài toán theo thứ tự từ điển, trong đó mỗi dãy con được bao bởi các ký tự [, ]. Nếu không có dãy con nào thỏa mãn yêu cầu bài toán, hãy đưa ra -1.
    Input
    
    Output
    
    2
    5 50
    
    5  10 15 20  25
    8  53
    15  22  14  26  32  9  16  8
    
    [5 10 15 20] [5 20 25] [10 15 25]
    [8 9 14 22] [8 14 15 16] [15 16 22]

---
+++ CTDL&DT/DÃY CON TỔNG BẰNG K.cpp
@@ -0,0 +1,77 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int n;
+int sumRequired;
+int* a = new int[n + 1000];
+vector<int>result;
+vector<vector<int>>Display;
+void input() {
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		result.push_back(x);
+	}
+	sort(result.begin(), result.end());
+}
+void Defalt() {
+	for (int i = 0; i < n; i++) {
+		a[i] = 0;
+	}
+}
+void Try(int i) {
+	for (int j = 0; j <= 1; j++) {
+		a[i] = j;
+		if (i == n-1) {
+			int sum = 0;
+			for (int x = 0; x < n; x++) {
+				if (a[x]) {
+					sum += result[x];
+				}
+			}
+			if (sum == sumRequired) {
+				vector<int>e;
+				for (int x = 0; x < n; x++) {
+					if (a[x]) {
+						e.push_back(result[x]);
+					}
+				}
+				Display.push_back(e);
+			}
+		}
+		else {
+			Try( i + 1);
+		}
+	}
+}
+int main() {
+	
+	int t; cin >> t;
+	while (t--) {
+		cin >> n >> sumRequired;
+		Display.clear();//sau mỗi lần cần lưu mảng mới
+		result.clear();// khác với việc nhập cái này cứ push do đó cũng cần xóa đi 
+		Defalt();
+		input();
+		Try(0);
+		sort(Display.begin(), Display.end());
+		if (Display.size() == 0) {
+			cout << -1;
+		}
+		else {
+
+		for (int i = 0; i < Display.size(); i++) {
+			cout << "[";
+			for (int x = 0; x < Display[i].size() - 1; x++) {
+				cout << Display[i][x] << " ";
+			}
+			cout << Display[i][Display[i].size() - 1] << "] ";
+		}
+		}
+		cout << endl;
+	}
+	
+
+	system("pause");
+	return 0;
+}
