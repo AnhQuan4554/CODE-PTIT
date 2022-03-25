b061837b3a626bcec2c4b21c95c5473058498707
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-25 08:27:00 +0700

    Dễ thi cuối kì
    Cho xâu ký tự S bao gồm các ký tự in hoa khác nhau. Hãy đưa ra tất cả các hoán vị của xâu ký tự S. Ví dụ S=”ABC” ta có kết quả {ABC ACB BAC BCA CAB CBA}. 
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng tiếp theo đưa vào các bộ test. Mỗi bộ test là một xâu ký tự S được viết trên 1 dòng.
    T, S thỏa mãn ràng buộc: 1≤T≤10; 1≤length(S) ≤10;
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
     
    Input
    
    Output
    
    2
    AB
    
    ABC
    
    AB BA
    
    ABC ACB BAC BCA CAB CBA

---
+++ CTDL&DT/HOÁN VỊ XÂU KÝ TỰ.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<string>
+int n;
+int* a = new int[n + 1000];
+int* used = new int[n + 1000];
+vector<pair<string, int>>result(n + 1000);
+string start;
+void input() {
+	for (int i = 0; i < start.length(); i++) {
+		result[i+1].first = start[i]; 
+		result[i+1].second = i+1;
+	}
+}
+void Default() {
+	for (int i = 1; i <= n; i++) {
+		a[i] = i;
+		used[i] = 0;
+	}
+}
+void Try(int i) {
+	for (int j = 1; j <= n; j++) {
+		if (used[j] ==  0) {
+			a[i] = j;
+			used[j] = 1;
+			if (i == n) {
+				for (int x = 1; x <= n; x++) {
+					//cout << a[x] << " ";
+					cout << result[a[x]].first ;
+				}cout << " ";
+			}
+			else {
+				Try(i + 1);
+			}
+			used[j] = 0;
+		}
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		cin >> start;
+		n = start.length();
+		Default();
+		input();
+		Try(1);
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
