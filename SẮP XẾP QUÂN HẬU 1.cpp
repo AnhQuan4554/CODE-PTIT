3c1bdf8492dae6a52d17c6cf4a568a5516d9ca2d
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-25 11:21:22 +0700

    Create SẮP XẾP QUÂN HẬU 1.cpp
    Cho một bàn cờ vua có kích thước n * n, ta biết ràng quân hậu có thể di chuyển theo chiều ngang, dọc, chéo. Vấn đề đặt ra rằng, có n quân hậu, bạn cần đếm số cách đặt n quân hậu này lên bàn cờ sao cho với 2 quân hậu bất kì, chúng không “ăn” nhau.
    
    Input: Dòng đầu ghi số bộ test T (T<5). Mỗi bộ test ghi một số nguyên dương n duy nhất (không quá 10)
    
    Output:  Ghi kết quả mỗi bộ test trên một dòng. Số cách đặt quân hậu.
    
    Ví dụ:
    
    Input
    
    Output
    
    1
    
    4
    
     
    
    2

---
+++ SẮP XẾP QUÂN HẬU 1.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+using namespace std;
+int a[1000];
+int column[1000];
+int  cheo1[1000];
+int	 cheo2[1000];
+int n;
+int count1 ;
+void input() {
+	for (int i = 1; i <= n; i++) {
+		column[i] = 0;
+		cheo1[i] = 0;
+		cheo2[i] = 0;
+	}
+}
+void Try(int i) {
+	for (int j = 1; j <= n; j++) {
+		if (column[j] == 0&&cheo1[i-j+n]==0&&cheo2[i+j-1]==0) {
+			a[i] = j;
+			column[j] = cheo1[i-j+n] = cheo2[i + j - 1] = 1;
+			if (i == n) {
+			/*	for (int x = 1; x <= n; x++) {
+					cout << "con hau pwr hang thu " << x << " " << "cot" << a[x];
+				}cout << endl;*/
+				count1++;
+			}
+			else {
+				Try(i + 1);
+			}
+			column[j] = cheo1[i - j + n] = cheo2[i + j - 1] = 0;
+
+		}
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+
+	cin >> n;
+	count1 = 0;
+	input();
+	Try(1);
+	cout << count1 << endl;
+	}
+	system("pause");
+	return 0;
+}
