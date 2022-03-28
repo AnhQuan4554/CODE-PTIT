a513d2d48450965014b204fd8be1baa43c61deea
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-28 08:17:29 +0700

    Create NGÀY ĐẶC BIỆT.cpp
    Hôm nay là ngày 22/02/2022. Một ngày đặc biệt. Chỉ có hai chữ số là 0 và 2. Đồng thời viết xuôi viết ngược tám chữ số ta đều được cùng một kết quả.
    
    Nhiệm vụ của bạn trong ngày đặc biệt này là hãy liệt kê hết tất cả các ngày hợp lệ chỉ có 2 chữ số 0 và 2.
    
    Một số chú ý:
    
    Luôn có đủ 8 chữ số của ngảy tháng năm và hai dấu gạch chéo.
    Ngày và tháng có thể có chữ số 0 ở đầu nhưng với năm thì không được phép. Tức là ta chỉ tính các ngày bắt đầu từ năm 2000 trở về sau.
    Bài toán không có dữ liệu vào. Bạn chỉ cần liệt kê kết quả gồm tất cả các ngày tháng hợp lệ. Coi dãy ký tự ngày tháng là một xâu ký tự. Hãy liệt kê theo đúng thứ tự từ điển tăng dần.

---
+++ CTDL&DT/NGÀY ĐẶC BIỆT.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main() {
+	vector<string>day = { "02","20","22" };
+	string month = "02";
+	vector<string>year = { "2000","2002","2020","2022","2200","2202","2220","2222" };
+	for (int i = 0; i < day.size(); i++) {
+		for (int j = 0; j < year.size(); j++) {
+			cout << day[i] << "/" << month << "/" << year[j] << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
