0513804cdcea0608300d1463ddf46f7ad296e40b
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-28 11:38:29 +0700

    Create LIỆT KÊ XÂU KÝ TỰ.cpp
    Cho chữ cái c in hoa (‘A’ < c < ’K’) và số nguyên K (0 < K < (c – ‘A’)).
    
    Hãy tìm cách  liệt kê tất cả các xâu ký tự khác nhau được tạo ra bởi các chữ cái tính từ ‘A’ đến ký tự c. Các ký tự được phép lặp lại nhưng không tính các xâu là hoán vị của xâu nào đó đã liệt kê trước đó.
    
    Xem ví dụ để hiểu thêm yêu cầu đề bài.
    
    Input
    
    Chỉ có một dòng ghi chữ cái c và số nguyên K thỏa mãn ràng buộc đề bài.
    
    Output
    
    Ghi ra lần lượt các xâu ký tự kết quả theo thứ tự từ điển, mỗi xâu trên một dòng.
    
    Ví dụ
    
    Input
    
    Output
    
    D 2
    
    AA
    
    AB
    
    AC
    
    AD
    
    BB
    
    BC
    
    BD
    
    CC
    
    CD
    
    DD

---
+++ CTDL&DT/LIỆT KÊ XÂU KÝ TỰ.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+#include<algorithm>
+int n,k;
+int* a = new int[n + 10];
+vector<vector<int>> result;
+void creat() {
+	for (int i = 1; i <= k; i++) {
+		a[i] = i;
+	}
+}
+void Try(int i) {
+	for (int j = a[i-1] ; j < n ; j++) {
+		a[i] = j;
+		if (i == k) {
+			
+			for (int x = 1; x <= k; x++) {
+				//cout << a[x] << " ";
+				char b = a[x] + 65;
+				cout << b;
+			}cout << endl;
+		}
+		else Try(i + 1);
+	}
+}
+int main() {
+	string x; cin >> x;
+	 n = x[0]-65+1;
+	cin>>k;
+	a[0] = 0;
+	Try(1);
+	system("pause");
+	return 0;
+}
