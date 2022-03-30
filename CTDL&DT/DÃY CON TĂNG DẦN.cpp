4501734eb487e4ee3bcf9562840eada47c3c12f1
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-30 22:08:21 +0700

    dùng string để xuất lưu ý số lượng mảng
    Cho dãy số a[] có n phần tử là các số nguyên dương khác nhau từng đôi một. Hãy liệt kê tất cả các dãy con có từ 2 phần tử trở lên của dãy a[] thỏa mãn tính chất tăng dần.
    
    Dãy con tạo được bằng cách lấy ra các phần tử trong dãy a[] nhưng vẫn giữ nguyên thứ tự ban đầu.
    
    Coi mỗi dãy con như một xâu ký tự với các phần tử cách nhau một khoảng trống, hãy liệt kê theo thứ tự từ điển.
    
    Input
    
    Dòng đầu ghi số n (không quá 20)
    Dòng thứ 2 ghi n số của dãy a[]. Các số khác nhau từng đôi một và có giá trị không quá 100.
    Output
    
    Ghi ra lần lượt các dãy con tăng dần theo thứ tự từ điển.
    
    Ví dụ
    
    Input
    
    Output
    
    4
    
    6 3 7 11
    
    3 11
    
    3 7
    
    3 7 11
    
    6 11
    
    6 7
    
    6 7 11
    
    7 11

--- CTDL&DT/DÃY CON TĂNG DẦN.cpp
+++ CTDL&DT/DÃY CON TĂNG DẦN.cpp
@@ -1,1 +1,67 @@
 
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+#include<string>
+int n,k;
+int* a = new int[n+1000];
+vector<string>display;
+vector<int>result(n + 1000);
+void input() {
+	for (int i = 1; i <= n; i++) {
+		int x; cin >> x;
+		result[i] = x;
+	}
+}
+//kiem tra co tang dan ko 
+int check() {
+	for (int x = 1; x <= k-1; x++) {
+		if (result[a[x]] > result[a[x+1]])return 0;
+	}
+	return 1;
+}
+void Creat() {
+	for (int i = 1; i <= k; i++) {
+		a[i] = i;
+	}
+}
+void Try(int i) {
+	for (int j = a[i - 1] + 1; j <= n - k + i; j++) {
+		a[i] = j;
+		if (i == k) {
+			string add="";
+		/*	for (int x = 1; x <= k; x++) {
+				cout<<result[a[x]]<<' ';
+			}*/
+			if (check()) {
+				for (int x = 1; x <= k; x++) {
+				add += to_string(result[a[x]]);
+				if (x != k)add += " ";
+
+				}
+				display.push_back(add);
+			}
+		}
+		else {
+			Try(i + 1);
+		}
+	}
+}
+int main() {
+	
+	cin >> n;
+	input();
+	a[0] = 0;
+	for (int i = 2; i <= n; i++) {
+		k = i;
+	Try(1);
+	}
+	sort(display.begin(), display.end());
+	for (int i = 0; i < display.size(); i++) {
+		cout << display[i] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
