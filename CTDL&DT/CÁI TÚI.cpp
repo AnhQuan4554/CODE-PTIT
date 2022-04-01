9e42d9ab30a219d589b7fea1fae1ffbc3dd0e13c
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-01 11:04:12 +0700

    Create CÁI TÚI.cpp
    Một người có cái túi thể tích V (V<1000). Anh ta có N đồ vật cần mang theo (N≤1000), mỗi đồ vật có thể tích là A[i] (A[i]≤100) và giá trị là C[i] (C[i]≤100). Hãy xác định tổng giá trị lớn nhất của các đồ vật mà người đó có thể mang theo, sao cho tổng thể tích không vượt quá V.
    
    Input
    
    Dòng đầu ghi số bộ test T (T<10)
    Mỗi bộ test gồm ba dòng. Dòng đầu ghi 2 số N và V. Dòng tiếp theo ghi N số của mảng A. Sau đó là một dòng ghi N số của mảng C.
    Dữ liệu vào luôn đảm bảo không có đồ vật nào có thể tích lớn hơn V.
    Output
    
    Với mỗi bộ test, ghi trên một dòng giá trị lớn nhất có thể đạt được.
    Ví dụ
    
    Input
    
    Output
    
    1
    
    15 10
    
    5 2 1 3 5 2 5 8 9 6 3 1 4 7 8
    
    1 2 3 5 1 2 5 8 7 4 1 2 3 2 1
    
    15

---
+++ CTDL&DT/CÁI TÚI.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+int dp[1002][1002];
+int n,W;
+struct dat {
+	int value = 0, weight = 0;
+};
+dat a[1002];
+int res = 0;
+void Quy_hoach() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= W; j++) {
+			if (j < a[i].weight) {
+				dp[i][j] = dp[i - 1][j];
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], (dp[i - 1][j - a[i].weight]) + a[i].value);
+			}
+			res = max(res, dp[i][j]);
+		}
+	}
+	
+}
+int main() {
+
+	int t; cin >> t;
+	while (t--) {
+		cin >> n >> W;
+		for (int i = 1; i <= n; i++) {
+			cin >> a[i].weight;
+		}
+		for (int i = 1; i <= n; i++) {
+			cin >> a[i].value;
+		}
+		Quy_hoach(); cout << res << endl;
+		res = 0;
+	}
+	system("pause");
+	return 0;
+}
