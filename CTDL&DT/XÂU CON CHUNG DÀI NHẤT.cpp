0654cf1d3bebc11b7c4a8a3336b344507a712600
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-01 11:47:27 +0700

    Create XÂU CON CHUNG DÀI NHẤT.cpp
    Cho 2 xâu S1 và S2. Hãy tìm xâu con chung dài nhất của 2 xâu này (các phần tử không nhất thiết phải liên tiếp nhau).
    
    Input: Dòng đầu tiên là số lượng bộ test T (T ≤ 20). Mỗi test gồm hai dòng, mô tả xâu S1 và S2, mỗi xâu có độ dài không quá 1000 và chỉ gồm các chữ cái in hoa.
    
    Output:  Với mỗi test, in ra độ dài dãy con chung dài nhất trên một dòng.
    
    Ví dụ:
    
    Input
    
    Output
    
    2
    
    AGGTAB
    
    GXTXAYB
    
    AA
    
    BB
    
    4
    
    0
    
    Giải thích test 1: Dãy con chung là G, T, A, B.

---
+++ CTDL&DT/XÂU CON CHUNG DÀI NHẤT.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+#include<string>
+#include<algorithm>
+string a, b;
+int result[1001][1001];
+int res = 0;
+void Phan_hoach() {
+	for (int i = 1; i <= a.size(); i++) {
+		for (int j = 1; j <= b.size(); j++) {
+			if (a[i-1] == b[j-1]) {
+				result[i][j] = result[i - 1][j-1] + 1;
+			}
+			else {
+				result[i][j] = max(result[i - 1][j], result[i][j - 1]);
+			}
+			res = max(res, result[i][j]);
+		}
+	}
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		cin >> a >> b;
+		Phan_hoach();
+		cout << res << endl;
+		res = 0;
+	
+	}
+	system("pause");
+	return 0;
+}
