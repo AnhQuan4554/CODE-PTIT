d6e71b894ed0e76ead25ccdd454fc34ffa5fbbd4
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-05 18:20:12 +0700

    Create PHẦN TỬ KHÁC NHAU.cpp
    Cho hai mảng đã được sắp xếp A[] và B[] gồm N và N-1 phần tử. Các phần tử của mảng A[] chỉ khác mảng B[] một phần tử duy nhất. Hãy tìm vị trí của phần tử khác nhau giữa A[] và B[].
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test gồm 3 dòng: dòng thứ nhất đưa vào số N; dòng tiếp theo đưa vào N số của mảng A[];dòng tiếp theo đưa vào N-1 số của mảng B[]; các số được viết cách nhau một vài khoảng trống.
    T, N, A[i], B[i] thỏa mãn ràng buộc: 1≤T≤100;  1≤ N≤107; 0≤ A[i]≤1018.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Ví dụ:
    
    Input
    
    Output
    
    2
    7
    2 4 6 8 9 10 12
    2 4 6 8 10 12
    6
    3 5 7 9 11 13
    3 5 7 11 13
    
    5
    4

---
+++ PHẦN TỬ KHÁC NHAU.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+typedef long long ll;
+
+void input(ll* a, ll n) {
+	for (ll i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		ll n; cin >> n;
+		ll* a = new ll[n + 10000];
+		ll* b = new ll[n + 10000];
+		input(a, n);
+		input(b, n - 1);
+		int check = 0;
+		for (ll i = 1; i <= n - 1; i++) {
+			if (a[i] != b[i]) {
+				cout << i << endl;
+				check = 1;
+				break;
+			}
+		}
+		if (check == 0)cout << n << endl;
+	}
+}
