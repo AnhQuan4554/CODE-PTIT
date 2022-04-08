49478822d9a2d7c420c603952f784d72cf869968
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-08 09:59:45 +0700

    Create BẬC THANG.cpp
    Một chiếc cầu thang có N bậc. Mỗi bước, bạn được phép bước lên trên tối đa K bước. Hỏi có tất cả bao nhiêu cách bước để đi hết cầu thang? (Tổng số bước đúng bằng N).
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 100).
    Mỗi test gồm hai số nguyên dương N và K(1 ≤ N ≤ 100000, 1 ≤ K ≤ 100).
    Output: 
    
    Với mỗi test, in ra đáp án tìm được trên một dòng theo modulo 109+7.
    Ví dụ:
    
    Input
    
    Output
    
    2
    
    2 2
    
    4 2
    
     
    
    2
    
    5
    
     
    
    Giải thích test 1: Có 2 cách đó là (1, 1) và (2).
    
    Giải thích test 2: 5 cách đó là: (1, 1, 1, 1), (1, 1, 2), (1, 2, 1), (2, 1, 1), (2, 2).

---
+++ CTDL&DT/BẬC THANG.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+#include<algorithm>
+typedef long long ll;
+const ll modu = 1e9 + 7;
+ll n,k;
+ll* a = new ll[n + 1000000];
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		cin >> n >> k;
+		for (ll i = 1; i <= n; i++) {
+			a[i] = 0;
+		}
+		a[1] = a[0] = 1;
+		for (ll i = 2; i <= n; i++) {
+			for (int j = 1; j <= min(i, k); j++) {
+				a[i] += a[i - j];
+				a[i] %= modu;
+			}
+		}cout << a[n];
+		cout << endl;
+	}
+		
+	
+	system("pause");
+	return 0;
+}
