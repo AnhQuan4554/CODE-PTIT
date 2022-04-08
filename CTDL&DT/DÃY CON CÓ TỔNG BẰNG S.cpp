9a9c1d8fa90c09ff84a5e51e42bac0efc1df1d08
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-08 19:44:02 +0700

    quy hoạch động
    Cho N  số nguyên dương tạo thành dãy A={A1, A2, ..., AN}. Tìm ra một dãy con của dãy A (không nhất thiết là các phần tử liên tiếp trong dãy) có tổng bằng S cho trước.
    
    Input: Dòng đầu ghi số bộ test T (T<10).  Mỗi bộ test có hai dòng, dòng đầu tiên ghi hai số nguyên dương N và S (0 < N ≤ 200) và S (0 < S ≤ 40000). Dòng tiếp theo lần lượt ghi N số hạng của dãy A là các số A1, A2, ..., AN (0 < Ai ≤ 200).
    
    Output:  Với mỗi bộ test, nếu bài toán vô nghiệm thì in ra “NO”, ngược lại in ra “YES”
    
    Ví dụ:
    
    Input
    
    Output
    
    2
    
    5 6
    1 2 4 3 5
    
    10 15
    
    2 2 2 2 2 2 2 2 2 2
    
    YES
    
    NO

---
+++ CTDL&DT/DÃY CON CÓ TỔNG BẰNG S.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+#include<memory.h>
+int SUM;
+int used[40009];
+void input(int* a, int n) {
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+}
+int check(int* a, int n) {
+	memset(used, 0, sizeof(used));
+	used[0] = 1;// truowng hop co chinh no// cungx chinh la de luu 
+	for (int i = 1; i <= n; i++) {
+		for (int j = SUM; j >= a[i]; j--) {
+			if (used[j - a[i] ]== 1 && used[j] == 0)used[j] = 1;
+		}
+	}return used[SUM];
+}
+
+int main() {
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int a[50000];
+		int n;
+		cin >> n >> SUM;
+		input(a, n);
+		if (check(a, n))cout << "YES\n";
+		else cout << "NO\n";
+	}
+	system("pause");
+	return 0;
+}
