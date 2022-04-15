35bb624cd13fad234f5275f42bf81fc4e842ab56
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-15 11:24:23 +0700

    chú ý số lượng mảng nếu ko RTE
    John có một đàn bò. Một ngày đẹp trời, anh ta quyết định mua xe tải với khả năng chở được C kg (1000 ≤ C ≤ 25000) để đưa những con bò đi xem phim. Cho số con bò là N (20 ≤ N ≤ 100) và khối lượng w[i] của từng con (đều nhỏ hơn C), hãy cho biết khối lượng bò lớn nhất mà John có thể đưa đi xem phim là bao nhiêu.
    
    Input:
    
    Dòng 1: 2 số nguyên C và N cách nhau bởi dấu cách
    Dòng 2..N+1:   Ghi lần lượt các số nguyên: w[i]
    Output:
    
    Một số nguyên là tổng khối lượng bò lớn nhất mà John có thể mang đi xem phim.
    Ví dụ:
    
    Input
    
    Output
    
    259 5
    
    81
    
    58
    
    42
    
    33
    
    61
    
     
    
    242

---
+++ CTDL&DT/XEM PHIM.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<memory.h>
+using namespace std;
+int a[102];
+int resul[102][25006];
+int n;
+int Weight;
+int res = 0;
+void Quy_hoach() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= Weight; j++) {
+			if (j < a[i]) {
+				resul[i][j] = resul[i - 1][j];
+			}
+			else {
+				resul[i][j] = max(resul[i - 1][j], resul[i - 1][j - a[i]] + a[i]);
+			}
+			res = max(res, resul[i][j]);
+		}
+	}
+}
+int main() {
+	cin >> Weight >> n;
+	for (int i = 1; i <= n; i++)cin >> a[i];
+	Quy_hoach(); cout << res;
+	system("pause");
+	return 0;
+}
