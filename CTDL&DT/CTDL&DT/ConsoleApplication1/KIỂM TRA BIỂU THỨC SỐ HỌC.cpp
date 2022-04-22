8a1850152c994f270d45a611ddad0d0c8051613e
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-22 08:59:06 +0700

    kiểm tra trước mỗi ')' có + _*/  hay ko
    Cho biểu thức số học, hãy cho biết biểu thức số học có dư thừa các cặp ký hiệu ‘(’,’) ‘ hay không?
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T;
    Những dòng tiếp theo mỗi dòng đưa vào một bộ test. Mỗi bộ test là một biểu thức.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Ràng buộc:
    
    T, exp thỏa mãn ràng buộc: 1≤T≤100; 2≤length(exp)≤20.
    Ví dụ:
    
     
    
    Input
    
    Output
    
    3
    
    ((a+b))
    
    (a + (b)/c)
    
    (a + b*(c-d))
    
    Yes
    
    Yes
    
    No

---
+++ CTDL&DT/CTDL&DT/ConsoleApplication1/KIỂM TRA BIỂU THỨC SỐ HỌC.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	stack<int>a;
+	cin.ignore();
+	while (t--) {
+		int check = 0;
+		string x;
+		getline(cin, x);
+		for (int i = 0; i < x.length(); i++) {
+			if (x[i] == ')') {
+				check = 0;//sau mỗi lần kiểm tra 1 cặp mới thì luôn cho check =0 tức là dư thừa 
+				char trung_gian = a.top();
+				while (trung_gian!='(')
+				{
+					if (trung_gian == '+' || trung_gian == '-' || trung_gian == '*' || trung_gian == '/') {
+						check = 1;
+					}
+					a.pop();
+					trung_gian = a.top();
+				}a.pop();//thấy một cặp thỏa mãn thì xóa luôn xấu "(" đi để kiể tra cặp khác
+				if (check == 0)break;
+			}
+			else {
+				a.push(x[i]);
+			}
+		}
+		if (check==0)cout << "Yes\n";
+		else cout << "No\n";
+
+
+	}
+	system("pause");
+	return 0;
+}
