236535a4115642fcec2066bae10a29392e5a3ea7
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-05-05 17:08:57 +0700

    Ngăn xếp
    Cho một xâu chỉ gồm các kí tự ‘(‘, ‘)’, ‘[‘, ‘]’, ‘{‘, ‘}’. Một dãy ngoặc đúng được định nghĩa như sau:
    
    -     Xâu rỗng là 1 dãy ngoặc đúng.
    
    -     Nếu A là 1 dãy ngoặc đúng thì (A), [A], {A} là 1 dãy ngoặc đúng.
    
    -     Nếu A và B là 2 dãy ngoặc đúng thì AB là 1 dãy ngoặc đúng.
    
    Cho một xâu S. Nhiệm vụ của bạn là xác định xâu S có là dãy ngoặc đúng hay không?
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
    
    Mỗi test gồm 1 xâu S có độ dài không vượt quá 100 000.
    
    Output: 
    
    Với mỗi test, in ra “YES” nếu như S là dãy ngoặc đúng, in ra “NO” trong trường hợp ngược lại.
    
    Ví dụ:
    
    Input:
    
    Output
    
    2
    
    [()]{}{[()()]()}
    
    [(])
    
    YES
    
    NO

---
+++ CTDL&DT/KIỂM TRA DÃY NGOẶC ĐÚNG.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+using namespace std;
+#include<string>
+#include<stack>
+
+int main() {
+	int t;
+	cin >> t; cin.ignore();
+	while (t--) {
+
+		string x;
+		getline(cin, x);
+		stack<char>a;
+		int check = 1;
+		for (int i = 0; i < x.length(); i++) {
+			if (x[i] == '[' || x[i] == '(' || x[i] == '{') {
+				a.push(x[i]);
+			}
+			else if (x[i] == ']' && !a.empty() && a.top() == '[') {
+
+				a.pop();
+			}
+			else if (x[i] == '}' && !a.empty() && a.top() == '{') {
+
+				a.pop();
+			}
+			else if (x[i] == ')' && !a.empty() && a.top() == '(') {
+
+				a.pop();
+			}
+			
+		}
+		if (a.empty())cout << "YES\n";
+		else cout << "NO\n";
+	}
+	system("pause");
+	return 0;
+}
