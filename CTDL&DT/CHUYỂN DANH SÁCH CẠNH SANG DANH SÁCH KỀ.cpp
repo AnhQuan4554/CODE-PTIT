89255a196df9fde847ec05bc52f5638f1ad2dfed
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-05-05 16:48:58 +0700

    Create CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp
    Cho đồ thị vô hướng G=<V, E> được biểu diễn dưới dạng danh sách cạnh. Hãy viết chương trình thực hiện chuyển đổi biểu diễn đồ thị dưới dạng danh sách kề.
    
    Input:
    
    Dòng đầu tiên đưa vào T là số lượng bộ test.
    Những dòng tiếp theo đưa vào các bộ test. Mỗi bộ test gồm |E| +1 dòng: dòng đầu tiên đưa vào hai số |V|, |E| tương ứng với số đỉnh và số cạnh của đồ thị; |E| dòng tiếp theo đưa vào các bộ đôi uÎV, vÎV tương ứng với một cạnh của đồ thị.
    T, |V|, |E| thỏa mãn ràng buộc: 1≤T≤200; 1≤|V|≤103; 1≤|E|≤|V|(|V|-1)/2;
    Output:
    
    Đưa ra danh sách kề của các đỉnh tương ứng theo khuôn dạng của ví dụ dưới đây. Các đỉnh trong danh sách in ra theo thứ tự tăng dần.
         Ví dụ:
    
    Input:
    
    Output:
    
    1
    
    6  9
    
    1  2
    
    1  3 
    
    2  3
    
    2  5
    
    3  4
    
    3  5
    
    4  5
    
    4  6
    
    5  6
    
    1: 2 3
    
    2: 1 3 5
    
    3: 1 2 4 5
    
    4: 3 5 6
    
    5: 2 3 4 6
    
    6: 4 5

---
+++ CTDL&DT/CHUYỂN DANH SÁCH CẠNH SANG DANH SÁCH KỀ.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<int>a[1002];
+		int n, m; cin >> n >> m;//n la so diem , m la so hang
+		for (int i = 1; i <= m; i++) {
+			int u, v; 
+			cin >> u >> v;
+			a[u].push_back(v);
+			a[v].push_back(u);
+		}
+		for (int i = 1; i <= n; i++) {
+			cout << i << ": ";
+			for (int j = 0; j < a[i].size(); j++) {
+				cout << a[i][j] << " ";
+			}cout << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
