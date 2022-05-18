aa001ae1d4ae66eae5ed0c97d82721b4243b5094
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-05-18 15:19:59 +0700

    duyệt đường đi
    Cho đồ thị có hướng G=<V, E> được biểu diễn dưới dạng danh sách cạnh. Hãy tìm đường đi từ đỉnh sÎV đến đỉnh tÎV trên đồ thị bằng thuật toán DFS.
    
    Input:
    
    Dòng đầu tiên đưa vào T là số lượng bộ test.
    Những dòng tiếp theo đưa vào các bộ test. Mỗi bộ test gồm 2 dòng: dòng đầu tiên đưa vào bốn số |V|, |E|, sÎV, tÎV tương ứng với số đỉnh, số cạnh,  đỉnh u, đỉnh v; Dòng tiếp theo đưa vào các bộ đôi uÎV, vÎV tương ứng với một cạnh của đồ thị.
    T, |V|, |E| thỏa mãn ràng buộc: 1≤T≤100; 1≤|V|≤103; 1≤|E|≤|V|(|V|-1)/2;
    Output:
    
    Đưa ra đường đi từ đỉnh s đến đỉnh t của mỗi test theo thuật toán DFS của mỗi test theo khuôn dạng của ví dụ dưới đây. Nếu không có đáp án, in ra -1.
          Ví dụ:
    
    Input:
    
    Output:
    
    1
    
    6  9  1  6
    
    1 2 2 5 3 1 3 2 3 5 4 3 5 4 5 6 6 4
    
    1 2 5 6

---
+++ CTDL&DT/ĐƯỜNG ĐI THEO DFS VỚI ĐỒ THỊ CÓ HƯỚNG.cpp
@@ -0,0 +1,68 @@
+﻿#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int check[1005];
+int parent[1005];
+#include<stdlib.h>
+void DFS(int u, vector<int>list[1005]) {
+	check[u] = 1;
+	for (int i : list[u]) {
+		if (check[i] == 0) {
+			parent[i] = u;
+			DFS(i, list);
+		}
+	}
+}
+//dat sau khi DFS roi
+void Path(int s, int t) {//s la bat dau , t la diem ket thuc
+	if (check[t] == 0) {
+		cout << "-1";
+	}
+	else {
+		vector<int>path;
+		while (t != s) {
+			path.push_back(t);
+			t = parent[t];
+		}
+		//con diem cuoi cung phai cho not vao
+		path.push_back(s);
+		reverse(path.begin(), path.end());
+		for (int x : path)cout << x << " ";
+	}
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		memset(check, 0, sizeof(check));
+		memset(parent, 0, sizeof(check));
+		int v, e, u,t; cin >> v >> e >> u>>t;
+		vector<int>list[1002];
+		for (int i = 1; i <= e; i++) {
+			int x, y; cin >> x >> y;
+			list[x].push_back(y);
+		//	list[y].push_back(x);
+		}
+		DFS(u, list); 
+		Path(u, t);
+		cout << endl;
+		/*for (int i = 1; i <= e; i++) {
+			for (int j : list[i]) {
+				cout << j << " ";
+			}cout << endl;
+		}*/
+	}
+	
+	return 0;
+	system("pause");
+}
+//6 9 5
+//1 2
+//1 3
+//2 3
+//2 4
+//3 4
+//3 5
+//4 5
+//4 6
+//5 6
